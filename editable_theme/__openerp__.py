1071f3f63b7517ae9a5543aed470cb4266e1d917
cybroodoo <odoo@cybrosys.com>
2016-06-06 13:04:43 +0530

    description updated

--- editable_theme/__openerp__.py
+++ editable_theme/__openerp__.py
@@ -1,5 +1,5 @@
 {
-    'name': "Custom Backend Appearence",
+    'name': "Custom Backend Appearance",
     'summary': """ You can simply change font colour and background colour using this Module.""",
     'description': """ Your own colors on your interface.""",
     'author': "Cybrosys Techno Solutions",
