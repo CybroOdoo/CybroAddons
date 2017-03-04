44727ba346e62c8c5072767bd642ace39d4effcb
cybroodoo <odoo@cybrosys.com>
2017-03-04 15:55:01 +0530

    removed

--- report_xlsx/models/ir_report.py
+++
@@ -1,7 +0,0 @@
-from openerp import fields, models
-
-
-class IrActionsReportXml(models.Model):
-    _inherit = 'ir.actions.report.xml'
-
-    report_type = fields.Selection(selection_add=[("xlsx", "xlsx")])
