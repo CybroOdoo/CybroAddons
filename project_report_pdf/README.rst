2f0e5ae7493543755bc315db5dddfdabccc7fa4e
SHEREEF PT <shereef@cybrosys.in>
2017-03-20 10:34:33 +0530

    A report addon for project app

---
+++ project_report_pdf/README.rst
@@ -0,0 +1,25 @@
+Project Report
+==============
+PDF and XLS Reports for Project Module v8.
+
+
+Features
+========
+* Project Tasks & Issues Report XLS [With advanced Filtration]
+* Project Tasks & Issues Report PDF [With advanced Filtration]
+
+Installation
+============
+To install this module, you need also the **report_xlsx**
+module located in:
+https://github.com/OCA/reporting-engine/tree/8.0/report_xlsx
+
+Credits
+=======
+Cybrosys Techno Solutions <www.cybrosys.com>
+
+Author
+------
+*  Developer: Avinash Nk @ cybrosys
+*  Guidance: Nilmar Shereef @ cybrosys, shereef@cybrosys.in
+
---
+++ project_report_pdf/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2009-TODAY Cybrosys Technologies(<http://www.cybrosys.com>).
+#    Author: Nilmar Shereef(<http://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Project Report XLS & PDF',
+    'version': '0.2',
+    "category": "Project",
+    'author': 'Cybrosys Techno Solutions',
+    'website': "http://www.cybrosys.com",
+    'company': 'Cybrosys Techno Solutions',
+    'summary': """This Module gives PDF & XLS Reports for Project With Advanced Filtrations""",
+    'depends': ['base', 'project', 'project_issue', 'report_xlsx'],
+    'data': ['views/wizard_report.xml',
+             'views/project_report_pdf_view.xml',
+             'views/project_report_button.xml',
+             'views/project_report.xml'
+             ],
+    'license': 'AGPL-3',
+    'images': ['static/description/banner.jpg'],
+    'installable': True,
+    'auto_install': False,
+}
