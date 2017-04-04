a6a2dab82939ba78899df815130ee76868cf459f
SHEREEF PT <shereef@cybrosys.in>
2017-04-04 15:08:42 +0530

    Initial Commit

---
+++ pos_chatter/README.rst
@@ -0,0 +1,25 @@
+==============
+POS Chatter v9
+==============
+This module adds chat interface in Point of sale screen.
+
+Installation
+============
+Just select it from available modules to install it, there is no need to extra installations.
+
+Configuration
+=============
+Nothing to configure.
+
+Usage
+=====
+* Chat Button on POS screen.
+* On clicking chat button,display chat list on the screen.
+* On clicking an item in chat list, chat window open in pos screen.
+
+Credits
+=======
+Developer: Aswani pc @ cybrosys
+Guidance: Nilmar Shereef @ cybrosys, shereef@cybrosys.in
+
+
---
+++ pos_chatter/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2008-TODAY Cybrosys Technologies(<http://www.cybrosys.com>).
+#    Author: Nilmar Shereef(<http://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': "POS Chat",
+    'summary': """Chat in POS""",
+    'description': "This module adds chat interface in Point of sale screen.",
+    'author': "Cybrosys Techno Solutions",
+    'company': "Cybrosys Techno Solutions",
+    'website': "http://www.cybrosys.com",
+    'category': 'Point Of Sale',
+    'version': '0.3',
+    'depends': ['base', 'point_of_sale'],
+    'data': ['views/pos_chat.xml'],
+    'qweb': ['static/src/xml/pos_msg.xml'],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
