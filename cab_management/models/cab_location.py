c7264cd673d637e48c2302d05b2d0a11187a0ea1
SHEREEF PT <shereef@cybrosys.in>
2017-04-17 14:48:47 +0530

    Initial Commit

---
+++ cab_management/models/cab_location.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2008-TODAY Cybrosys Technologies(<http://www.cybrosys.com>).
+#    Author: Nilmar Shereef(<http://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields
+
+
+class CabLocation(models.Model):
+    _name = 'cab.location'
+
+    name = fields.Char(string='City', required=True)
+    cab_zip = fields.Char(string='ZIP')
+    cab_code = fields.Char(string='City Code', size=64, help="The official code for the city")
+    state_id = fields.Many2one('res.country.state', string='State', required=True)
+    country_id = fields.Many2one('res.country', string='Country', required=True)
