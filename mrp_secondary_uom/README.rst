0aa60e7ac99508b0d13e26cfe99a9da74fcd8075
SHEREEF PT <shereef@cybrosys.in>
2017-04-25 17:11:00 +0530

    [ADD]Initial Commit

---
+++ mrp_secondary_uom/README.rst
@@ -0,0 +1,29 @@
+======================
+MRP - Secondary UoM v8
+======================
+
+This module Manage the Manufacturing process with secondary UoM.
+
+Installation
+============
+
+Just select it from available modules to install it, there is no need to extra installations.
+
+Configuration
+=============
+
+After installing this module, Go to Setting->Configuration->Warehouse->Location & Warehouse->
+Enable the different units of measure for products.
+
+Features
+========
+
+* Allows to manage MRP with Secondary UoM.
+* Products to Produce With Secondary UoM.
+* Produced Products With Secondary UoM.
+
+Credits
+=======
+Developer: Nikhil krishnan @ cybrosys, nikhil@cybrosys.in
+
+
---
+++ mrp_secondary_uom/__openerp__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2009-TODAY Cybrosys Technologies(<http://www.cybrosys.com>).
+#    Author: Nikhil krishnan(<http://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'MRP - Secondary UoM',
+    'version': '8.0.1.0.0',
+    'category': 'Manufacturing',
+    'summary': 'Secondary UoM in Manufacturing order',
+    'description': """
+Manage the Manufacturing process with secondary UoM
+===================================================
+This module allows you to cover planning, ordering, stocks and the manufacturing with a Secondary UoM. It handles the
+Products to Produce & Produced Products according to the production with Secondary UoM
+-----------------------------------------------------------------------------------------
+* Allows to manage MRP with Secondary UoM
+* Products to Produce With Secondary UoM
+* Produced Products With Secondary UoM
+""",
+    'author': 'Cybrosys Techno Solutions',
+    'website': "http://www.yourcompany.com",
+    'company': 'Cybrosys Techno Solutions',
+    'depends': ['mrp'],
+    'data': ['views/mrp_sec_uom.xml'],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+}
