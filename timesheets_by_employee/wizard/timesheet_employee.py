281e969540c887b67315d05c0272070d441f71d9
SHEREEF PT <shereef@cybrosys.in>
2017-04-25 14:23:50 +0530

    [ADD]:Initial Commit

---
+++ timesheets_by_employee/wizard/timesheet_employee.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2009-TODAY Cybrosys Technologies(<http://www.cybrosys.com>).
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api
+
+
+class EmployeeTimesheet(models.TransientModel):
+    _name = 'timesheet.wizard'
+
+    employee = fields.Many2one('hr.employee', string="Employee", required=True)
+    from_date = fields.Date(string="Starting Date")
+    to_date = fields.Date(string="Ending Date")
+
+    @api.model
+    def print_timesheet(self, data):
+        """Redirects to the report with the values obtained from the wizard
+                'data['form']': name of employee and the date duration"""
+        rec = self.browse(data)
+        data = {}
+        data['form'] = rec.read(['employee', 'from_date', 'to_date'])
+        return self.env['report'].get_action(self.browse(data), 'timesheets_by_employee.report_timesheets', data=data)
+
