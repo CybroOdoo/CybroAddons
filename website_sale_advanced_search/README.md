f91b552f3e9666219443c2646d218fefdb2f3f3e
SHEREEF PT <shereef@cybrosys.in>
2017-04-27 12:06:56 +0530

    [ADD]Initial Commit

---
+++ website_sale_advanced_search/README.md
@@ -0,0 +1,36 @@
+# Ecommerce Advanced Search
+
+Ecommerce Advanced Search is an odoo e-commerse based addon, which enhance the searching criterias including category selector.
+
+  - E-commerce product search
+  - Can Select Category
+  - New drop down with product website categories
+  - Find all products related to searching content according to category selected
+  - Display results as drop down
+  - Selection of results redirects to product description page
+
+### Depends
+Ecommerce Advanced Search addon depends on Odoo website and website_sale
+### Tech
+
+* [jQuery] - Search AutoComplete
+* [Python] - Controllers
+* [XML] - Odoo website templates
+
+### Installation
+- www.odoo.com/documentation/9.0/setup/install.html
+- Install our custom addon, which also installs its depends [website, website_sale]
+ 
+### Usage
+>Select categories from the search drop down
+>First Install our custom addon, then go to website and select Shop menu.
+On the product search bar type any key, then it will make searching hints as dropdown box under the bar.
+Selecting one of results will redirect to the current product description page.
+
+License
+----
+GNU LESSER GENERAL PUBLIC LICENSE, Version 3 (LGPLv3)
+(http://www.gnu.org/licenses/agpl.html)
+
+
+
---
+++ website_sale_advanced_search/controllers/main.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2017-TODAY Cybrosys Technologies(<http://www.cybrosys.com>).
+#    Author: Hilar AK(<hilar@cybrosys.in>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import json
+from openerp import http
+from openerp.http import request
+
+
+class WebsiteSearch(http.Controller):
+    @http.route('/shop/search', csrf=False, type="http", methods=['POST', 'GET'], auth="public", website=True)
+    def search_contents(self, **kw):
+        """
+        Searches products according to the category selected on front,
+        :param kw: dict contains the category and search key
+        :return: Dict with params as name, res_id, value
+        """
+        strings = '%' + kw.get('name') + '%'
+        category = int(kw.get('category')) if not kw.get('category') == 'all' else ''
+        try:
+            domain = [('public_categ_ids', 'child_of', [category])] if category else []
+            product_as_category = request.env['product.template'].search(domain)
+            sql = """select id as res_id, name as name, name as value from product_template where name ILIKE %s"""
+            extra_query = ''
+            if product_as_category:
+                extra_query = " and id in %s"
+            limit = " limit 15"
+            request.cr.execute(sql+extra_query+limit,\
+                               (tuple([strings]), tuple(product_as_category and product_as_category.ids)))
+            name = request.cr.dictfetchall()
+        except:
+            name = {'name': 'None', 'value': 'None'}
+        return json.dumps(name)
