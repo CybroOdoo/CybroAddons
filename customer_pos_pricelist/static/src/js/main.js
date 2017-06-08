bef0cac5ae0037bb825f2dfeb0d3590c0768e366
SHEREEF PT <shereef@cybrosys.in>
2017-06-08 09:11:13 +0530

    [RMV]Removal

--- customer_pos_pricelist/static/src/js/main.js
+++
@@ -1,8 +0,0 @@
-
-openerp.customer_pos_pricelist = function (instance) {
-    var module = instance.point_of_sale;
-    customer_pos_pricelist_db(instance, module);
-    customer_pos_pricelist_models(instance, module);
-    customer_pos_pricelist_screens(instance, module);
-    customer_pos_pricelist_widgets(instance, module);
-};
