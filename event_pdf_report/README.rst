da67b2d0111a07196dc4e9fe0c6b97482a22e5ef
SHEREEF PT <shereef@cybrosys.in>
2017-06-17 11:16:43 +0530

    [ADD] Initial Commit

---
+++ event_pdf_report/README.rst
@@ -0,0 +1,21 @@
+Event PDF Report v9
+===================
+
+This module helps to get each event details including its attendees  details.
+
+Installation
+============
+Just select it from modules list to install, there is no need to extra installations.
+(It Depends On Events Module)
+
+Usage
+=====
+#.Take print either from the tree view or from the form view of the event from events module.
+Credits
+=======
+Developer: Anusha @ cybrosys
+Guidance: Nilmar Shereef @ cybrosys, shereef@cybrosys.in
+
+
+
+
---
+++ event_pdf_report/report/event_report_parser.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2016-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Anusha(<https://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <https://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.report import report_sxw
+from openerp.osv import osv
+
+
+class EventParser(report_sxw.rml_parse):
+
+    def __init__(self, cr, uid, name, context=None):
+        super(EventParser, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'event_attendee_details': self.event_attendee_details,
+        })
+        self.context = context
+
+    def event_attendee_details(self, d):
+        event = []
+
+        obj_attendee = self.pool.get('event.registration').search(self.cr, self.uid, [('event_id', '=', d)])
+        if obj_attendee:
+            for obj in obj_attendee:
+                obj_event = self.pool.get('event.registration').browse(self.cr, self.uid, obj, context=None)
+                vals = {
+                    'partner_id': obj_event.partner_id.name,
+                    'date_open': obj_event.date_open,
+                    'date_closed': obj_event.date_closed,
+                    'state': obj_event.state,
+
+                }
+                event.append(vals)
+
+            return event
+
+
+class PrintReport(osv.AbstractModel):
+    _name = 'report.event_pdf_report.report_event'
+    _inherit = 'report.abstract_report'
+    _template = 'event_pdf_report.report_event'
+    _wrapped_report_class = EventParser
