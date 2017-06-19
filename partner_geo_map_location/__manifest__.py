d5687b3a6b7149d7807f934ebf0c7424328ed1a0
SHEREEF PT <shereef@cybrosys.in>
2017-06-19 18:12:26 +0530

    Initial Commit

---
+++ partner_geo_map_location/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2016-TODAY Cybrosys Technologies(<http://www.cybrosys.com>).
+#    Author: Cybrosys Technologies(<https://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Partners Location Map',
+    'summary': """Partners geo location can view in the Google Map""",
+    'version': '10.0.1.0.0',
+    'author': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'company': 'Cybrosys Techno Solutions',
+    "category": "Sales",
+    'depends': ['crm', 'web_map'],
+    'data': ['views/partner_map.xml',
+             ],
+    'demo': [
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+}
