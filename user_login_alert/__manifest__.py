b336656cafc4e2d749ed8242efd2df3d00817195
SHEREEF PT <shereef@cybrosys.in>
2017-08-10 15:22:46 +0530

    [ADD] Initial Commit

---
+++ user_login_alert/__manifest__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2017-TODAY Cybrosys Technologies(<http://www.cybrosys.com>).
+#    Author: Niyas Raphy(<http://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'User Login Alert',
+    'summary': """Secure your Odoo account by alerts at right time. If any successful login to your
+                account happens, an alert mail will be send to you with the browser and IP details.""",
+    'version': '10.0.1.0',
+    'description': """Secure your Odoo account by alerts at right time. If any successful login to your
+                    account happens, an alert mail will be send to you with the browser and IP details.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': 'http://www.cybrosys.com',
+    'category': 'Tools',
+    'depends': ['base', 'mail'],
+    'license': 'AGPL-3',
+    'data': [
+        'security/notification_group.xml',
+        'views/logged_details_view.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'external_dependencies': {
+        'python': ['httpagentparser'],
+    },
+
+
+}
