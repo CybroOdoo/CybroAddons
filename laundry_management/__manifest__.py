1e3b5483148654e1c61a2ed0e2d115368042adb7
SHEREEF PT <shereef@cybrosys.in>
2017-08-19 14:59:49 +0530

    [ADD] Inital Commit

---
+++ laundry_management/__manifest__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2017-TODAY Cybrosys Technologies(<http://www.cybrosys.com>).
+#    Author: Jesni Banu(<http://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Laundry Management',
+    'version': '10.0.1.0.0',
+    'summary': """Complete Laundry Service Management""",
+    'description': 'This module is very useful to manage all process of laundry service',
+    "category": "Industries",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['base', 'mail', 'sale', 'account'],
+    'data': [
+        'data/data.xml',
+        'security/laundry_security.xml',
+        'security/ir.model.access.csv',
+        'views/laundry_view.xml',
+        'views/washing_view.xml',
+        'views/config_view.xml',
+        'views/laundry_report.xml',
+        'views/laundry_label.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
