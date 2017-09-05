42042b82ca4b191f1558e88fd34bb3ad090f94ac
Afwan Wali Hamim <afwanwh@users.noreply.github.com>
2017-09-05 11:37:20 +0700

    [FIX] models importation is commented out.
    It's causing error when installing the module.

--- cron_failure_notification/__init__.py
+++ cron_failure_notification/__init__.py
@@ -19,4 +19,5 @@
 #    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
 #    If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################import models
+##############################################################################
+import models
