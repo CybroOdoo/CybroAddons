45508af4eb628abb34f9eef8ce959e1bf797a48f
SHEREEF PT <shereef@cybrosys.in>
2017-09-11 11:37:04 +0530

    [ADD] Initial Commit

---
+++ pos_pricing/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2017-TODAY Cybrosys Technologies(<http://www.cybrosys.com>).
+#    Author: Linto C T(<http://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Price Lists in Point of Sale',
+    'version': '10.0.1.0',
+    'category': 'Point of Sale',
+    'summary': """Set Price Lists for Customers. Products/POS Categories Can be Added to Price Lists""",
+    'author': 'Cybrosys Techno Solutions',
+    'website': "http://www.cybrosys.com",
+    'company': 'Cybrosys Techno Solutions',
+    'depends': ['point_of_sale'],
+    'data': [
+             'views/pricelist.xml',
+             'views/pricelist_menu.xml',
+             'views/pos_template.xml',
+             'security/ir.model.access.csv',
+            ],
+    'qweb': [
+        'static/src/xml/*.xml'],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
