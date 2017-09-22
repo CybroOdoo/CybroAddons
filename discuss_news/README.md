3c58eb97e54596a1493b2636fc4e3621689bdc77
SHEREEF PT <shereef@cybrosys.in>
2017-09-22 09:22:08 +0530

    [ADD] Initial Commit

---
+++ discuss_news/README.md
@@ -0,0 +1,37 @@
+# World News
+
+Subscribe to World news Channels from listed channels under Odoo News. Joined channels will be listed under Discuss Menu.
+
+  - Goto https://newsapi.org/
+  - Register a new account.
+  - Get API key.
+  - Paste under news configurations under News -> Configurations -> settings -> Apply.
+  - Check the Updated News channels Under menu News
+  - Join and Read
+
+
+### Depends
+[mail] addon Odoo
+
+### Tech
+
+* [Python] - Models
+* [XML] - Odoo views
+
+### Installation
+- www.odoo.com/documentation/10.0/setup/install.html
+- Install our custom addon
+- set up https://newsapi.org/ account
+ 
+### Usage
+> Join News channels under News
+> Got Discuss
+> Select the joined channels & Read
+
+License
+----
+GNU LESSER GENERAL PUBLIC LICENSE, Version 3 (LGPLv3)
+(http://www.gnu.org/licenses/agpl.html)
+
+
+
---
+++ discuss_news/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2017-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Hilar AK(<hilar@cybrosys.in>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': "World News",
+    'version': '10.0.1.0.0',
+    'summary': """ World News as Discuss Channels""",
+    'description': """
+       Users can Maintain news channels and subscribe/unsubscribe from world news channels.
+    """,
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': "https://cybrosys.com/",
+    'category': 'Discuss',
+    'depends': ['base', 'mail'],
+    'data': [
+        'views/channel_views.xml',
+        'views/res_config_view.xml',
+        'data/data.xml',
+    ],
+    'demo': [],
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False
+}
