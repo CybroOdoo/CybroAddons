2fcd69d07eb4549e2b617c204b4632a3e2d3a097
SHEREEF PT <shereef@cybrosys.in>
2017-10-19 14:45:35 +0530

    [ADD] Initial Commit

---
+++ inventory_barcode_scanning/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2017-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Sreejith P(<https://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Barcode scanning in Inventory',
+    'version': '10.0.1.0.0',
+    'summary': 'Barcode Support in Stock Picking.',
+    'author': 'Cybrosys Techno solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['stock'],
+    'category': 'Inventory',
+    'demo': [],
+    'data': ['views/stock_picking.xml'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'images': ['static/description/banner.jpg'],
+    'qweb': [],
+    'license': 'LGPL-3',
+}
