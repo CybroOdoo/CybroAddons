49ccfdf093f7e042b7c9e5f0625c8896b996136a
SHEREEF PT <shereef@cybrosys.in>
2017-11-01 09:39:19 +0530

    [ADD] Initial Commit

---
+++ 11.0/pos_ticket/README.rst
@@ -0,0 +1,14 @@
+POS Ticket v11
+==============
+
+This module will add company logo & company info in POS receipt. Also it will print the customer name
+in the receipt if the customer is selected.
+Default POS logo will be replaced by the company logo in POS status bar
+
+Credits
+=======
+Cybrosys Techno Solutions
+
+Author
+------
+* Niyas Raphy <niyas@cybrosys.in>
---
+++ 11.0/pos_ticket/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2017-TODAY Cybrosys Technologies(<http://www.cybrosys.com>).
+#    Author: Niyas Raphy(<http://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Company Logo In POS Receipt',
+    'summary': """Add Company Logo ,Info & Customer name to POS Ticket""",
+    'version': '11.0.1.0',
+    'description': """Add Company Logo , Info & Customer name to POS Ticket""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': 'http://www.cybrosys.com',
+    'category': 'Point Of Sale',
+    'depends': ['base', 'point_of_sale'],
+    'license': 'AGPL-3',
+    'data': [],
+    'qweb': ['static/src/xml/pos_ticket_view.xml'],
+    'images': ['static/description/banner.jpg'],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+
+}
