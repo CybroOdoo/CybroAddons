cf2f6a6647fa5eae0264d360e9be0010324d6f53
SHEREEF PT <shereef@cybrosys.in>
2017-11-02 10:06:49 +0530

    [ADD] Initial Commit

---
+++ translation_helper/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2017-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Linto C T(<https://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Translation Helper',
+    'version': '10.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': """Button To Translate New Words Using 'googletrans'.""",
+    'description': """This module will help you to translate new words.""",
+    'author': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'depends': ['base'],
+    'external_dependencies': {'python': ['googletrans']},
+    'data': [
+        'data/lang_data.xml',
+        'views/translation.xml',
+        'security/ir.model.access.csv',
+            ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ translation_helper/models/translation_helper.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2017-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: LINTO C T(<https://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import time
+try:
+    from googletrans import Translator
+    translator = Translator()
+except ImportError:
+    print "Unable to import "
+
+from odoo import models, fields, _
+from odoo.exceptions import UserError
+
+
+class LanguageList(models.Model):
+    _name = 'languages.list'
+
+    name = fields.Char(string="Name")
+    code = fields.Char(string="Code")
+
+
+class TranslationHelper(models.Model):
+    _inherit = 'ir.translation'
+
+    def translate_term(self):
+        if self.lang:
+            lang = dict(self.fields_get(allfields=['lang'])['lang']['selection'])[self.lang]
+            rec = self.env['languages.list'].search([('name', '=', lang)], limit=1)
+            if rec:
+                code = rec.code
+            if self.source and code:
+                try:
+                    val = translator.translate(self.source, dest=code)
+                    time.sleep(1)
+                    self.value = val.text
+                except:
+                    raise UserError(_("No Translation Found. Please Check Your Internet Connection."))
+            else:
+                raise UserError(_("There is nothing to translate !"))
+        else:
+            raise UserError(_("Select a language!"))
