ba2d200ccd9810b29f9e0a5a8c3f5e4c4b8cefad
SHEREEF PT <shereef@cybrosys.in>
2017-12-06 14:34:08 +0530

    [ADD] Initial Commit

---
+++ sale_promotion/models/promotion_product.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2017-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Anusha P P(<https://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <https://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+from odoo import models, fields
+
+
+class PromotionProduct(models.Model):
+    _inherit = 'product.template'
+
+    is_promotion_product = fields.Boolean(string="Promotion Product", default=False,help='This is a promotion product')
+
+
+
