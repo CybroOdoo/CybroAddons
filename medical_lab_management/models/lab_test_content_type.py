b356094b21272872ad16904fb08dd5d434b80ff9
SHEREEF PT <shereef@cybrosys.in>
2017-12-21 14:58:59 +0530

    [ADD] Initial Commit

---
+++ medical_lab_management/models/lab_test_content_type.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2017-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Maintainer: Cybrosys Technologies (<https://www.cybrosys.com>)
+#
+##############################################################################
+
+from odoo import models, fields
+
+
+class LabTestContentType(models.Model):
+    _name = 'lab.test.content_type'
+    _rec_name = 'content_type_name'
+    _description = "Content"
+
+    content_type_name = fields.Char(string="Name", required=True, help="Content type name")
+    content_type_code = fields.Char(string="Code")
+    parent_test = fields.Many2one('lab.test', string="Test Category")
+
+
+
+
---
+++ medical_lab_management/models/testing_unit.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2017-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Maintainer: Cybrosys Technologies (<https://www.cybrosys.com>)
+#
+##############################################################################
+
+from odoo import models, fields
+
+
+class TestingUnit(models.Model):
+    _name = 'test.unit'
+    _rec_name = 'code'
+    _description = "Test Unit"
+
+    unit = fields.Char(string="Unit", required=True)
+    code = fields.Char(string="code", required=True)
