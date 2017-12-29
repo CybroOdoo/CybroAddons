be4f1707aaf67f7297a000ca64ff70058cf9abfe
Cybrosys <odoo@cybrosys.com>
2017-12-29 17:38:22 +0530

    [ADD] Initial Commit

---
+++ hr_overtime/README.rst
@@ -0,0 +1,39 @@
+HR Overtime Payment v10
+=======================
+
+Overtime payment based on attendance.
+
+Depends
+=======
+[hr_payroll, hr_attendance] addon Odoo
+
+Tech
+====
+* [Python] - Models
+* [XML] - Odoo views
+
+Installation
+============
+- www.odoo.com/documentation/10.0/setup/install.html
+- Install our custom addon
+
+License
+=======
+GNU LESSER GENERAL PUBLIC LICENSE, Version 3 (LGPLv3)
+(http://www.gnu.org/licenses/agpl.html)
+
+Bug Tracker
+===========
+
+Contact odoo@cybrosys.com
+
+Authors
+-------
+* Cybrosys <www.cybrosys.com>
+
+Maintainer
+----------
+
+This module is maintained by Cybrosys Technologies.
+
+For support and more information, please visit https://www.cybrosys.com.
---
+++ hr_overtime/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2017-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Amal P(<https://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <https://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'HR Overtime Payment',
+    'version': '10.0.1.0.0',
+    'summary': 'Overtime Payment Based On Attendance',
+    'category': 'Human Resources',
+    'author': 'Cybrosys Techno solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['hr_payroll', 'hr_attendance'],
+    'data': ['data/salary_rule.xml'],
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
