5af162c00ea56bf34a58674f6b1a8a3b6f4a2803
Sreejith <sreejith@cybrosys.in>
2018-03-03 11:04:06 +0530

    [ADD] Initial Commit

---
+++ website_coupon/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2018-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: LINTO C T(<https://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Website Coupon Code',
+    'version': '11.0.1.0.0',
+    'summary': 'Manage Website Coupon Codes for Products/Categories/All Products & Its Redeem Operations',
+    'category': 'Website',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'depends': ['sale_management', 'website_sale'],
+    'website': 'https://www.cybrosys.com',
+    'data': [
+        'data/product_data.xml',
+        'views/gift_voucher.xml',
+        'views/applied_coupons.xml',
+        'views/templates.xml',
+        'security/ir.model.access.csv'
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
