94be74188592c2833666111a2f7b18b6593559aa
Sreejith <sreejith@cybrosys.in>
2018-03-30 14:29:42 +0530

    [ADD] Initial Commit

---
+++ oh_employee_documents_expiry/models/employee_documents.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#    A part of OpenHRMS Project <https://www.openhrms.com>
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2018-TODAY Cybrosys Technologies (<https://www.cybrosys.com>).
+#    Author: Nilmar Shereef (<https://www.cybrosys.com>)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+###################################################################################
+from datetime import datetime, date, timedelta
+from odoo import models, fields, api, _
+from odoo.exceptions import Warning
+
+
+class HrEmployeeDocument(models.Model):
+    _name = 'hr.employee.document'
+    _description = 'HR Employee Documents'
+
+    def mail_reminder(self):
+        now = datetime.now() + timedelta(days=1)
+        date_now = now.date()
+        match = self.search([])
+        for i in match:
+            if i.expiry_date:
+                exp_date = fields.Date.from_string(i.expiry_date) - timedelta(days=7)
+                if date_now >= exp_date:
+                    mail_content = "  Hello  " + i.employee_ref.name + ",<br>Your Document " + i.name + "is going to expire on " + \
+                                   str(i.expiry_date) + ". Please renew it before expiry date"
+                    main_content = {
+                        'subject': _('Document-%s Expired On %s') % (i.name, i.expiry_date),
+                        'author_id': self.env.user.partner_id.id,
+                        'body_html': mail_content,
+                        'email_to': i.employee_ref.work_email,
+                    }
+                    self.env['mail.mail'].create(main_content).send()
+
+    @api.constrains('expiry_date')
+    def check_expr_date(self):
+        for each in self:
+            if each.expiry_date:
+                exp_date = fields.Date.from_string(each.expiry_date)
+                if exp_date < date.today():
+                    raise Warning('Your Document Is Expired.')
+
+    name = fields.Char(string='Document Number', required=True, copy=False)
+    document_name = fields.Many2one('employee.checklist', string='Document', required=True)
+    description = fields.Text(string='Description', copy=False)
+    expiry_date = fields.Date(string='Expiry Date', copy=False)
+    employee_ref = fields.Many2one('hr.employee', invisible=1, copy=False)
+    doc_attachment_id = fields.Many2many('ir.attachment', 'doc_attach_rel', 'doc_id', 'attach_id3', string="Attachment",
+                                         help='You can attach the copy of your document', copy=False)
+    issue_date = fields.Date(string='Issue Date', default=fields.datetime.now(), copy=False)
+
+
+class HrEmployee(models.Model):
+    _inherit = 'hr.employee'
+
+    @api.multi
+    def _document_count(self):
+        for each in self:
+            document_ids = self.env['hr.employee.document'].sudo().search([('employee_ref', '=', each.id)])
+            each.document_count = len(document_ids)
+
+    @api.multi
+    def document_view(self):
+        self.ensure_one()
+        domain = [
+            ('employee_ref', '=', self.id)]
+        return {
+            'name': _('Documents'),
+            'domain': domain,
+            'res_model': 'hr.employee.document',
+            'type': 'ir.actions.act_window',
+            'view_id': False,
+            'view_mode': 'tree,form',
+            'view_type': 'form',
+            'help': _('''<p class="oe_view_nocontent_create">
+                           Click to Create for New Documents
+                        </p>'''),
+            'limit': 80,
+            'context': "{'default_employee_ref': '%s'}" % self.id
+        }
+
+    document_count = fields.Integer(compute='_document_count', string='# Documents')
+
+
+class HrEmployeeAttachment(models.Model):
+    _inherit = 'ir.attachment'
+
+    doc_attach_rel = fields.Many2many('hr.employee.document', 'doc_attachment_id', 'attach_id3', 'doc_id',
+                                      string="Attachment", invisible=1)
