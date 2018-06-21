8f7b24000a14728f201727d212a9bc7b27837897
Sreejith P <sreejith@cybrosys.in>
2018-06-21 10:49:28 +0530

    [ADD] Initial Commit

---
+++ mrp_production_draft/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2017-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Maintainer: Cybrosys Technologies (<https://www.cybrosys.com>)
+##############################################################################
+from . import models
+
