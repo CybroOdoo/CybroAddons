5e3d4cbaba403cdcef0c6cfe11e7e6c15ab774e3
Sreejith P <sreejith@cybrosys.in>
2018-07-06 14:30:20 +0530

    [ADD] Initial Commit

---
+++ employee_orientation/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from . import orientation_checklist_line
+from . import orientation_checklist
+from . import employee_orientation
+from . import orientation_checklist_request
+from . import employee_training
