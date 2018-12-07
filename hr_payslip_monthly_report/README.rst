a770a71610ea3f30f3a930678b3d1c281cb83424
Sreejith P <sreejith@cybrosys.in>
2018-12-07 09:44:11 +0530

    [INIT]Initial Commit

---
+++ hr_payslip_monthly_report/README.rst
@@ -0,0 +1,53 @@
+Payroll-Payslip Reporting v12
+=============================
+
+This Module help Human resource managers to get a over all view for the payslips as pivot
+
+
+Features
+========
+
+* Pivot view for HR payslips.
+* Group By options like Employee wise, department wise, job title wise, date wise, status wise and Company wise.
+* Spot Export to XLS Report.
+
+
+Depends
+=======
+[hr_payroll] addon Odoo
+
+
+Tech
+====
+* [XML] - Odoo views
+
+
+Installation
+============
+- www.odoo.com/documentation/12.0/setup/install.html
+- Install our custom addon
+
+
+License
+=======
+GNU LGPL, Version 3 (LGPLv3)
+(http://www.gnu.org/licenses/agpl.html)
+
+
+Bug Tracker
+===========
+
+Contact odoo@cybrosys.com
+
+
+Authors
+-------
+* Nikhil Krishnan <nikhil@cybrosys.in>
+* Niyas Raphy <niyas@cybrosys.in>
+
+Maintainer
+----------
+
+This module is maintained by Cybrosys Technologies.
+
+For support and more information, please visit https://www.cybrosys.com.
---
+++ hr_payslip_monthly_report/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2018-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Cybrosys Technologies(<https://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <https://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from . import models
