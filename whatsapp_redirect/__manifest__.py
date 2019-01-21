712db580f19451a810cd8486023b44bd6a531e40
AjmalCybro <ajmal@cybrosys.in>
2019-01-21 14:17:35 +0530

    [UPDT] Manifest Updated

--- whatsapp_redirect/__manifest__.py
+++ whatsapp_redirect/__manifest__.py
@@ -36,7 +36,7 @@
         'views/view.xml',
         'wizard/wizard.xml',
     ],
-    'images': ['static/description/banner.jpg'],
+    'images': ['static/description/banner.png'],
     'installable': True,
     'application': False,
     'auto_install': False,
