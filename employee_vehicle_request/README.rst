31d8d449162db26ed31ca5c833507274a0ad0d5c
Niyas Raphy <niyas@cybrosys.in>
2019-02-02 15:20:13 +0530

    [ADD] Initial Commit

---
+++ employee_vehicle_request/README.rst
@@ -0,0 +1,42 @@
+Employee Vehicle Request v12
+============================
+* Manage Vehicle Requests From Employee
+
+Depends
+=======
+[base] addon Odoo
+[hr] addon Odoo
+[fleet] addon Odoo
+
+
+Tech
+====
+* [Python] - Models
+* [XML] - Odoo views
+
+Installation
+============
+- www.odoo.com/documentation/12.0/setup/install.html
+- Install our custom addon
+
+License
+=======
+ GNU Affero General Public License
+ (http://www.gnu.org/licenses/agpl.html)
+
+Bug Tracker
+===========
+
+Contact odoo@cybrosys.com
+
+Authors
+-------
+* Developer: Nilmar Shereef @ Cybrosys
+* Developer v11: Niyas Raphy @ Cybrosys
+
+Maintainer
+----------
+
+This module is maintained by Cybrosys Technologies.
+
+For support and more information, please visit https://www.cybrosys.com.
---
+++ employee_vehicle_request/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2018-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Employee Vehicle Request',
+    'version': '12.0.1.0.0',
+    'summary': """Manage Vehicle Requests From Employee""",
+    'description': """This module is used for manage vehicle requests from employee.
+                   This module also checking the vehicle availability at the requested time slot.""",
+    'category': "Generic Modules/Human Resources",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['base', 'hr', 'fleet'],
+    'data': [
+        'data/data.xml',
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/employee_fleet_view.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
