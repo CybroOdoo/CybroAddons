cffef9a28f4f29c2dfe708c999589c77e7b30826
Ajmalcybrosys <ajmal@cybrosys.in>
2019-02-21 08:54:28 +0530

    [ADD] Initial Commit

---
+++ sale_amount_check/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2019-TODAY Cybrosys Technologies(<http://www.cybrosys.com>).
+#    Author: Cybrosys Technologies(<http://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Excess Amount Warning',
+    'version': '11.0.1.0.0',
+    'summary': 'Prevent Invoice Creation When Down Payment is Greater Than SO Total',
+    'description': """Prevent Invoice Creation When Down Payment is Greater Than SO Total""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': "http://www.cybrosys.com",
+    'category': 'Sales',
+    'website': 'www.cybrosys.com',
+    'depends': [
+        'base',
+        'sale',
+        'stock'
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
---
+++ sale_amount_check/models/warning_sale_invoice.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2019-TODAY Cybrosys Technologies(<http://www.cybrosys.com>).
+#    Author: Cybrosys Technologies(<http://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from odoo import models, api, _
+from odoo.exceptions import UserError
+
+
+class WarningOnSaleInvoice(models.TransientModel):
+    _inherit = 'sale.advance.payment.inv'
+
+    @api.multi
+    def _create_invoice(self, order, so_line, amount):
+        total_inv = 0
+        for invoices in self.env['account.invoice'].search([('origin', '=', order.name)]):
+            total_inv += invoices.amount_total
+        total_inv += amount
+        if total_inv > order.amount_total:
+            raise UserError(_('You are trying to invoice more than total price'))
+        return super(WarningOnSaleInvoice, self)._create_invoice(order, so_line, amount)
+
