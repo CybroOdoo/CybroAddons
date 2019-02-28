35fddd914137cd90198795f10f3e3ffe13eb0be7
Ajmalcybrosys <ajmal@cybrosys.in>
2019-02-28 09:55:16 +0530

    [ADD] Initial Commit

---
+++ pos_product_category_filter/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2019-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'POS Product Category Filter',
+    'version': '12.0.1.0.0',
+    'category': 'Point of Sale',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': 'http://www.cybrosys.com',
+    'sequence': 6,
+    'summary': 'Show Only Specified Categories of Product in Point of Sale ',
+    'description': """
+
+=======================
+
+""",
+    'depends': ['point_of_sale'],
+    'images': ['static/description/banner.jpg'],
+    'data': ['views/category.xml',
+             'views/template.xml'],
+    'installable': True,
+    'auto_install': False,
+}
