c22dd786cd8c55f405d6168f2b245c223ec23de2
Ajmalcybrosys <ajmal@cybrosys.in>
2019-03-20 08:58:00 +0530

    [UPDT] Manifest Updated

--- asset_documents/__manifest__.py
+++ asset_documents/__manifest__.py
@@ -34,7 +34,7 @@
         'security/ir.model.access.csv',
             "views/asset_notebook.xml",
     ],
-    'images': ['static/description/banner.jpg'],
+    'images': ['static/description/banner.png'],
     'license': 'AGPL-3',
     'demo': [],
     'installable': True,
