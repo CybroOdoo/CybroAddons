3b90449798be007f66afeb57cb96c4fad0d59664
Ajmalcybrosys <ajmal@cybrosys.in>
2019-04-04 15:44:26 +0530

    [ADD] Initial Commit

---
+++ backend_theme_odoo12/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Blueberry Backend Theme",
+    "summary": "Blueberry Backend Theme",
+    "version": "12.0.1.0.0",
+    "category": "Theme/Backend",
+    "website": "https://www.cybrosys.com",
+    "description": """Backend theme for Odoo 12.0 community edition.""",
+    'images': ['static/description/banner.jpg'],
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    "installable": True,
+    "depends": [
+        'website',
+        'portal',
+        'web_responsive'
+    ],
+    "data": [
+        'views/assets.xml',
+        'views/login_templates.xml',
+    ],
+    'qweb': ['static/src/xml/*.xml'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
