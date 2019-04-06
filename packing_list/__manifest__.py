d66a0da226c3192145b9983ade3b37afa5e0f209
Ajmalcybrosys <ajmal@cybrosys.in>
2019-04-06 10:34:15 +0530

    [ADD] Initial Commit

---
+++ packing_list/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2017-TODAY Cybrosys Technologies(<http://www.cybrosys.com>).
+#    Author: Ijaz Ahammed(<http://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': "Packing List",
+    'version': '11.0.1.0.0',
+    'summary': """Packing List""",
+    'description': """ """,
+    'author': "Cybrosys Technologies",
+    'company': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'category': 'Accounting',
+    'depends': ['base', 'account', 'stock', 'sale'],
+    'data': [
+        'views/packing_list.xml',
+        'reports/packing_list_pdf_report.xml'
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ packing_list/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <packing_list>
+
+#### 6.04.2019
+#### Version 11.0.1.0.0
+##### ADD
+
+
