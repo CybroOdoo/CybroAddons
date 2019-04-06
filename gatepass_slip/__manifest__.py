22be64437287e6eaff38ec310bcdcbb4865ed77a
Ajmalcybrosys <ajmal@cybrosys.in>
2019-04-06 17:36:27 +0530

    [ADD] Initial Commit

---
+++ gatepass_slip/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2018-TODAY Cybrosys Technologies (<https://www.cybrosys.com>).
+#    Author: Sayooj A O(<https://www.cybrosys.com>)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Lesser General Public License(LGPLv3) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+###################################################################################
+{
+    'name': 'Gatepass',
+    'summary': """Generating Gate pass slip in delivery orders""",
+    'version': '11.0.1.0.0',
+    'description': """Generating Gate pass slip in delivery orders""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': 'http://www.cybrosys.com',
+    'category': 'Inventory',
+    'depends': ['base', 'stock'],
+    'license': 'LGPL-3',
+    'data': [
+        'views/gate_pass_details_views.xml',
+        'report/gate_pass_template.xml',
+        'report/gate_pass_report.xml',
+        # 'security/ir.model.access.csv',
+
+    ],
+    'demo': [],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ top_selling_product_report/README.rst
@@ -0,0 +1,16 @@
+Top/Least Selling Product Report v11
+====================
+Top Selling and Least Selling Product Reports
+
+Installation
+============
+	- www.odoo.com/documentation/11.0/setup/install.html
+	- Install our custom addon
+Configuration
+=============
+
+    No additional configurations needed
+
+Credits
+=======
+    Developer: Ajmal JK @ cybrosys, Contact: odoo@cybrosys.com
