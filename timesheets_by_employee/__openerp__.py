086e097ef8f2e4f658247afd6ccf073df63e7f75
Ajmalcybrosys <ajmal@cybrosys.in>
2019-04-17 14:17:00 +0530

    [CHG] Manifest Changed

--- timesheets_by_employee/__openerp__.py
+++ timesheets_by_employee/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Timesheet PDF Report',
     'version': '9.0.1.0.0',
-    "category": "Generic Modules/Human Resources",
+    'author': 'Cybrosys Techno Solutions',
     'sequence': 25,
     'summary': 'Timesheet PDF Report of Employees',
     'category': "Generic Modules/Human Resources",
