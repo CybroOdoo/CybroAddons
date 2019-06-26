149e2b895505f3952bd332ddf0950419d6c9b46e
Ajmalcybrosys <ajmal@cybrosys.in>
2019-06-26 09:00:55 +0530

    [ADD] Initial Commit

---
+++ hr_payroll_community/__manifest__.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#    A part of OpenHRMS Project <https://www.openhrms.com>
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2018-TODAY Cybrosys Technologies (<https://www.cybrosys.com>).
+#    Author: Tintuk Tomin (<https://www.cybrosys.com>)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+###################################################################################
+
+{
+    'name': 'Payroll',
+    'category': 'Human Resources',
+    'sequence': 38,
+    'author': 'Cybrosys Techno Solutions',
+    'summary': 'Manage your employee payroll records',
+    'description': "",
+    'depends': [
+        'hr_contract',
+        'hr_holidays',
+        'decimal_precision',
+        'hr_contract_types',
+    ],
+    'data': [
+        'security/hr_payroll_security.xml',
+        'security/ir.model.access.csv',
+        'wizard/hr_payroll_payslips_by_employees_views.xml',
+        'views/hr_contract_views.xml',
+        'views/hr_salary_rule_views.xml',
+        'views/hr_payslip_views.xml',
+        'views/hr_employee_views.xml',
+        'data/hr_payroll_sequence.xml',
+        'views/hr_payroll_report.xml',
+        'data/hr_payroll_data.xml',
+        'wizard/hr_payroll_contribution_register_report_views.xml',
+        'views/res_config_settings_views.xml',
+        'views/report_contributionregister_templates.xml',
+        'views/report_payslip_templates.xml',
+        'views/report_payslipdetails_templates.xml',
+    ],
+    # 'demo': ['data/hr_payroll_demo.xml'],
+}
