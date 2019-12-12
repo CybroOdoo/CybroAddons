5fe558d8c747b6c465e8ab03bb99efce61d22cb9
Ajmalcybrosys <ajmal@cybrosys.in>
2019-12-12 10:34:16 +0530

    [ADD] Initial Commit 'pos_mrp_order'

---
+++ pos_mrp_order/README.rst
@@ -0,0 +1,45 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--1-blue.svg
+    :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+    :alt: License: AGPL-3
+
+Make MRP orders from POS v13
+============================
+
+* From the POS sale we need to generate MRP orders in the back end with out close the current window.
+* Automatically create MRP order form POS sale.
+* We can assign which products make MRP order Automatically.
+
+Configuration
+=============
+* No additional configurations needed
+
+Company
+-------
+* Cybrosys Techno Solutions <https://cybrosys.com/>`__
+
+Credits
+-------
+* Developer:	V10 Nikhil Krishnan @ cybrosys, odoo@cybrosys.com
+		V12 Akshay Babu     @cybrosys,  odoo@cybrosys.com 
+    		V13 Vaishnavi B     @cybrosys,Contact: odoo@cybrosys.com
+Contacts
+--------
+* Mail Contact : odoo@cybrosys.com
+* Website : https://cybrosys.com
+
+Bug Tracker
+-----------
+Bugs are tracked on GitHub Issues. In case of trouble, please check there if your issue has already been reported.
+
+Maintainer
+==========
+.. image:: https://cybrosys.com/images/logo.png
+   :target: https://cybrosys.com
+
+This module is maintained by Cybrosys Technologies.
+
+For support and more information, please visit `Our Website <https://cybrosys.com/>`__
+
+Further information
+===================
+HTML Description: `<static/description/index.html>`__
---
+++ pos_mrp_order/static/src/js/models.js
@@ -0,0 +1,55 @@
+odoo.define('pos_mrp_order.models_mrp_order', function (require) {
+"use strict";
+var pos_model = require('point_of_sale.models');
+var pos_screens = require('point_of_sale.screens');
+var models = pos_model.PosModel.prototype.models;
+var rpc = require('web.rpc');
+
+
+for(var i=0; i<models.length; i++){
+    var model=models[i];
+        if(model.model === 'product.product'){
+            model.fields.push('to_make_mrp');
+
+        }
+    }
+pos_screens.PaymentScreenWidget.include({
+        validate_order: function(force_validation) {
+            var self = this
+            this._super(force_validation);
+            var order = self.pos.get_order();
+            var order_line = order.orderlines.models;            
+            var due = order.get_due();
+             for (var i in order_line)
+              {
+		         var list_product = []
+                 if (order_line[i].product.to_make_mrp)
+                 {
+                   if (order_line[i].quantity>0)
+                   {
+                     var product_dict = {
+                        'id': order_line[i].product.id,
+                        'qty': order_line[i].quantity,
+                        'product_tmpl_id': order_line[i].product.product_tmpl_id,
+                        'pos_reference': order.name,
+                        'uom_id': order_line[i].product.uom_id[0],
+                   };
+                  list_product.push(product_dict);
+                 }
+
+              }
+
+              if (list_product.length)
+              {
+                rpc.query({
+                    model: 'mrp.production',
+                    method: 'create_mrp_from_pos',
+                    args: [1,list_product],
+                    });
+              }
+            }
+
+        },
+
+    });
+});
