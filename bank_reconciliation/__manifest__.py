6617beaae670397f3fbcd986cb59a41046936394
Ajmal JK <ajmal@cybrosys.in>
2020-02-04 11:14:56 +0530

    [ADD] Initial Commit 'bank_reconciliation'

---
+++ bank_reconciliation/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2018-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Fasluca(<faslu@cybrosys.in>)
+#    you can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Manual Bank Reconciliation',
+    'version': '13.0.1.0.0',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'category': 'Accounting',
+    'summary': 'Replacing default bank statement reconciliation method by traditional way',
+    'description': """Replacing default bank statement reconciliation method by traditional way""",
+    'depends': ['account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/account_move_line_view.xml',
+        'views/account_journal_dashboard_view.xml',
+        'wizard/bank_statement_wiz_view.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
---
+++ bank_reconciliation/models/account_move_line.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    bank_statement_id = fields.Many2one('bank.statement', 'Bank Statement', copy=False)
+    statement_date = fields.Date('Bank.St Date', copy=False)
+
+    def write(self, vals):
+        if not vals.get("statement_date"):
+            vals.update({"reconciled": False})
+            for record in self:
+                if record.payment_id and record.payment_id.state == 'reconciled':
+                    record.payment_id.state = 'posted'
+        elif vals.get("statement_date"):
+            vals.update({"reconciled": True})
+            for record in self:
+                if record.payment_id:
+                    record.payment_id.state = 'reconciled'
+        res = super(AccountMoveLine, self).write(vals)
+        return res
