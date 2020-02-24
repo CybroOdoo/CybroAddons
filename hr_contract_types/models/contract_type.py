736d001ac3f35a29821841dcf4ea0831e0c25f86
Tintuk <tintuk@cybrosys.in>
2020-02-24 19:47:26 +0530

    [UPDT] help,arabic

--- hr_contract_types/models/contract_type.py
+++ hr_contract_types/models/contract_type.py
@@ -2,19 +2,19 @@
 
 from odoo import api, fields, models, _
 
-class ContractType(models.Model):
 
+class ContractType(models.Model):
     _name = 'hr.contract.type'
     _description = 'Contract Type'
     _order = 'sequence, id'
 
-    name = fields.Char(string='Contract Type', required=True)
+    name = fields.Char(string='Contract Type', required=True, help="Name")
     sequence = fields.Integer(help="Gives the sequence when displaying a list of Contract.", default=10)
 
-class ContractInherit(models.Model):
 
+class ContractInherit(models.Model):
     _inherit = 'hr.contract'
 
     type_id = fields.Many2one('hr.contract.type', string="Employee Category",
-                              required=True,
+                              required=True, help="Employee category",
                               default=lambda self: self.env['hr.contract.type'].search([], limit=1))
