9b07538f55371524435a2ab2b6ec9a9cd6a40d85
Ajmal JK <ajmal@cybrosys.in>
2020-05-16 15:36:17 +0530

    [ADD] Initial Commit 'product_combo_pack'

---
+++ product_combo_pack/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2020-TODAY Cybrosys Technologies (<https://www.cybrosys.com>).
+#    Author: Afras Habis (odoo@cybrosys.com)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+###################################################################################
+
+{
+    'name': 'Product Pack',
+    'version': '13.0.1.0.0',
+    'summary': 'Manage Products as Pack',
+    'description': 'Manage Products as Pack',
+    'category': 'Sales',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'sale_management', 'stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_form_view.xml',
+        'wizard/select_product_pack_view.xml',
+        'views/sale_order_view.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'license': 'AGPL-3',
+    'auto_install': False,
+}
---
+++ product_combo_pack/doc/RELEASE_NOTES.md
@@ -0,0 +1,5 @@
+## Module <product_combo_pack>
+
+#### 02.05.2020
+#### Version 13.0.1.0.0
+Initial Commit for product_combo_pack
