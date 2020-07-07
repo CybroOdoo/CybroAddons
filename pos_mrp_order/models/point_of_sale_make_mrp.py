4bb2d0601bef7617c8ab09d36d36692d5d9b8efe
Ajmal JK <ajmal@cybrosys.in>
2020-07-07 12:12:25 +0530

    [FIX] Bug Fixed 'pos_mrp_order'

--- pos_mrp_order/models/point_of_sale_make_mrp.py
+++ pos_mrp_order/models/point_of_sale_make_mrp.py
@@ -65,21 +65,19 @@ class MrpProduction(models.Model):
                             mrp_order = self.sudo().create(vals)
                             list_value = []
                             for bom_line in mrp_order.bom_id.bom_line_ids:
-                                print(bom_line.product_id.with_context(force_company=self.company_id.id).property_stock_production.id)
                                 list_value.append((0, 0, {
-                                            'raw_material_production_id': mrp_order.id,
-                                            'name': mrp_order.name,
-                                            'product_id': bom_line.product_id.id,
-                                            'product_uom': bom_line.product_uom_id.id,
-                                            'product_uom_qty': bom_line.product_qty,
-                                            'picking_type_id': mrp_order.picking_type_id.id,
-                                            'location_id': mrp_order.location_src_id.id,
-                                            'location_dest_id': bom_line.product_id.with_context(force_company=self.company_id.id).property_stock_production.id,
-                                            'company_id': mrp_order.company_id.id,
-
+                                    'raw_material_production_id': mrp_order.id,
+                                    'name': mrp_order.name,
+                                    'product_id': bom_line.product_id.id,
+                                    'product_uom': bom_line.product_uom_id.id,
+                                    'product_uom_qty': bom_line.product_qty * mrp_order.product_qty,
+                                    'picking_type_id': mrp_order.picking_type_id.id,
+                                    'location_id': mrp_order.location_src_id.id,
+                                    'location_dest_id': bom_line.product_id.with_context(force_company=self.company_id.id).property_stock_production.id,
+                                    'company_id': mrp_order.company_id.id,
                                 }))
-                            mrp_order.update({'move_raw_ids':list_value})
 
+                            mrp_order.update({'move_raw_ids':list_value})
         return True
 
 
