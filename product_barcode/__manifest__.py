1a07e6e7ad5377afaaf7977f8bcf1dc8fc0d5949
Ajmal Cybro <ajmal@cybrosys.in>
2020-12-03 12:47:14 +0530

    [UPDT] Live previe added

--- product_barcode/__manifest__.py
+++ product_barcode/__manifest__.py
@@ -24,6 +24,7 @@
     'name': 'Product Barcode Generator',
     'version': '14.0.1.0.0',
     'summary': 'Generates EAN13 Standard Barcode for Product.',
+    'live_test_url': 'https://www.youtube.com/watch?v=0BrFcOEkWu4&feature=youtu.be',
     'category': 'Inventory',
     'author': 'Cybrosys Techno solutions',
     'maintainer': 'Cybrosys Techno Solutions',
