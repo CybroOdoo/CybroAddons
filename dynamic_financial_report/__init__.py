5d91a1eb171b0c0a9b724bbb826fcc5ed74d5b15
Ajmal Cybro <ajmal@cybrosys.in>
2021-04-07 10:57:17 +0530

    [UPDT] Index Updated

--- dynamic_financial_report/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import wizard
-from . import report
-from . import controllers
--- dynamic_financial_report/report/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import general_ledger
-from . import financial_reports
-from . import partner_ledger
-from . import ageing_partner
-from . import bank_book
-from . import cash_flow_report
-from . import daybook_report
