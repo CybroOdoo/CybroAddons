9d79dcdb1fe9251642093fbd46e1c9133b50c879
Ajmal Cybro <ajmal@cybrosys.in>
2021-05-15 17:40:42 +0530

    [ADD] Initial Commit 'product_approval_management'

---
+++ product_approval_management/__manifest__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2020-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions (<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': "Product Approval",
+    'summary': """Product Approval Management""",
+    'description': """
+        Using this module a user can create product which will be in 
+             draft state and only a product manager can confirm the product.
+             Also only the confirmed products can be selected from 
+             sale order line
+        """,
+    'category': "Sales/Sales",
+    'author': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'depends': ['sale_management'],
+    'data': [
+        'security/approve_security.xml',
+        'security/ir.model.access.csv',
+        'views/product_approval_views.xml'
+    ],
+    'images': ['static/description/banner.jpg'],
+    'version': '14.0.1.0.0',
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ product_approval_management/models/models.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2020-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions (<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+from odoo import fields, models
+
+
+class ApproveProduct(models.Model):
+    _inherit = 'product.template'
+
+    approve_state = fields.Selection([
+        ('draft', 'Draft'),
+        ('confirmed', 'Confirmed')
+    ], default='draft')
+
+    def confirm_product_approval(self):
+        for rec in self:
+            rec.approve_state = 'confirmed'
+
+    def reset_product_approval(self):
+        for rec in self:
+            rec.approve_state = 'draft'
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    product_id = fields.Many2one(
+        'product.product', string='Product',
+        domain="[('approve_state', '=', 'confirmed'), ('sale_ok', '=', True), '|', ('company_id', '=', False),('company_id', '=', company_id)]",
+        change_default=True, ondelete='restrict', check_company=True)
