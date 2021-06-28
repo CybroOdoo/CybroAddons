45bb4dd4e8f6507f3a263b5850f65a5beb7fed7f
Ajmal Cybro <ajmal@cybrosys.in>
2021-06-28 12:23:42 +0530

    [UPDT] Updated 'theme_boec'

--- theme_boec/static/src/js/custom.js
+++ theme_boec/static/src/js/custom.js
@@ -1,57 +1,58 @@
 odoo.define('theme_boec.theme_boec', function (require) {
 	"use strict";
 
-    //Preloader//
     $(document).ready(function () {
-      setTimeout(function () {
-        $('body').addClass('loaded');
-      }, 1000);
-    });
 
-    //Banner Slider//
-    $(document).ready(function () {
-      $(".owl-carousel").owlCarousel(
-        {
-          items: 1,
-          loop: true,
-          margin: 0,
-          stagePadding: 0,
-          smartSpeed: 450,
-          autoplay: false,
-          autoPlaySpeed: 1000,
-          autoPlayTimeout: 1000,
-          autoplayHoverPause: true,
-          onInitialized: counter,
-          dots: true,
-          nav: true,
-          navText: ['<div class="pre"><i class="material-icons" style="font-size:36px">keyboard_arrow_left</i></div>', '<div class="nxt"><i class="material-icons" style="font-size:36px">keyboard_arrow_right</i></div>'],
-          animateOut: 'fadeOut'
-        }
-      );
-    });
-    function counter() {
-      var buttons = $('.owl-dots button');
-      buttons.each(function (index, item) {
-        $(item).find('span').text(index + 1);
-      });
-    }
+        //Preloader//
+        setTimeout(function () {
+            $('body').addClass('loaded');
+        }, 1000);
 
-    //tab Heading Boarder//
-    $(document).ready(function () {
-      $(".nav-link").click(function () {
-        var value = $(this).attr('data-filter');
-        if (value == "all") {
-          $('.filter').show('1000');
+        //Banner Slider//
+        $(".owl-carousel").owlCarousel({
+            items: 1,
+            loop: true,
+            margin: 0,
+            stagePadding: 0,
+            smartSpeed: 450,
+            autoplay: false,
+            autoPlaySpeed: 1000,
+            autoPlayTimeout: 1000,
+            autoplayHoverPause: true,
+            onInitialized: counter,
+            dots: true,
+            nav: true,
+            navText: ['<div class="pre"><i class="material-icons" style="font-size:36px">keyboard_arrow_left</i></div>', '<div class="nxt"><i class="material-icons" style="font-size:36px">keyboard_arrow_right</i></div>'],
+            animateOut: 'fadeOut'
+        });
+
+        function counter() {
+            var buttons = $('.owl-dots button');
+            buttons.each(function (index, item) {
+                $(item).find('span').text(index + 1);
+            });
         }
-        else {
-          $(".filter").not('.' + value).hide('3000');
-          $('.filter').filter('.' + value).show('3000');
+
+        //add another class for banner_added
+        var banner = $(".banner_added");
+        var banner_id = banner.data("id");
+        $("div").addClass(banner_id);
+        banner.addClass("banner_hide");
+
+        //tab Heading Boarder//
+        $(".nav-link").click(function () {
+            var value = $(this).attr('data-filter');
+            if (value == "all") {
+                $('.filter').show('1000');
+            }else {
+                $(".filter").not('.' + value).hide('3000');
+                $('.filter').filter('.' + value).show('3000');
+            }
+        });
+        if ($(".nav-link").removeClass("active")) {
+            $(this).removeClass("active");
         }
-      });
-      if ($(".nav-link").removeClass("active")) {
-        $(this).removeClass("active");
-      }
-      $(this).addClass("active");
+        $(this).addClass("active");
     });
 
     var url = window.location;
@@ -74,7 +75,7 @@ odoo.define('theme_boec.theme_boec', function (require) {
       });
     }
 
-    // Add active class to the current button (highlight it)
+    //Add active class to the current button (highlight it)
     var header = document.getElementById("myDIV");
     var btns = header.getElementsByClassName("btn");
     for (var i = 0; i < btns.length; i++) {
