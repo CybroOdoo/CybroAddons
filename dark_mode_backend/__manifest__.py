1587a42552fcd98a79dabd04007b01c5ef72c14e
Ajmal Cybro <ajmal@cybrosys.in>
2021-10-01 17:06:02 +0530

    [ADD] Initial Commit 'dark_mode_backend'

---
+++ dark_mode_backend/__manifest__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2021-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+{
+    "name": "Dark Mode Backend Theme",
+    "description": """Minimalist and elegant backend theme for Odoo 14, Backend Theme, Theme""",
+    "summary": "Dark Mode Backend Theme V14 is an attractive theme for backend",
+    "category": "Theme/Backend",
+    "version": "14.0.1.0.0",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    "depends": ['base', 'web', 'mail'],
+    "data": [
+        'assets/assets.xml',
+        'views/layout.xml',
+    ],
+    'images': [
+        'static/description/banner.png',
+        'static/description/theme_screenshots.png',
+    ],
+    'license': 'LGPL-3',
+    'pre_init_hook': 'test_pre_init_hook',
+    'post_init_hook': 'test_post_init_hook',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ dark_mode_backend/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <dark_mode_backend>
+
+#### 09.06.2021
+#### Version 14.0.1.0.0
+#### ADD
+Initial Commit
