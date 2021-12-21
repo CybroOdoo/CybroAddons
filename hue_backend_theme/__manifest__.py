bb32296897d726da8a7dc68a067bc6431a61c7af
Ajmal Cybro <ajmal@cybrosys.in>
2021-12-21 12:26:28 +0530

    [ADD] Initial commit 'hue_backend_theme'

---
+++ hue_backend_theme/__manifest__.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2021-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+{
+    "name": "Hue Backend Theme v15",
+    "description": """Minimalist and elegant backend theme for Odoo 15, Backend Theme, Theme""",
+    "summary": "Hue backed Theme V15 is an attractive theme for backend",
+    "category": "Themes/Backend",
+    "version": "15.0.1.0.0",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    "depends": ['base', 'web', 'mail'],
+    "data": [
+        'views/style.xml',
+        'views/res_config_settings.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+                'hue_backend_theme/static/src/layout/style/login.scss',
+                'hue_backend_theme/static/src/layout/style/layout_colors.scss',
+                'hue_backend_theme/static/src/components/app_menu/menu_order.css',
+                'hue_backend_theme/static/src/layout/style/layout_style.scss',
+                'hue_backend_theme/static/src/layout/style/sidebar.scss',
+                'hue_backend_theme/static/src/components/app_menu/search_apps.js',
+        ],
+        'web.assets_qweb': [
+                'hue_backend_theme/static/src/components/app_menu/side_menu.xml',
+
+        ],
+    },
+    'images': [
+        'static/description/banner.png',
+        'static/description/theme_screenshot.png',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ hue_backend_theme/models/res_config_settings.py
@@ -0,0 +1,80 @@
+from odoo import models, fields, tools, api
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    background_image = fields.Binary(string="Background Image", attachment=True)
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    theme_background = fields.Binary(string="App menu Background",
+                                     related='company_id.background_image',
+                                     readonly=False)
+
+    app_bar_color = fields.Char(string='Appbar color',
+                                config_parameter='hue_backend_theme.appbar_color',
+                                default='#FFFFFF')
+    primary_accent = fields.Char(string="Navbar color",
+                                 config_parameter='hue_backend_theme.primary_accent_color',
+                                 default='#A53860')
+    # secondary_accent = fields.Char(string="Navbar color",
+    #                                config_parameter='hue_backend_theme.secondary_color',
+    #                                default='#0C4D9D')
+
+    kanban_bg_color = fields.Char(string="Kanban Bg Color",
+                                  config_parameter='hue_backend_theme.kanban_bg_color',
+                                  default='#F7F7F7')
+
+    primary_hover = fields.Char(string="Hover Primary Color",
+                                config_parameter='hue_backend_theme.primary_hover',
+                                default='#953256')
+    light_hover = fields.Char(string="Light Hover",
+                              config_parameter='hue_backend_theme.light_hover',
+                              default='#d5d5d5')
+    appbar_text = fields.Char(string="Home Menu Text Color",
+                              config_parameter='hue_backend_theme.appbar_text',
+                              default='#F7F8F7')
+    secoundary_hover = fields.Char(string="AppBar Hover",
+                                   config_parameter='hue_backend_theme.secoundary_hover',
+                                   default='#06070f')
+
+    @api.model
+    def create(self, values):
+        if 'theme_background' in values:
+            resize_image = tools.image_process(values['theme_background'], (1024, 1024))
+            values['theme_background'] = resize_image
+            return super(ResConfigSettings, self).create(values)
+
+    def config_color_settings(self):
+        colors = {}
+        print("ppp", self.env.user.company_id.background_image)
+        colors['full_bg_img'] = self.env.user.company_id.background_image
+        colors['appbar_color'] = self.env[
+            'ir.config_parameter'].sudo().get_param(
+            'hue_backend_theme.appbar_color')
+        colors['primary_accent'] = self.env[
+            'ir.config_parameter'].sudo().get_param(
+            'hue_backend_theme.primary_accent_color')
+        # colors['secondary_color'] = self.env[
+        #     'ir.config_parameter'].sudo().get_param(
+        #     'hue_backend_theme.secondary_color')
+        colors['kanban_bg_color'] = self.env[
+            'ir.config_parameter'].sudo().get_param(
+            'hue_backend_theme.kanban_bg_color')
+        colors['primary_hover'] = self.env[
+            'ir.config_parameter'].sudo().get_param(
+            'hue_backend_theme.primary_hover')
+        colors['light_hover'] = self.env[
+            'ir.config_parameter'].sudo().get_param(
+            'hue_backend_theme.light_hover')
+        colors['appbar_text'] = self.env[
+            'ir.config_parameter'].sudo().get_param(
+            'hue_backend_theme.appbar_text')
+        colors['secoundary_hover'] = self.env[
+            'ir.config_parameter'].sudo().get_param(
+            'hue_backend_theme.secoundary_hover')
+
+        return colors
