41e21a6b6219e9ba2aad7ca6d11263123a498f4b
Ajmal Cybro <ajmal@cybrosys.in>
2021-12-29 13:00:48 +0530

    [ADD] Initial Commit

---
+++ product_brand_inventory/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2021-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+###################################################################################
+{
+    'name': 'Product Brand in Inventory',
+    'version': '15.0.1.0.0',
+    'category': 'Warehouse',
+    'summary': 'Product Brand in Inventory',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'images': ['static/description/banner.png'],
+    'website': 'https://www.cybrosys.com',
+    'depends': ['stock'],
+    'data': [
+        'views/brand_views.xml',
+        'security/ir.model.access.csv',
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+
+}
---
+++ product_brand_inventory/doc/RELEASE_NOTES.md
@@ -0,0 +1,5 @@
+## Module <product_brand_inventory>
+
+#### 20.09.2021
+#### Version 15.0.1.0.0
+#### ADD
