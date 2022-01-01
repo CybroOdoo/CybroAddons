ae4f140d455339fff0b38498cc8ab8c46c08f5c7
Ajmal Cybro <ajmal@cybrosys.in>
2022-01-01 12:41:34 +0530

    [ADD] Initial Commit 'sale_product_image'

---
+++ sale_product_image/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2021-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    "name": "Sale Order Line Images",
+    "summary": "Order Line Images In Sale and Sale Report",
+    "version": "15.0.1.0.0",
+    "category": 'Sales',
+    "website": "https://www.cybrosys.com",
+    "description": """Order Line Images In Sale and Sale Report, odoo 14, order line images""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    "depends": [
+        'sale_management',
+    ],
+    "data": [
+        'views/sale_order_line_image.xml',
+        'views/res_config_settings.xml',
+        'report/sale_order_report.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ sale_product_image/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <sale_orderline_image>
+
+#### 06.10.2021
+#### Version 15.0.1.0.0
+##### ADD
+- Initial commit for Sale Order Line Images
