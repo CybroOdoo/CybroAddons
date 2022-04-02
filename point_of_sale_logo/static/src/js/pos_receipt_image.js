4bec3125ebeb0445b6714163cb98513bc03f29e3
Ajmal Cybro <ajmal@cybrosys.in>
2022-04-02 12:28:34 +0530

    [ADD] Initial Commit 'point_of_sale_logo'

---
+++ point_of_sale_logo/static/src/js/pos_receipt_image.js
@@ -0,0 +1,25 @@
+odoo.define("point_of_sale_logo.pos_receipt_image", function (require) {
+    "use strict";
+    const Registries = require('point_of_sale.Registries');
+    const OrderReceipt = require('point_of_sale.OrderReceipt');
+    const PosReceiptLogoChrome = (OrderReceipt) =>
+        class extends OrderReceipt {
+            get imageUrl() {
+                if (this.env.pos){
+                    if (this.env.pos.config){
+                            if (this.env.pos.config.image != false){
+                                console.log('hi')
+                                return `/web/image?model=pos.config&field=image&id=${this.env.pos.config_id}&unique=1`;
+
+
+                            }else{
+                                console.log('hello')
+                                return false
+                            }
+                    }
+                }
+            }
+        };
+    Registries.Component.extend(OrderReceipt, PosReceiptLogoChrome);
+    return OrderReceipt;
+});
