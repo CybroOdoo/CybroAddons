eb4aa678b57dd62288b5de986b9315e8df6a40e3
AjmalCybro <ajmal@cybrosys.in>
2022-05-24 16:00:06 +0530

    [FIX] Bug Fixed 'multiple_datepicker_widget'

--- multiple_datepicker_widget/static/src/js/datepicker_widget.js
+++ multiple_datepicker_widget/static/src/js/datepicker_widget.js
@@ -1,6 +1,10 @@
 /** @odoo-module **/
 import basicFields from 'web.basic_fields';
 import fieldRegistry from 'web.field_registry';
+    const field_utils = require('web.field_utils');
+    var time = require('web.time');
+    var translation = require('web.translation');
+    var _t = translation._t;
 
 var FieldDateMultipleDate = basicFields.InputField.extend({
     template: 'FieldDateMultipleDate',
@@ -8,15 +12,44 @@ var FieldDateMultipleDate = basicFields.InputField.extend({
         'click': '_onSelectDateField',
     }),
 
+
     _onSelectDateField: function(ev) {
+      var dateFormat = time.getLangDateFormat();
+      console.log("dateFormat....",dateFormat);
+       if (dateFormat.includes('MMMM')){
+          var dates = dateFormat.toLowerCase()
+          var result = dates.replace(/mmmm/g, 'MM');
+          dateFormat = result
+
+
+      }
+      else if (dateFormat.includes('MMM')) {
+          var dates = dateFormat.toLowerCase()
+          var result = dates.replace(/mmm/g, 'M');
+          dateFormat = result
+
+      }
+      else if(dateFormat.includes('ddd')){
+
+          var dates = dateFormat.toLowerCase()
+          var result = dates.replace(/ddd/g, 'DD');
+          dateFormat = result
+
+      }
+
+     else{
+        dateFormat = dateFormat.toLowerCase()
+     }
         if (this.$input){
             this.$input.datepicker({
                 multidate: true,
+                format: dateFormat,
             }).trigger('focus');
         }
+
     },
 });
-
+console.log("FieldDateMultipleDate",FieldDateMultipleDate)
 fieldRegistry.add('multiple_datepicker', FieldDateMultipleDate);
 return {
     FieldDateMultipleDate: FieldDateMultipleDate
