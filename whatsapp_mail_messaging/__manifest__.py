512c8ecb182877d29616aff00c08832008866ad8
AjmalCybro <ajmal@cybrosys.in>
2022-08-02 10:04:48 +0530

    [FIX] Bug Fixed 'whatsapp_mail_messaging'

--- whatsapp_mail_messaging/__manifest__.py
+++ whatsapp_mail_messaging/__manifest__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Odoo Whatsapp Connector',
-    'version': '15.0.1.0.0',
+    'version': '15.0.1.0.1',
     'category': 'Extra Tools',
     'summary': """Odoo Whatsapp Connector For Sales, Invoice, and Floating button in Website""",
     'description': """Added options for sending Whatsapp messages and Mails in systray bar,sale order, invoices, 
@@ -32,7 +32,7 @@
     'website': "https://www.cybrosys.com",
     'company': 'Cybrosys Techno Solutions',
     'maintainer': 'Cybrosys Techno Solutions',
-    'depends': ['sale', 'account', 'website'],
+    'depends': ['sale_management', 'account', 'website'],
     'data': [
         'views/portal_whatsapp_view.xml',
         'views/sale_order_inherited.xml',
