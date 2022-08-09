c63259c9838f4f6296201d86eac1c7401efdee4a
AjmalCybro <ajmal@cybrosys.in>
2022-08-09 12:35:40 +0530

    [ADD] Aug 09 : Initial Commit 'account_payment_instalment'

---
+++ account_payment_instalment/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2022-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Tintuk Tomin(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+{
+    'name': "Instalment in Payment Terms",
+    'version': '15.0.1.0.0',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'category': 'Accounting',
+    'summary': 'Instalment in Payment Terms"',
+    'description': """
+    New type 'Instalment' in Odoo payment terms, Instalment in Payment Terms,Installment
+    """,
+    'depends': ['account'],
+    'data': [
+                'views/account_payment_term_view.xml',
+            ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
