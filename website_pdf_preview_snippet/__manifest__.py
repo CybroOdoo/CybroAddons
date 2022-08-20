16dfee79c050f8868f356f834e19857938a9ac93
AjmalCybro <ajmal@cybrosys.in>
2022-08-20 14:49:41 +0530

    [ADD] Aug 20 : Initial Commit 'website_pdf_preview_snippet'

---
+++ website_pdf_preview_snippet/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2022-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Prayag K (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+{
+    'name': 'Website PDF Preview Snippet',
+    'version': '15.0.1.0.0',
+    'summary': """Website PDF Preview Snippet""",
+    'description': """Website PDF Preview Snippet""",
+    'category': 'Website',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'depends': ['base', 'website', ],
+    'website': 'https://www.cybrosys.com',
+    'data': [
+        'views/pdf_preview_snippet.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
