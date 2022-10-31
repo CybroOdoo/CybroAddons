6f7a76c6668ace34fc070b98cd59756c4f1d707b
AjmalCybro <ajmal@cybrosys.in>
2022-10-31 11:57:16 +0530

    Oct 31 [ADD] Initial Commit 'purchase_terms'

---
+++ purchase_terms/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2022-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+{
+    'name': 'Purchase Default Terms and Conditions',
+    'version': '15.0.1.0.0',
+    'summary': 'Purchase Default Terms and Conditions',
+    'description': """Purchase Default Terms and Conditions.""",
+    'category': 'Purchase',
+    'author': 'Cybrosys Techno solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'purchase'],
+    'data': [
+        'views/res_config_settings.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
---
+++ purchase_terms/docs/RELEASE_NOTES.md
@@ -0,0 +1,8 @@
+## Module <purchase_terms>
+
+#### 30.07.2022
+#### Version 15.0.1.0.0
+##### ADD
+- Initial Commit for Purchase Terms
+
+
