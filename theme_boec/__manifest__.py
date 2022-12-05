9cc133f5ba4168b3e0e21d150b0a70d00cc7a67d
AjmalCybro <ajmal@cybrosys.in>
2022-12-05 20:09:25 +0530

    Dec 5: [ADD] Initial Commit 'theme_boec'

---
+++ theme_boec/__manifest__.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2022-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Theme Boec',
+    'description': 'Theme Boec is an attractive and modern eCommerce Website theme',
+    'summary': 'Theme Boec is a new kind of Theme. '
+               'The theme is a very user-friendly and is suitable for your eCommerce website with blog.',
+    'category': 'Theme/eCommerce',
+    'version': '16.0.1.0.0',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['website_blog', 'website_sale_wishlist', 'website_sale',
+                'website_sale_comparison', 'website'],
+    'data': [
+        'data/boec_config.xml',
+        'data/boec_config_data.xml',
+        'data/brand_filter.xml',
+        'data/brand_inherit.xml',
+        'data/hot_deals_button.xml',
+        'security/ir.model.access.csv',
+        'views/about.xml',
+        'views/blog.xml',
+        'views/blog_preview.xml',
+        'views/cart.xml',
+        'views/contact_us.xml',
+        'views/footer.xml',
+        'views/header.xml',
+        'views/layouts.xml',
+        'views/pages_top.xml',
+        'views/product_view.xml',
+        'views/shop.xml',
+        'views/sidebar_shop.xml',
+        'views/snippets/banner.xml',
+        'views/snippets/blog_latest.xml',
+        'views/snippets/deal_week.xml',
+        'views/snippets/demo_product.xml',
+        'views/snippets/insta_feed.xml',
+        'views/snippets/product_tab.xml',
+        'views/snippets/product_tab_demo.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            ('replace', 'website_sale/static/src/js/website_sale_utils.js',
+             'theme_boec/static/src/js/sale_utils.js'),
+            "/theme_boec/static/src/css/style.css",
+            "/theme_boec/static/src/css/style.css.map",
+            "/theme_boec/static/src/css/style.scss",
+            "/theme_boec/static/src/css/owl_carousel_min.css",
+            "/theme_boec/static/src/css/owl_theme_default_min.css",
+            "/theme_boec/static/src/js/owl.carousel.js",
+            "/theme_boec/static/src/js/owl.carousel.min.js",
+            "/theme_boec/static/src/js/jquery.countdown.min.js",
+            "/theme_boec/static/src/js/deal_week.js",
+            "/theme_boec/static/src/js/product_tab.js",
+            "/theme_boec/static/src/js/custom.js",
+            "https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,500&amp;family=Montserrat&amp;display=swap"
+
+        ],
+    },
+    'images': [
+        'static/description/banner.png',
+        'static/description/theme_screenshot.png',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ theme_boec/controllers/brand_filter_controller.py
@@ -0,0 +1,242 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2022-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+
+from odoo.addons.website_sale.controllers.main import TableCompute, WebsiteSale
+
+from datetime import datetime
+from werkzeug.exceptions import Forbidden, NotFound
+from odoo import fields, http, SUPERUSER_ID, tools, _
+from odoo.http import request
+from odoo.addons.http_routing.models.ir_http import slug
+from odoo.addons.website.controllers.main import QueryURL
+from odoo.tools import lazy
+from odoo.addons.website.models.ir_http import sitemap_qs2dom
+
+
+class ProductBrand(WebsiteSale):
+
+    def sitemap_shop(env, rule, qs):
+        if not qs or qs.lower() in '/shop':
+            yield {'loc': '/shop'}
+
+        Category = env['product.public.category']
+        dom = sitemap_qs2dom(qs, '/shop/category', Category._rec_name)
+        dom += env['website'].get_current_website().website_domain()
+        for cat in Category.search(dom):
+            loc = '/shop/category/%s' % slug(cat)
+            if not qs or qs.lower() in loc:
+                yield {'loc': loc}
+
+    @http.route([
+        '/shop',
+        '/shop/page/<int:page>',
+        '/shop/category/<model("product.public.category"):category>',
+        '/shop/category/<model("product.public.category"):category>/page/<int:page>',
+        '/shop/brand/<model("product.brand"):brand>'
+    ], type='http', auth="public", website=True, sitemap=sitemap_shop)
+    def shop(self, brand=None, page=0, category=None, search='', min_price=0.0, max_price=0.0, ppg=False, **post):
+        add_qty = int(post.get('add_qty', 1))
+        try:
+            min_price = float(min_price)
+        except ValueError:
+            min_price = 0
+        try:
+            max_price = float(max_price)
+        except ValueError:
+            max_price = 0
+
+        Category = request.env['product.public.category']
+        if category:
+            category = Category.search([('id', '=', int(category))], limit=1)
+            if not category or not category.can_access_from_current_website():
+                raise NotFound()
+        else:
+            category = Category
+
+        website = request.env['website'].get_current_website()
+        if ppg:
+            try:
+                ppg = int(ppg)
+                post['ppg'] = ppg
+            except ValueError:
+                ppg = False
+        if not ppg:
+            ppg = website.shop_ppg or 20
+
+        ppr = website.shop_ppr or 4
+
+        attrib_list = request.httprequest.args.getlist('attrib')
+        attrib_values = [[int(x) for x in v.split("-")] for v in attrib_list if v]
+        attributes_ids = {v[0] for v in attrib_values}
+        attrib_set = {v[1] for v in attrib_values}
+
+        if not brand:
+            keep = QueryURL('/shop',
+                            **self._shop_get_query_url_kwargs(category and int(category), search, min_price, max_price,
+                                                              **post))
+        else:
+            keep = QueryURL('shop/brand')
+
+        now = datetime.timestamp(datetime.now())
+        pricelist = request.env['product.pricelist'].browse(request.session.get('website_sale_current_pl'))
+        if not pricelist or request.session.get('website_sale_pricelist_time',
+                                                0) < now - 60 * 60:  # test: 1 hour in session
+            pricelist = website.get_current_pricelist()
+            request.session['website_sale_pricelist_time'] = now
+            request.session['website_sale_current_pl'] = pricelist.id
+
+        request.update_context(pricelist=pricelist.id, partner=request.env.user.partner_id)
+
+        filter_by_price_enabled = website.is_view_active('website_sale.filter_products_price')
+        if filter_by_price_enabled:
+            company_currency = website.company_id.currency_id
+            conversion_rate = request.env['res.currency']._get_conversion_rate(
+                company_currency, pricelist.currency_id, request.website.company_id, fields.Date.today())
+        else:
+            conversion_rate = 1
+
+        url = "/shop"
+        if search:
+            post["search"] = search
+        if attrib_list:
+            post['attrib'] = attrib_list
+
+        options = self._get_search_options(
+            category=category,
+            attrib_values=attrib_values,
+            pricelist=pricelist,
+            min_price=min_price,
+            max_price=max_price,
+            conversion_rate=conversion_rate,
+            **post
+        )
+        fuzzy_search_term, product_count, search_product = self._shop_lookup_products(attrib_set, options, post, search,
+                                                                                      website)
+
+        filter_by_price_enabled = website.is_view_active('website_sale.filter_products_price')
+        if filter_by_price_enabled:
+            # TODO Find an alternative way to obtain the domain through the search metadata.
+            Product = request.env['product.template'].with_context(bin_size=True)
+            domain = self._get_search_domain(search, category, attrib_values)
+
+            # This is ~4 times more efficient than a search for the cheapest and most expensive products
+            from_clause, where_clause, where_params = Product._where_calc(domain).get_sql()
+            query = f"""
+                SELECT COALESCE(MIN(list_price), 0) * {conversion_rate}, COALESCE(MAX(list_price), 0) * {conversion_rate}
+                  FROM {from_clause}
+                 WHERE {where_clause}
+            """
+            request.env.cr.execute(query, where_params)
+            available_min_price, available_max_price = request.env.cr.fetchone()
+
+            if min_price or max_price:
+                # The if/else condition in the min_price / max_price value assignment
+                # tackles the case where we switch to a list of products with different
+                # available min / max prices than the ones set in the previous page.
+                # In order to have logical results and not yield empty product lists, the
+                # price filter is set to their respective available prices when the specified
+                # min exceeds the max, and / or the specified max is lower than the available min.
+                if min_price:
+                    min_price = min_price if min_price <= available_max_price else available_min_price
+                    post['min_price'] = min_price
+                if max_price:
+                    max_price = max_price if max_price >= available_min_price else available_max_price
+                    post['max_price'] = max_price
+
+        website_domain = website.website_domain()
+        categs_domain = [('parent_id', '=', False)] + website_domain
+        if search:
+            search_categories = Category.search(
+                [('product_tmpl_ids', 'in', search_product.ids)] + website_domain
+            ).parents_and_self
+            categs_domain.append(('id', 'in', search_categories.ids))
+        else:
+            search_categories = Category
+        categs = lazy(lambda: Category.search(categs_domain))
+
+        if category:
+            url = "/shop/category/%s" % slug(category)
+
+        pager = website.pager(url=url, total=product_count, page=page, step=ppg, scope=7, url_args=post)
+        offset = pager['offset']
+        if not brand:
+            products = search_product[offset:offset + ppg]
+        else:
+            products = request.env['product.template'].sudo().search([('brand_id', '=', brand.id),
+                                                                      ('website_published', '=', True)],
+                                                                     order="id desc",
+                                                                     offset=pager['offset'])
+
+        ProductAttribute = request.env['product.attribute']
+        if products:
+            # get all products without limit
+            attributes = lazy(lambda: ProductAttribute.search([
+                ('product_tmpl_ids', 'in', search_product.ids),
+                ('visibility', '=', 'visible'),
+            ]))
+        else:
+            attributes = lazy(lambda: ProductAttribute.browse(attributes_ids))
+
+        layout_mode = request.session.get('website_sale_shop_layout_mode')
+        if not layout_mode:
+            if website.viewref('website_sale.products_list_view').active:
+                layout_mode = 'list'
+            else:
+                layout_mode = 'grid'
+            request.session['website_sale_shop_layout_mode'] = layout_mode
+
+        products_prices = lazy(lambda: products._get_sales_prices(pricelist))
+
+        values = {
+            'search': fuzzy_search_term or search,
+            'original_search': fuzzy_search_term and search,
+            'order': post.get('order', ''),
+            'category': category,
+            'attrib_values': attrib_values,
+            'attrib_set': attrib_set,
+            'pager': pager,
+            'pricelist': pricelist,
+            'add_qty': add_qty,
+            'products': products,
+            'search_count': product_count,  # common for all searchbox
+            'bins': lazy(lambda: TableCompute().process(products, ppg, ppr)),
+            'ppg': ppg,
+            'ppr': ppr,
+            'categories': categs,
+            'attributes': attributes,
+            'keep': keep,
+            'search_categories_ids': search_categories.ids,
+            'layout_mode': layout_mode,
+            'products_prices': products_prices,
+            'get_product_prices': lambda product: lazy(lambda: products_prices[product.id]),
+            'float_round': tools.float_round,
+        }
+        if filter_by_price_enabled:
+            values['min_price'] = min_price or available_min_price
+            values['max_price'] = max_price or available_max_price
+            values['available_min_price'] = tools.float_round(available_min_price, 2)
+            values['available_max_price'] = tools.float_round(available_max_price, 2)
+        if category:
+            values['main_object'] = category
+        values.update(self._get_additional_shop_values(values))
+        return request.render("website_sale.products", values)
