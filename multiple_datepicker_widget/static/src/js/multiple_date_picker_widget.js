bbab87a010c67280b3f6117793245113be1a053a
AjmalCybro <ajmal@cybrosys.in>
2022-12-14 18:12:41 +0530

    Dec 14 : [ADD] Initial Commit 'multiple_datepicker_widget'

---
+++ multiple_datepicker_widget/static/src/js/multiple_date_picker_widget.js
@@ -0,0 +1,53 @@
+/** @odoo-module **/
+import { registry } from "@web/core/registry";
+import { useInputField } from "@web/views/fields/input_field_hook";
+import time from 'web.time';
+var translation = require('web.translation');
+var _t = translation._t;
+
+const { Component,useRef} = owl;
+
+export class DomainSelectorTextField extends Component {
+    static template = 'FieldDateMultipleDate'
+
+    setup(){
+        super.setup();
+        this.input = useRef('inputdate')
+        useInputField({ getValue: () => this.props.value || "", refName: "inputdate" });
+    }
+    _onSelectDateField(ev){
+        var dateFormat = time.getLangDateFormat();
+        console.log("dateFormat....",dateFormat);
+       if (dateFormat.includes('MMMM')){
+          var dates = dateFormat.toLowerCase()
+          var result = dates.replace(/mmmm/g, 'MM');
+          console.log("Result..............",result)
+          dateFormat = result
+      }
+      else if (dateFormat.includes('MMM')) {
+          var dates = dateFormat.toLowerCase()
+          var result = dates.replace(/mmm/g, 'M');
+          dateFormat = result
+          console('date',result)
+
+      }
+      else if(dateFormat.includes('ddd')){
+          var dates =new dateFormat.toLowerCase()
+          var result = new dates.replace(/ddd/g, 'DD');
+          console.log("Result..............",result)
+          dateFormat = result
+      }
+     else{
+        dateFormat = dateFormat.toLowerCase()
+     }
+        if (this.input.el){
+           this.props.update(this.input.el.value.replace(DomainSelectorTextField, ''));
+            console.log('this',dateFormat)
+            $(this.input.el).datepicker({
+                multidate: true,
+                format: dateFormat,
+            }).trigger('focus');
+        }
+    }
+}
+registry.category("fields").add("multiple_datepicker", DomainSelectorTextField);
