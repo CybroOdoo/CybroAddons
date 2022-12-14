cda1d494e0b795d13089c2a3079b61355454994b
AjmalCybro <ajmal@cybrosys.in>
2022-12-14 17:40:00 +0530

    Dec 14 : [ADD] Initial Commit 'pos_category_wise_receipt'

---
+++ pos_category_wise_receipt/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2009-TODAY Cybrosys Technologies(<http://www.cybrosys.com>).
+#    Author: Nilmar Shereef(<http://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'POS Category wise receipt',
+    'version': '16.0.1.0.0',
+    'summary': 'Category wise receipt for the Point of Sale ',
+    'author': 'Cybrosys Techno Solutions',
+    'website': "http://www.cybrosys.com",
+    'company': 'Cybrosys Techno Solutions',
+    'category': 'Point of Sale',
+    'depends': ['point_of_sale'],
+
+    'assets': {
+            'point_of_sale.assets': [
+                'pos_category_wise_receipt/static/src/js/pos_receipt.js',
+                'pos_category_wise_receipt/static/src/xml/pos_receipt.xml'
+            ]
+    },
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
---
+++ pos_category_wise_receipt/static/src/js/pos_receipt.js
@@ -0,0 +1,31 @@
+odoo.define('pos_category_wise_receipt.receipt', function(require) {
+    'use strict';
+
+//    var models =  require('point_of_sale.models');
+    const OrderReceipt =require('point_of_sale.OrderReceipt');
+    const Registries = require('point_of_sale.Registries');
+
+   const CategoryOrderReceipt = OrderReceipt =>
+        class extends OrderReceipt {
+
+        get orderlines() {
+
+            var order_lines = this.receiptEnv.orderlines;
+
+            var categ = {
+                'category': [],
+                'orderlines': order_lines
+            }
+
+            for (var i = 0; i <= order_lines.length - 1; i++){
+                if(!categ.category.includes(order_lines[i].product.pos_categ_id[1])){
+                    categ.category.push(order_lines[i].product.pos_categ_id[1]);
+                }
+            }
+            console.log('hloo', categ)
+            return categ;
+        }
+     }
+     Registries.Component.extend(OrderReceipt, CategoryOrderReceipt);
+     return OrderReceipt;
+   });
