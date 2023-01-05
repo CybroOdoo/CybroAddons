e04159fff54daf93603c71461b98dd299f4e7dee
AjmalCybro <ajmal@cybrosys.in>
2023-01-05 14:32:57 +0530

    Jan 05 : Updated 'laundry_management'

--- laundry_management/models/laundry.py
+++ laundry_management/models/laundry.py
@@ -196,9 +196,10 @@ class LaundryManagement(models.Model):
                                               'draft': [('readonly', False)],
                                               'order': [('readonly', False)]},
                                           help="Delivery address for current sales order.")
-    order_date = fields.Datetime(string="Date",
-                                 default=datetime.now().strftime(
-                                     '%Y-%m-%d %H:%M:%S'))
+    order_date = fields.Datetime(string='Date', readonly=True, index=True,
+                                 states={'draft': [('readonly', False)],
+                                         'order': [('readonly', False)]},
+                                 copy=False, default=fields.Datetime.now, )
     laundry_person = fields.Many2one('res.users', string='Laundry Person',
                                      required=1)
     order_lines = fields.One2many('laundry.order.line', 'laundry_obj',
