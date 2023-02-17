6b5085fe6737ac8bdf4bfd5384f0eac8e2e0cf6e
AjmalCybro <ajmal@cybrosys.in>
2023-02-17 09:19:19 +0530

    Feb 17 [UPDT] : Bug Fixed 'inventory_report_generator'

--- inventory_report_generator/__manifest__.py
+++ inventory_report_generator/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Inventory All In One Report Generator',
-    'version': '14.0.1.0.0',
+    'version': '14.0.1.0.1',
     'summary': "Dynamic Inventory Report Generator",
     'description': "Dynamic Inventory Report Generator",
     'category': 'Inventory',
