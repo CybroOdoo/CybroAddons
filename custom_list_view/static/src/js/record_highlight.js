ca7d124dc5caf1c0948398748212dcdf054fc6fb
AjmalCybro <ajmal@cybrosys.in>
2023-03-13 11:09:07 +0530

    Mar 13 : [FIX] Bug Fixed 'custom_list_view'

--- custom_list_view/static/src/js/record_highlight.js
+++ custom_list_view/static/src/js/record_highlight.js
@@ -4,11 +4,18 @@ import { ListRenderer } from "@web/views/list/list_renderer";
 import { patch } from "@web/core/utils/patch";
 import { browser } from "@web/core/browser/browser";
 import { useService } from "@web/core/utils/hooks";
+const {onMounted} = owl;
 
 patch(ListRenderer.prototype, 'custom_list_view/static/src/js/record_highlight.js', {
       setup(){
          this._super(...arguments)
+        onMounted(() => {
+        var list = $('.o_section_and_note_list_view thead').find('tr')
+        var elem ='<th style="min-width: 33px; width: 67px;">Sl No</th>'
+        list.prepend(elem)
+        });
       },
+
       toggleRecordSelection(record) {
         var self = this;
         this._super.apply(this, arguments);
