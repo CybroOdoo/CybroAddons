3bccb1e11897a1891e9edf5bf28dc24a32ab88b9
AjmalCybro <ajmal@cybrosys.in>
2023-03-13 16:22:15 +0530

    Mar 13 : [ADD] Initial Commit 'pos_magnify_image'

---
+++ pos_magnify_image/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2021-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Aswani PC (aswani@cybrosys.in)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+################################################################################
+
+{
+    'name': "Pos Product Magnify Image",
+    'version': '16.0.1.0.0',
+    'summary': """Magnify product image in PoS""",
+    'description': """Magnify Product Image in Point of Sale""",
+    'author': "Cybrosys Techno Solutions",
+    'company': "Cybrosys Techno Solutions",
+    'website': "http://www.cybrosys.com",
+    'category': 'Point of Sale',
+    'depends': ['point_of_sale', 'web'],
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_magnify_image/static/src/js/MagnifyProductPopup.js',
+            'pos_magnify_image/static/src/js/ProductScreen.js',
+            'pos_magnify_image/static/src/css/pos_magnify_image.css',
+            'pos_magnify_image/static/src/xml/MagnifyProductPopup.xml',
+            'pos_magnify_image/static/src/xml/ProductItem.xml'
+        ],
+    },
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
---
+++ pos_magnify_image/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <pos_magnify_image>
+
+#### 13.12.2022
+#### Version 16.0.1.0.0
+#### ADD
+
+- Initial commit for Pos Product Magnify Image
