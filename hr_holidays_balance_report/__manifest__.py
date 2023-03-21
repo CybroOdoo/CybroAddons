42209b904bddadc0d293abd2d9b6a9630bcfdf8b
Sayooj <odoo@cybrosys.com>
2023-03-21 16:17:27 +0530

    Mar 21 : [ADD] Initial Commit 'hr_holidays_balance_report'

---
+++ hr_holidays_balance_report/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+{
+    'name': 'HR Balance Leave Report',
+    'version': '16.0.1.0.0',
+    'category': 'Human Resources/Time Off',
+    'summary': 'Allocated balance, taken leaves and remaining balance per leave type for each employee',
+    'description': 'User Can view Allocated balance, taken leaves and remaining balance per leave type for '
+                   'each employee',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['hr', 'hr_holidays'],
+    'data': [
+        'security/balance_report_security.xml',
+        'security/ir.model.access.csv',
+        'report/leave_balance_report_view.xml'
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ hr_holidays_balance_report/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <hr_holidays_balance_report>
+
+#### 22.02.2023
+#### Version 16.0.1.0.0
+#### ADD
+- Initial Commit for HR Balance Leave Report
