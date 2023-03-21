76c7cccf1d2fea99a444efcf1c7e00d0422a85bc
Sayooj <odoo@cybrosys.com>
2023-03-21 16:19:54 +0530

    Mar 21 : [ADD] Initial Commit 'model_access_rights'

---
+++ model_access_rights/__manifest__.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Model Access Rights',
+    'version': '16.0.1.0.0',
+    'category': 'Others',
+    'summary': """ Can hide options from user """,
+    'description': """Can hide options according to specific model
+                    and access group """,
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': [
+        'base_setup',
+        'mail',
+    ],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'data/ir_sequence_data.xml',
+        'views/model_access_rights_views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'model_access_rights/static/src/js/form_controller.js',
+            'model_access_rights/static/src/js/list_controller.js',
+        ]
+    },
+
+    'images': ['static/description/banner.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
