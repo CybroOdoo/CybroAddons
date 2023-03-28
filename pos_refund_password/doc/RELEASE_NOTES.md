f10576f915e478d6bdda693a9d923fb2e9bdb3d9
AjmalCybro <ajmal@cybrosys.in>
2023-03-28 16:54:14 +0530

    Mar 28 : [UPDT] Bug Fixed 'pos_refund_password'

--- pos_refund_password/doc/RELEASE_NOTES.md
+++ pos_refund_password/doc/RELEASE_NOTES.md
@@ -4,3 +4,7 @@
 #### Version 16.0.1.0.0
 #### ADD
 - Initial commit for pos_refund_password odoo
+#### 28.03.2023
+#### Version 16.0.1.0.1
+#### FIX
+- Fixed the undeclared body 
--- pos_refund_password/model/refund_password.py
+++ pos_refund_password/model/refund_password.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class PosConfig(models.Model):
@@ -15,3 +15,4 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     global_refund_security = fields.Integer(string='Global Refund Security',config_parameter='pos_refund_password.global_refund_security')
+
