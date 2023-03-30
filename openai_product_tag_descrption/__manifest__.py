a8e572c7817ba1f8dea29ce0999bf30cf453bb87
Sayooj <odoo@cybrosys.com>
2023-03-30 17:00:43 +0530

    Mar 30 : [ADD] Initial Commit 'openai_product_tag_description'

---
+++ openai_product_tag_descrption/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding:utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Ranjith R (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+{
+    'name': 'Generate Product Description/Tags Using OpenAI',
+    'version': '16.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Automate Product Tag Creation and Description Generation with My Module in Odoo Using OpenAI API',
+    'description': 'Automate Product Tag Creation and Description Generation with My Module in Odoo Using OpenAI API',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'images': ['static/description/banner.png,static/description/icon.png'],
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'product', 'openai_odoo_base', 'stock'],
+    'data': [
+        'views/product_actions.xml'
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ openai_product_tag_descrption/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <openai_product_tag_or_description_creation>
+
+#### 17.03.2023
+#### Version 16.0.1.0.0
+#### ADD
+- Initial commit for OpenAI Product Tag Description
+
