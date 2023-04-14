ddbb4880ef980ff3e24486cb12e65354af59dd9f
Sayooj <odoo@cybrosys.com>
2023-04-14 11:46:59 +0530

    Apr 14 : [ADD] Initial Commit 'product_stock_details'

---
+++ product_stock_details/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <product_stock_details>
+
+#### 03.03.2023
+#### Version 16.0.1.0.0
+##### ADD
+- Initial Commit for Product Stock Details
---
+++ product_stock_details/models/product.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+from odoo import fields, models
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    product_stock_location_ids = fields.One2many('stock.quant', 'product_id')
+
+    def get_wo_description(self):
+        """Method for print button"""
+        return self.env.ref(
+            'product_stock_details.action_report_product_stock_details').report_action(
+            self, data='')
