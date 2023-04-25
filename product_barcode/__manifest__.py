3ab2cdeae2b74ed03350e73a8935855d63ac5d2f
AjmalCybro <ajmal@cybrosys.in>
2023-04-25 12:23:09 +0530

    Apr 25 [UPDT] : Bug Fixed 'product_barcode'

--- product_barcode/__manifest__.py
+++ product_barcode/__manifest__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Product Barcode Generator',
-    'version': '16.0.1.0.0',
+    'version': '16.0.1.0.1',
     'summary': 'Generates EAN13 Standard Barcode for Product.',
     'live_test_url': 'https://www.youtube.com/watch?v=0BrFcOEkWu4&feature=youtu.be',
     'category': 'Inventory',
