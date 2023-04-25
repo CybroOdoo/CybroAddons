8fb30bb0e37bccea35d3858c3e35b83654f418b9
Sayooj <cybroodoo@gmail.com>
2023-04-25 10:04:28 +0530

    Apr 25 : [ADD] Initial Commit 'call_for_price_website'

---
+++ call_for_price_website/doc/RELEASE_NOTES.md
@@ -0,0 +1,11 @@
+## Module <call_for_price_website>
+
+#### 15.03.2023
+#### Version 16.0.1.0.0
+#### ADD
+Initial Commit for Website Call For Price
+
+
+
+
+
---
+++ call_for_price_website/static/src/js/create_call_form.js
@@ -0,0 +1,26 @@
+/**
+     * This file is used to give a success alert after requesting a call for price.
+*/
+
+odoo.define('call_for_price_website.create', function (require) {
+"use strict";
+    const rpc = require('web.rpc');
+    var core = require('web.core');
+
+    $('#send_btn').on('click', function(){
+          var first = $('#first_name').val();
+          var last = $('#last_name').val();
+          var product_id = $('#product_id').val();
+          var phone = $('#phone').val();
+          var email = $('#email').val();
+          var message = $('#message').val();
+          var qty = $('#quantity').val();
+          rpc.query({
+                model: "call.price",
+                method: "create_form",
+                args:[first,last,product_id,phone,email,message,qty]
+                }).then(function (result) {
+                    document.getElementById('alert_message').style.display = "block"
+                });
+    });
+});
