4787fd3c9c002fdc7897e5ef006e4de4990cd27b
Sayooj <cybroodoo@gmail.com>
2023-05-03 12:29:14 +0530

    May 03 : [ADD] Initial Commit 'index_and_follow'

---
+++ index_and_follow/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <index_and_follow>
+
+#### 18.02.2023
+#### Version 16.0.1.0.0
+#### ADD
+
+- Initial commit for Website Index and Follow
---
+++ index_and_follow/static/src/js/website.js
@@ -0,0 +1,41 @@
+odoo.define('index_and_follow.index', function (require) {
+'use strict';
+
+    var ajax = require('web.ajax');
+    var publicWidget = require('web.public.widget');
+
+    publicWidget.registry.IndexAndFollow = publicWidget.Widget.extend({
+         selector: '#product_detail_main',
+         events: {
+             'change .is_index': '_setProductIndex',
+        },
+        _setProductIndex: function(event){
+            //function to set product index
+            var product = this.el.querySelector('.product').value;
+            if (event.target.checked == true){
+                /*check index input is checked, if input is checked, it sends an AJAX request
+                 to the server to set the product's indexing status to true.*/
+                this.el.querySelector('.is_index').checked = true;
+                ajax.jsonRpc('/web_index', 'call',{
+                    'index': true,
+                    'product': product,
+                }).then(function(){
+                     location.reload();
+                });
+            }
+            else{
+                 //input is unchecked, it sends an AJAX request to set the status to false.
+                 ajax.jsonRpc('/web_index', 'call',{
+                    'index': false,
+                     'product': product,
+                 }).then(function(){
+                     location.reload();
+                });
+            }
+
+        },
+    });
+
+    return publicWidget.registry.IndexAndFollow;
+
+  });
