cbcb3b6c288e545354dd21a0be8d16c14e7d59cf
Sayooj <cybroodoo@gmail.com>
2023-05-03 12:30:51 +0530

    May 03 : [ADD] Initial Commit 'picking_line_description'

---
+++ picking_line_description/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Ammu (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+{
+    'name': 'Picking Line Description(Shipment/Delivery)',
+    'version': '16.0.1.0.0',
+    'summary': 'To add product description to Pickings.',
+    'category': 'Inventory/Delivery',
+    'description': 'To view the Sale/Purchase order line Description to Shipments/Delivery slips.',
+    'author': 'Cybrosys Techno solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'license': 'AGPL-3',
+    'depends': ['stock'],
+    'data': [
+        'report/stock_picking_report.xml',
+        'views/stock_picking_views.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ picking_line_description/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <picking_line_description>
+
+#### 10.03.2023
+#### Version 16.0.1.0.0
+#### ADD
+- Initial commit for Picking Line Description(Shipment/Delivery)
