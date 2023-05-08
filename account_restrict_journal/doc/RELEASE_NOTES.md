7714e7fd6c3c0450ad65068e7403d7430d386647
Sayooj <cybroodoo@gmail.com>
2023-05-08 17:31:55 +0530

    May 08 : [ADD] Initial Commit 'account_restrict_journal'

---
+++ account_restrict_journal/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <account_restrict_journal>
+
+#### 05.05.2023
+#### Version 16.0.1.0.0
+#### ADD
+- Initial commit for Restrict Journal for Users
---
+++ account_restrict_journal/models/res_users.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+from odoo import fields, models
+
+
+class ResUsers(models.Model):
+    """ Adding journal fields where we can select allowed journal """
+
+    _inherit = 'res.users'
+
+    journal_ids = fields.Many2many(
+        'account.journal',
+        string='Allowed Journals',
+        help='Only the selected journal will be visible'
+             ' to the particular user')
