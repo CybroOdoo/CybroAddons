4ec87def3213f527018b71da759276a4189e81b3
Sayooj <cybroodoo@gmail.com>
2023-05-17 12:32:49 +0530

    May 17 : [ADD] Initial Commit 'contact_documents'

---
+++ contact_documents/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# ###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+# Copyright (C) 2023-TODAY Cybrosys Technologies(
+# <https://www.cybrosys.com>). Author: Albin P J (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+# ###############################################################################
+{
+    'name': 'Partner Related Documents',
+    'version': '16.0.1.0.0',
+    'category': 'Document Management',
+    'summary': """ Store and access documents for the related contact""",
+    'description': """ Store and access documents for the related contact, 
+    you can see the documents in the contact section""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['base', 'contacts'],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+
+}
---
+++ contact_documents/doc/RELEASE_NOTES.md
@@ -0,0 +1,9 @@
+## Module <contact_documents>
+
+#### 13.02.2023
+#### Version 16.0.1.0.0
+#### ADD
+- Initial commit for Partner Related Documents
+
+
+
