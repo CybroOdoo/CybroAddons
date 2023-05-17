d231f2ade3f9e33d88265e702c0954b62f886874
Sayooj <cybroodoo@gmail.com>
2023-05-17 12:15:55 +0530

    May 17 : [ADD] Initial Commit 'product_discount_limit'

---
+++ product_discount_limit/doc/RELEASE_NOTES.md
@@ -0,0 +1,8 @@
+## Module <product_discount_limit>
+
+#### 09.03.2023
+#### Version 16.0.1.0.0
+#### ADD
+
+- Initial commit for Product Discount Limit 
+
---
+++ product_discount_limit/models/res_users.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: JANISH BABU EK (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+from odoo import fields, models
+
+"""Inherited res.users to add field to allow discount over limit"""
+
+
+class ResUser(models.Model):
+    _inherit = 'res.users'
+
+    allow_discount = fields.Boolean("Allow Discount Over Limit",
+                                    help="""If it is True,
+                                     discount Limit will 
+                                     not affect the user""")
