b37efa42c3d61f96993d1089df2c93b39c72ec96
Sayooj <cybroodoo@gmail.com>
2023-05-17 12:08:11 +0530

    May 17 : [ADD] Initial Commit 'login_to_checkout'

---
+++ login_to_checkout/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Renu M (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+{
+    'name': 'Verify Before Checkout',
+    'version': '16.0.1.0.0',
+    'summary': 'Verify the user before checkout of the product',
+    'description': """The module helps you to verify the user before 
+    CheckOut of the product if the user is not logged in""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'category': 'Extra Tools',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'website_sale', 'portal'],
+    'data': [
+        'views/portal_templates.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'AGPL-3',
+}
---
+++ login_to_checkout/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <login_to_checkout>
+
+#### 13.02.2023
+#### Version 16.0.1.0.0
+#### ADD
+
+- Initial commit for Verify Before Checkout
