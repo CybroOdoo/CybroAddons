5cbc13f8be462d9065764289cfb1085d0c42f417
Sayooj <cybroodoo@gmail.com>
2023-05-23 10:50:43 +0530

    May 23 : [ADD] Initial Commit 'product_wise_shipping_method'

---
+++ product_wise_shipping_method/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Product Wise Shipping Method',
+    'version': '16.0.1.0.0',
+    'category': 'Sales/Sales',
+    'summary': """ Product Wise Shipping Method for sale order. """,
+    'description': """ This module helps to provide shipping method easily via choosing products in order line. """,
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'depends': ['base', 'sale','delivery'],
+    'website': 'https://www.cybrosys.com',
+    'data': [
+        'views/product_template_views.xml',
+        'views/sale_order_view.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ product_wise_shipping_method/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <product_wise_shipping_method>
+
+#### 13.04.2023
+#### Version 16.0.1.0.0
+##### ADD
+- Initial commit for Product Wise Shipping Method
