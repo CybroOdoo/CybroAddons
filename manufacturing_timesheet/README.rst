009cf2ab00ea9be5274dfca14fe74c4e1e20b88f
AjmalCybro <ajmal@cybrosys.in>
2023-06-02 17:17:48 +0530

    Jun 02 [ADD]: Initial Commit 'manufacturing_timesheet'

---
+++ manufacturing_timesheet/README.rst
@@ -0,0 +1,46 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+Manufacturing (MRP) Timesheet
+=============================
+This module will help you to create timesheet for manufacturing orders according to the work order time tracking.
+
+License
+-------
+Affero General Public License v3.0 (AGPL v3)
+(https://www.odoo.com/documentation/16.0/legal/licenses.html)
+
+Configuration
+=============
+* No additional configurations needed
+
+Company
+-------
+* `Cybrosys Techno Solutions <https://cybrosys.com/>`__
+
+Credits
+-------
+Developer : (V16) Amaya Aravind @ Cybrosys
+
+Contacts
+--------
+* Mail Contact : odoo@cybrosys.com
+* Website : https://cybrosys.com
+
+Bug Tracker
+-----------
+Bugs are tracked on GitHub Issues. In case of trouble, please check there if your issue has already been reported.
+
+Maintainer
+==========
+.. image:: https://cybrosys.com/images/logo.png
+   :target: https://cybrosys.com
+
+This module is maintained by Cybrosys Technologies.
+
+For support and more information, please visit `Our Website <https://cybrosys.com/>`__
+
+Further information
+===================
+HTML Description: `<static/description/index.html>`__
---
+++ manufacturing_timesheet/models/mrp_workorder.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Cybrosys Technologies(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+from datetime import datetime
+from odoo import fields, models
+
+
+class MrpWorkorder(models.Model):
+    """Inherited model mrp_workorder to add field and functions related to
+       manufacturing timesheet.
+
+        Methods:
+            button_start(self):
+                Supering the function of start button to start the time of
+                timesheet.
+            button_pending(self):
+                Supering the function of pause button to set timesheet in
+                progress state.
+            button_finish(self):
+                Supering the function of done button to calculate total
+                time in timesheet.
+    """
+    _inherit = 'mrp.workorder'
+
+    employee_id = fields.Many2one('hr.employee', string='Employee',
+                                  readonly=False, required=True,
+                                  help='Employee in work order', store=True)
+
+    def button_start(self):
+        """ Supering the function of start button to start the timer
+            of timesheet.
+
+            Boolean: Returns true
+        """
+        res = super(MrpWorkorder, self).button_start()
+
+        project = self.env['project.project'].search(
+            [('name', '=', ("MO: {}".format(self.production_id.name)))])
+
+        if project:
+            task_id = project.task_ids.search([('name', '=', (
+                "{} in {} for {} on {}".format(self.name,
+                                               self.workcenter_id.name,
+                                               self.product_id.display_name,
+                                               str(self.date_planned_start))))])
+            if not task_id:
+                task_id = self.env['project.task'].create({
+                    'name': ("{} in {} for {} on {}".format(self.name,
+                                                            self.workcenter_id.name,
+                                                            self.product_id.display_name,
+                                                            str(self.date_planned_start))),
+                    'project_id': project.id,
+                    'date_assign': self.date_planned_start,
+                    'date_deadline': self.date_planned_finished,
+                    'planned_hours': self.duration_expected,
+                })
+                self.env['account.analytic.line'].create({
+                    'task_id': task_id.id,
+                    'date': datetime.today(),
+                    'name': ("{} in {} for {}".format(self.name,
+                                                      self.workcenter_id.name,
+                                                      self.product_id.display_name)),
+                    'employee_id': self.employee_id.id,
+                    'is_manufacturing': True
+                })
+        else:
+            project_id = self.env['project.project'].create(
+                {'name': ("MO: {}".format(self.production_id.name)),
+                 'is_manufacturing': True})
+            task_id = project_id.task_ids.search([('name', '=', (
+                "{} in {} for {} on {}".format(self.name,
+                                               self.workcenter_id.name,
+                                               self.product_id.display_name,
+                                               str(self.date_planned_start))))])
+            if not task_id:
+                task_id = self.env['project.task'].create({
+                    'name': ("{} in {} for {} on {}".format(self.name,
+                                                            self.workcenter_id.name,
+                                                            self.product_id.display_name,
+                                                            str(self.date_planned_start))),
+                    'project_id': project_id.id,
+                    'date_assign': self.date_planned_start,
+                    'date_deadline': self.date_planned_finished,
+                    'planned_hours': self.duration_expected,
+                })
+                self.env['account.analytic.line'].create({
+                    'task_id': task_id.id,
+                    'date': datetime.today(),
+                    'name': ("{} in {} for {}".format(self.name,
+                                                      self.workcenter_id.name,
+                                                      self.product_id.display_name)),
+                    'employee_id': self.employee_id.id,
+                    'is_manufacturing': True
+                })
+        return res
+
+    def button_pending(self):
+        """ Supering the function of pause button to set timesheet in
+            progress state.
+
+            Boolean: Returns true
+        """
+        res = super(MrpWorkorder, self).button_pending()
+
+        project = self.env['project.project'].search(
+            [('name', '=', ("MO: {}".format(self.production_id.name)))])
+        task_id = project.task_ids.search([('name', '=', (
+            "{} in {} for {} on {}".format(self.name, self.workcenter_id.name,
+                                           self.product_id.display_name,
+                                           str(self.date_planned_start))))])
+        task_id.write({
+            'planned_hours': self.duration_expected
+        })
+        timesheet = task_id.mapped('timesheet_ids')
+        for rec in timesheet:
+            rec.write({
+                'unit_amount': self.duration,
+            })
+        return res
+
+    def button_finish(self):
+        """ Supering the function of done button to calculate total time in
+            timesheet.
+
+            Boolean: Returns true
+        """
+        res = super(MrpWorkorder, self).button_finish()
+
+        project = self.env['project.project'].search(
+            [('name', '=', ("MO: {}".format(self.production_id.name)))])
+        task_id = project.task_ids.search([('name', '=', (
+            "{} in {} for {} on {}".format(self.name, self.workcenter_id.name,
+                                           self.product_id.display_name,
+                                           str(self.date_planned_start))))])
+        task_id.write({
+            'planned_hours': self.duration_expected
+        })
+        timesheet = task_id.mapped('timesheet_ids')
+        for rec in timesheet:
+            rec.write({
+                'unit_amount': self.duration,
+            })
+        return res
