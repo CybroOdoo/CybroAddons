3e6159bdf9c0265805887b57b066a3d3d5c4e6e1
AjmalCybro <ajmal@cybrosys.in>
2023-06-20 17:57:01 +0530

    Jun 20 : [ADD] Initial Commit 'employee_less_working_hour_notification'

---
+++ employee_less_working_hour_notification/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <employee_less_working_hour_notification>
+
+#### 04.04.2023
+#### Version 16.0.1.0.0
+#### ADD
+
+- Initial commit for Employee Less Working Hour Notification
---
+++ employee_less_working_hour_notification/models/res_config_settings.py
@@ -0,0 +1,40 @@
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Unnimaya C O (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    """"Inherited model for adding extra fields"""
+    _inherit = 'res.config.settings'
+
+    minimum_working_hour = fields.Float(string='Minimum Working Hours',
+                                        help=" Set minimum working hours/day"
+                                             " to notify in Email",
+                                        config_parameter=
+                                        "employee_less_working_"
+                                        "hour_notification."
+                                        "minimum_working_hour",
+                                        default=0.00)
+    hr_email = fields.Char(string='HR Email', default="hr@gmail.com",
+                           help="  Set HR Manager's Email address to Send the"
+                                " Less Hour Worked Employees list to",
+                           config_parameter="employee_less_working_hour_"
+                                            "notification.hr_email")
