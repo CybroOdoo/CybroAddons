6c7edd6a42b903068c55ace8ad1592d68e03719a
AjmalCybro <ajmal@cybrosys.in>
2023-07-07 16:39:22 +0530

    Jul 7 : [ADD] Initial Commit 'pos_discount_manager'

---
+++ pos_discount_manager/__manifest__.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Cybrosys Techno Solutions(odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': "POS Discount Manager Approval",
+    'version': '15.0.1.0.0',
+    'summary': """Discount limit for each employee in every  point of sale""",
+    'description': """"This module helps you to set a discount limit for each 
+                       employee in every  point of sale. It facilitate the manager 
+                       approval when discount over the limit of employee""",
+    'category': 'Point Of Sale',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': "Cybrosys Techno Solutions",
+    'website': "http://www.cybrosys.com",
+    'depends': ['point_of_sale', 'pos_discount', 'hr'],
+    'data': [
+        'views/hr_employee_views.xml',
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_discount_manager/static/src/css/popup_size.css',
+            'pos_discount_manager/static/src/js/DiscountButton.js',
+            'pos_discount_manager/static/src/js/ValidateManager.js',
+        ],
+        'web.assets_qweb': [
+            'pos_discount_manager/static/src/xml/NumberPopup.xml'
+        ],
+    },
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
