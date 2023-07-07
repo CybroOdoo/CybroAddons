5372e63baec6f9b47cfc39861d3d10734634dcce
Shijin V <shijin@cybrosys.in>
2023-07-07 15:12:22 +0530

    Jul 07 : [ADD] Initial Commit 'sequence_for_journal'

---
+++ sequence_for_journal/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Mohamed Muzammil VP(odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Sequence For Journals',
+    'version': '16.0.1.0.0',
+    'category': 'Accounting',
+    'summary': """Customise sequence number of journals""",
+    'description': """Users can customise the sequence number of journals,
+                      Users can edit the journal's sequence still after a 
+                      entry is posted""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'http://www.cybrosys.com',
+    'depends': ['account'],
+    'data': [
+        'views/account_journal_views.xml',
+        'views/ir_sequence_views.xml',
+    ],
+    'license': 'AGPL-3',
+    'images': ['static/description/banner.jpg'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ sequence_for_journal/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <sequence_for_journal>
+
+#### 05.07.2023
+#### Version 16.0.1.0.0
+#### ADD
+
+- Initial commit for Sequence For Journals
