666ff4409a2d2f3ece942590a92653402a04c672
AjmalCybro <ajmal@cybrosys.in>
2023-07-17 14:10:13 +0530

    Jul 17 : [RMV] Module moved to OpenHRMS Git

--- hr_payroll_community/models/hr_leave_type.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-
-
-class HolidaysType(models.Model):
-    _inherit = "hr.leave.type"
-    _description = "Time Off Type"
-
-    code = fields.Char('code')
-
--- hr_payroll_community/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-#-*- coding:utf-8 -*-
-
-from . import hr_payroll_payslips_by_employees
-from . import hr_payroll_contribution_register_report
