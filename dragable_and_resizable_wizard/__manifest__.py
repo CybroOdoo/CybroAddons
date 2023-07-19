590152faa5cb00223b6d4223ba22d0cafc458240
AjmalCybro <ajmal@cybrosys.in>
2023-07-19 16:25:38 +0530

    Jul 19 : [ADD] Initial Commit 'dragable_and_resizable_wizard'

---
+++ dragable_and_resizable_wizard/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Mohammed Irfan T(odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Draggable And Resizable Wizard',
+    'version': '14.0.1.0.0',
+    'summary': 'Make wizard draggable and resizable',
+    'description': """Make every wizard in the backend draggable and resizable 
+                      as according to users need""",
+    'category': 'Extra Tools',
+    'author': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'web'],
+    'data': ['views/assets.xml'],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'AGPL-3'
+}
