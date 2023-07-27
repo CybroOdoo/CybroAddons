16243694ec908ca29684d2cb1cd8c7b96f4c133c
AjmalCybro <ajmal@cybrosys.in>
2023-07-27 18:16:10 +0530

    Jul 27 : [ADD] Initial Commit 'email_id_validation'

---
+++ email_id_validation/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<http://www.cybrosys.com>).
+#    Author: Rahul CK(<https://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU AFFERO GENERAL
+#    PUBLIC LICENSE (AGPL v3), Version 3.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC
+#    LICENSE (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': "Email Verification for Partner and Employee",
+    'version': '13.0.1.0.0',
+    'category': 'Discuss',
+    'summary': """Check Whether E-mail ID Is Valid for Partner and Employee""",
+    'description': """Check Whether E-mail ID Is Valid for Partner and Employee""",
+    'author': "Cybrosys Techno Solutions",
+    'company': "Cybrosys Techno Solutions",
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['base', 'hr'],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'external_dependencies': {'python': ['validate_email', 'py3DNS']},
+    'application': False,
+}
---
+++ email_id_validation/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <email_id_validation>
+
+#### 25.07.2023
+#### Version 13.0.1.0.0
+#### ADD
+- Initial commit for Email Verification for Partner and Employee
