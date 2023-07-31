56cf0e5df075ebcb8f0f8f0809df1e018c6ba314
AjmalCybro <ajmal@cybrosys.in>
2023-07-31 10:54:00 +0530

    Jul 31 : [FIX] Bug Fixed 'edit_save_button'

--- edit_save_button/static/src/views/form/form_controller.js
+++ edit_save_button/static/src/views/form/form_controller.js
@@ -1,30 +1,43 @@
 /** @odoo-module **/
-
 import { patch } from "@web/core/utils/patch";
 import { FormController } from "@web/views/form/form_controller";
+import { Component, onWillStart, useEffect, useRef, onRendered, useState, toRaw } from "@odoo/owl";
+import { useBus, useService } from "@web/core/utils/hooks";
+import { useModel } from "@web/views/model";
+import { SIZES } from "@web/core/ui/ui_service";
 
-patch(FormController, "form_controller", {
-    defaultProps: {
-        ...FormController.defaultProps,
-        preventEdit: true,
-    }
-});
+import { useViewButtons } from "@web/views/view_button/view_button_hook";
+import { useSetupView } from "@web/views/view_hook";
+import { useDebugCategory } from "@web/core/debug/debug_context";
+import { usePager } from "@web/search/pager_hook";
+import { isX2Many } from "@web/views/utils";
+import { registry } from "@web/core/registry";
+const viewRegistry = registry.category("views");
+import { StockPickingFormController } from "@stock/views/picking_form/picking_form_controller";
+
+odoo.__DEBUG__ && console.log("Console log inside the patch function", FormController.prototype, "form_controller");
+var data = false;
 
 patch(FormController.prototype, "save",{
+    setup() {
+        this.props.preventEdit = !data
+        this._super();
+    },
+
+    async edit(){
+        this._super();
+        data = true;
+        await this.model.root.switchMode("edit");
+    },
     async saveButtonClicked(params = {}){
         this._super();
+        data = false;
         await this.model.root.switchMode("readonly");
     },
     async discard(){
         this._super();
+        data = false;
         await this.model.root.switchMode("readonly");
     }
 })
-
-
-
-
-
-
-
 
