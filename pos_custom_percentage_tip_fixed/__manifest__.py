e0f1bf1d834f1d04afb5c324c97914f728159afb
RisvanaCybro <risvana@cybrosys.info>
2023-08-09 16:39:08 +0530

    Aug 09 : [ADD] Initial Commit 'pos_custom_percentage_tip_fixed'

---
+++ pos_custom_percentage_tip_fixed/__manifest__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Ruksana P(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Pos Custom Tips',
+    'version': '14.0.1.0.0',
+    'category': 'Point Of Sale',
+    'summary': """This module is used to apply fixed percentage of tip in POS 
+    Shops/Restaurant""",
+    'description': """Pos Custom Tips module provides features to manage tips 
+    in percentage.User can add tip as percentage to order and it will be 
+    displayed on receipt""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'point_of_sale'],
+    'data': [
+        'views/pos_config_views.xml',
+        'views/assets.xml'
+    ],
+    'qweb': [
+        'static/src/xml/PaymentScreen.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+
