38c6c8042edf1a00353b9a775694c3048f280b3a
amalgamated001 <websiteadmin@assl.com>
2023-08-09 14:27:53 +0530

    Aug 09 : [ADD] Initial Commit 'follower_restrict'

---
+++ follower_restrict/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Ayana KP (odoo@cybrosys.com)
+#
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+################################################################################
+{
+    'name': "Follower Restrict",
+    'version': '14.0.1.0.0',
+    'summary': 'Restrict Followers in Sale, Purchase and Invoicing module',
+    'description': 'Restrict adding the follower while confirming the '
+                   'sale order,purchase order and invoicing',
+    'category': 'Sales,Purchases,Accounting',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'sale_management', 'purchase'],
+    'data': ['views/res_config_settings_views.xml', ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ follower_restrict/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <follower_restrict>
+
+#### 23.06.2023
+#### Version 14.0.1.0.0
+#### ADD
+- Initial commit for Follower Restrict
