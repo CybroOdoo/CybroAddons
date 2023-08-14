72666da334a6eb4f8ff079bfb25ec9ff03f8f69d
Shijin V <shijin@cybrosys.in>
2023-08-14 11:09:40 +0530

    Aug 14 : [ADD] Initial Commit 'model_viewer_widget'

---
+++ model_viewer_widget/__manifest__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Technologies (<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': '3D Image Viewer Widget',
+    'version': '16.0.1.0.0',
+    'summary': """It helps to render interactive 3D models.""",
+    'description': """It helps to render interactive 3D models. Zoom in and
+     zoom out feature is available and also can move in in any direction""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'category': 'Sales, Extra Tools',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'depends': ['base', 'product'],
+    'data': [
+        'views/product_template_views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'https://unpkg.com/@egjs/view3d@2.10.1/dist/view3d.pkgd.min.js',
+            'model_viewer_widget/static/src/js/widget.js',
+            'model_viewer_widget/static/src/xml/widget.xml',
+        ]
+    },
+    'images': ['static/description/banner.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ model_viewer_widget/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <model_viewer_widget>
+
+#### 14.08.2023
+#### Version 16.0.1.0.0
+#### ADD
+- Initial Commit for Model Viewer Widget
