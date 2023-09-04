3e61de58b1cccd5e683eb4acc3eca801186bf190
AjmalCybro <ajmal@cybrosys.in>
2023-09-04 17:39:09 +0530

    Sep 1 [FIX] : Bug Fixed 'one2many_search'

--- one2many_search/__manifest__.py
+++ one2many_search/__manifest__.py
@@ -22,7 +22,7 @@
 
 {
     'name': "One2many Search Widget Odoo15",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     'description': "Quick Search Feature For One2many Fields In Odoo",
     'summary': 'Quick Search Feature For One2many Fields In Odoo',
     "website": "https://www.cybrosys.com",
--- one2many_search/doc/RELEASE_NOTES.md
+++ one2many_search/doc/RELEASE_NOTES.md
@@ -5,3 +5,8 @@
 #### ADD
 - Initial commit for one2many_search
 
+
+#### 04.09.2023
+#### Version 15.0.1.0.1
+#### ADD
+- Bug Fix - Solved the RTL issue
