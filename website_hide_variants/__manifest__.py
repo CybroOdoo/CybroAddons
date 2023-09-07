7e3be61abe06a50560dd2c04eaacb18e6d61b81e
RisvanaCybro <risvana@cybrosys.info>
2023-09-07 17:44:09 +0530

    Sep 07 : [ADD] Initial Commit 'website_hide_variants'

---
+++ website_hide_variants/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Cybrosys Techno Solutions (Contact :odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': "Disable Variants in Website",
+    'version': '14.0.1.0.0',
+    'category': "eCommerce ",
+    'summary': """This module helps to hide product variants in website shop""",
+    'description': """This module allows users to selectively conceal 
+     specific products and their associated variants within the website shop""",
+    'author': "Cybrosys Techno Solutions",
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'depends': ['website_sale', 'sale_management'],
+    'data': [
+             'views/product_product_views.xml',
+             'views/assets.xml',
+        ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
