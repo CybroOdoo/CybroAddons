e34e939ca7998667c1c443249d6880cd3323f011
RisvanaCybro <risvana@cybrosys.info>
2023-09-14 18:25:00 +0530

    Sep 14 : [ADD] Initial Commit 'countrybased_terms_and_condition'

---
+++ countrybased_terms_and_condition/doc/RELEASE_NOTES.md
@@ -0,0 +1,8 @@
+## Module <countrybased_terms_and_condition>
+
+#### 14.09.2023
+#### Version 15.0.1.0.0
+#### ADD
+
+- Initial commit for Country-Based Terms and Condition
+
---
+++ countrybased_terms_and_condition/models/sale_order.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Fathima Mazlin AM (<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+from odoo import api, models
+
+
+class SalesOrder(models.Model):
+    """ This class extends the Sale Order model to include custom fields and
+    methods."""
+    _inherit = "sale.order"
+
+    @api.onchange('partner_id')
+    def _default_note(self):
+        """
+        Compute the value of the 'note' field for the current record based
+        on the value of the 'partner_id' field. If the country associated
+        with the partner has a value for 'sale_terms_condition', use that
+        value as the note. Otherwise, call the parent method to compute the
+        value.
+        """
+        terms_and_condition = self.partner_id.country_id.sale_terms_condition
+        if terms_and_condition:
+            self.note = terms_and_condition
+        else:
+            self.note = " "
+            res = super(SalesOrder, self)._default_note()
+            return res
