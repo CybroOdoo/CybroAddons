50d8ee4386657cbb4d34dbe3f451db4f66c41ea5
RisvanaCybro <risvana@cybrosys.info>
2023-09-28 09:22:02 +0530

    Sep 28 : [ADD] Initial Commit 'website_cart_clear'

---
+++ website_cart_clear/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Fathima Mazlin AM (<https://www.cybrosys.com>)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Website Clear Cart',
+    'version': '15.0.1.0.0',
+    'category': 'Website',
+    'summary': 'This module helps to clear all product in cart list.',
+    'description': """This module helps to clear all product in cart list by 
+        clicking the button 'Clear Cart'.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'website', 'website_sale'],
+    'data': [
+        'views/website_cart_clear_templates.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
