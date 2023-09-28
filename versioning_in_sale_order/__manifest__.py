f1d5adb057c8967857fa03e16f584773042638a3
RisvanaCybro <risvana@cybrosys.info>
2023-09-28 09:21:45 +0530

    Sep 28 : [ADD] Initial Commit 'versioning_in_sale_order'

---
+++ versioning_in_sale_order/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Mohammed Irfan T (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Sale Order Versions',
+    'version': '15.0.1.0.0',
+    'category': 'Sales',
+    'summary': 'For creating multiple sale order versions',
+    'description': 'Many revisions may be necessary while '
+                   'working on a sale order, this module '
+                   'allows you to make adjustments to the '
+                   'same order by designating revision numbers'
+                   'for the original sales order to handle such'
+                   'circumstances.',
+    'author': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['sale_management'],
+    'data': [
+        'views/sale_order_views.xml'
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
