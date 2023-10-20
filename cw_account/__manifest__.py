c047969b2f2890c37d8e55ee16216d3106c1523c
RisvanaCybro <risvana@cybrosys.info>
2023-10-20 19:59:45 +0530

    Oct 20 : [ADD] Initial Commit 'cw_account'

---
+++ cw_account/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Unnimaya C O (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+{
+    'name': "Catch Weight Management: Accounting",
+    'version': '15.0.1.0.0',
+    'category': 'Accounting',
+    'summary': """Helps to add Catch Weight of products in Accounting module""",
+    'description': """Catch weight is simply a parallel unit of measure used to
+     manage variable-weight products. This module brings the Catch weight 
+     system in the Accounting module.We can invoice products based on their 
+     Catch Weight.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['account', 'cw_stock'],
+    'data': [
+        'views/account_move_views.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ cw_account/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <cw_account>
+
+#### 04.10.2023
+#### Version 15.0.1.0.0
+#### ADD
+
+- Initial commit for Catch Weight Management: Accounting
