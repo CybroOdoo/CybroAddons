42f92271856a3ef5ef4a281be91fc8f1af699fa6
RisvanaCybro <risvana@cybrosys.info>
2023-10-26 11:03:05 +0530

    Oct 26 : [ADD] Initial Commit 'task_overdue_email_odoo'

---
+++ task_overdue_email_odoo/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Cybrosys Technologies Pvt. Ltd.
+#
+# Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+# Author: Ayana K P (odoo@cybrosys.com)
+#
+# You can modify it under the terms of the GNU AFFERO
+# GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+# You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+# (AGPL v3) along with this program.
+# If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': "Task Overdue Email Notification",
+    'version': "14.0.1.0.0",
+    'category': 'Project',
+    'summary': """Send Email to Assigned for Overdue Task""",
+    'description': """This app is used for overdue task notifications by 
+     email. suggests that the app is designed to send notifications via email 
+     for tasks that are overdue""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['project'],
+    'data': [
+        'data/mail_data.xml',
+        'data/ir_cron_data.xml',
+        'views/res_config_settings_views.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ task_overdue_email_odoo/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <task_overdue_email_odoo>
+
+#### 20.10.2023
+#### Version 14.0.1.0.0
+#### ADD
+- Initial Commit for Task Overdue Email Notification
