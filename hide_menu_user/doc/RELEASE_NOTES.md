86e83649d33c1a73464ddcc7611cc188c57a973e
AjmalCybro <ajmal@cybrosys.in>
2023-10-26 10:04:36 +0530

    Oct 25 : [UPDT] Bug Fixed 'hide_menu_user'

--- hide_menu_user/doc/RELEASE_NOTES.md
+++ hide_menu_user/doc/RELEASE_NOTES.md
@@ -1,9 +1,11 @@
 ## Module <hide_menu_user>
 
 #### 30.12.2021
-#### Version 15.0.1.0.0
+#### Version 16.0.1.0.0
 #### ADD
 - Initial commit for hide_menu_user
 
-
-
+#### 19.10.2023
+#### Version 16.0.1.0.1
+#### UPDT
+- Bug Fix: Installation issue.
--- hide_menu_user/models/res_user.py
+++ hide_menu_user/models/res_user.py
@@ -38,13 +38,14 @@ class HideMenuUser(models.Model):
         """
         Else the menu will be still hidden even after removing from the list
         """
-        res = super(HideMenuUser, self).write(vals)
-        for menu in self.hide_menu_ids:
-            menu.write({
-                'restrict_user_ids': [(4, self.id)]
-            })
-        self.clear_caches()
-        return res
+        for rec in self:
+            res = super(HideMenuUser, rec).write(vals)
+            for menu in rec.hide_menu_ids:
+                menu.write({
+                    'restrict_user_ids': [(4, rec.id)]
+                })
+            rec.clear_caches()
+            return res
 
     def _get_is_admin(self):
         """
