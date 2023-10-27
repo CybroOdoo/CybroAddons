05ee09f7958a8a014bc986fc55659631f603a6da
RisvanaCybro <risvana@cybrosys.info>
2023-10-27 18:58:36 +0530

    Oct 27 : [ADD] Initial Commit 'odoo_chatgpt_connector'

---
+++ odoo_chatgpt_connector/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <odoo_chatgpt_connector>
+
+#### 03.10.2023
+#### Version 15.0.1.0.0
+#### ADD
+- Initial commit for Odoo ChatGPT Connector
---
+++ odoo_chatgpt_connector/static/src/js/submit_request.js
@@ -0,0 +1,67 @@
+odoo.define('odoo_chatgpt_connector.chatgpt_search', function(require) {
+    'use strict';
+    var ajax = require('web.ajax');
+    var publicWidget = require('web.public.widget');
+    var core = require('web.core');
+    var rpc = require('web.rpc');
+    /* extend the widget */
+    publicWidget.registry.QuestionInput = publicWidget.Widget.extend({
+        selector: "#main_layout",
+        events: {
+            'change #question_input ': 'get_answer',
+            'click #copy_content': 'copy',
+        },
+        /* @constructor*/
+        init: function() {
+            var api_key = "";
+        },
+        /* For getting the Answer of submitted question*/
+        get_answer: async function(event) {
+            this.$el.find(this).prop("disabled", true);
+            this.$el.find('#result_area').val('');
+            this.$el.find('#result_area')[0].placeholder = "Loading.....";
+            if (!this.api_key) {
+                var data = await rpc.query({
+                    model: 'ir.config_parameter',
+                    method: 'get_param',
+                    args: ['odoo_chatgpt_connector.api_key'],
+                }).then(result => {
+                    this.api_key = result;
+                });
+            }
+            var question = event.target.value;
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            myHeaders.append("Authorization", "Bearer " + this.api_key);
+            var raw = JSON.stringify({
+                "model": "text-davinci-003",
+                "prompt": question,
+                "temperature": 0,
+                "max_tokens": 1000,
+                "top_p": 1,
+                "frequency_penalty": 0,
+                "presence_penalty": 0,
+            });
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+            /* For showing the answer in the result area */
+            fetch("https://api.openai.com/v1/completions", requestOptions)
+                .then(response => response.text())
+                .then(result => {
+                    const obj = JSON.parse(result);
+                    let area = obj.choices[0].text;
+                    this.$el.find('#result_area').val(obj.choices[0].text.trim());
+                })
+                .catch(error => console.log('error', error));
+        },
+        /*For copying the Answer to the clipboard*/
+        copy: function(event) {
+            document.querySelector('#result_area').select();
+            document.execCommand('copy');
+        },
+    });
+});
