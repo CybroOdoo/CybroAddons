0d3dd99322a9c95b2957abf9bd656f1357fab39b
RisvanaCybro <risvana@cybrosys.info>
2023-10-31 16:51:18 +0530

    Oct 31 : [ADD] Initial Commit 'purchase_order_delivery_status'

---
+++ purchase_order_delivery_status/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Aysha Shalin (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Delivery Status on Purchase Order',
+    'version': '15.0.1.0.0',
+    'category': 'Purchases',
+    'summary': 'Status of delivery on purchase order.',
+    'description': """This module adds delivery status on purchase order form 
+    and tree view.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['purchase', 'stock', 'purchase_stock'],
+    'data': ['views/purchase_order_views.xml'],
+    'images': ['static/description/banner.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False
+}
