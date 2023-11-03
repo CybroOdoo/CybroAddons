44593f16d0a6a0c7329c3fa7004866c02747483b
RisvanaCybro <risvana@cybrosys.info>
2023-11-03 20:00:14 +0530

    Nov 03 : [ADD] Initial Commit 'mandatory_field_highlight'

---
+++ mandatory_field_highlight/__manifest__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Fathima Mazlin AM(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Highlight Mandatory Field',
+    'version': '15.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'This Module will help to customize mandatory fields '
+               'in odoo',
+    'description': 'User can customize the mandatory fields view by '
+                   'different colors',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base_setup'],
+    'data': [
+        'views/res_config_settings_views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'mandatory_field_highlight/static/src/js/action_manager.js',
+            'mandatory_field_highlight/static/src/scss/field.scss',
+        ],
+    },
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
