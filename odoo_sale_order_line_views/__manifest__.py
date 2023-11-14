32694883df413c2bcff664e5b13415c634e59995
AjmalCybro <ajmal@cybrosys.in>
2023-11-14 17:21:13 +0530

    Nov 14 : [ADD]Initial Commit 'odoo_sale_order_line_views'

---
+++ odoo_sale_order_line_views/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2022-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#############################################################################
+{
+    'name': 'Sale Order Line Views',
+    'version': '15.0.1.0.0',
+    'category': 'Sales',
+    'summary': """Sale order and quotation lines view""",
+    'description': """Two additional options for sale order lines and quotation 
+     lines are included for detailed view with images""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['sale_management'],
+    'data': [
+        'views/sale_order_line_views.xml',
+        'views/sale_order_views.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'licence': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ odoo_sale_order_line_views/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <odoo_sale_order_line_views>
+
+#### 25.09.2023
+#### Version 15.0.1.0.0
+#### ADD
+Initial Commit Sale Order Line Views
