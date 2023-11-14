a9e572b8cf9f08d08375df7213c7fa25551b9321
Shijin V <shijin@cybrosys.in>
2023-11-14 13:58:16 +0530

    NOV 14 : [ADD] Initial Commit 'payment_paytabs_odoo'

---
+++ payment_paytabs_odoo/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'PayTabs Payment Acquirer',
+    'version': '16.0.1.0.0',
+    'category': 'eCommerce,Marketing',
+    'summary': 'PayTabs Payment Gateway For Website',
+    'description': "This module enables seamless payments through PayTabs, "
+                   "ensuring secure and convenient online transactions.",
+    'author': "Cybrosys Techno Solutions",
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'images': ['static/description/banner.png'],
+    'depends': ['payment', 'account', 'website_sale'],
+    'data': [
+        'views/payment_provider_views.xml',
+        'views/payment_paytabs_templates.xml',
+        'data/payment_provider_data.xml',
+    ],
+    'post_init_hook': 'post_init_hook',
+    'uninstall_hook': 'uninstall_hook',
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
