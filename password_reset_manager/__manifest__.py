719ec98474b8738a290dc2cb85ec7f6ee9ccad69
AjmalCybro <ajmal@cybrosys.in>
2023-11-17 14:26:16 +0530

    Nov 17 : [FIX]Bug Fixed 'password_reset_manager'

--- password_reset_manager/__manifest__.py
+++ password_reset_manager/__manifest__.py
@@ -3,32 +3,34 @@
 #
 #    Cybrosys Technologies Pvt. Ltd.
 #
-#    Copyright (C) 2022-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
-#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions (<https://www.cybrosys.com>)
 #
 #    You can modify it under the terms of the GNU LESSER
-#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#    GNU LESSER GENERAL PUBLIC LICENSE (AGPL v3) for more details.
 #
 #    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
-#    (LGPL v3) along with this program.
+#    (AGPL v3) along with this program
 #    If not, see <http://www.gnu.org/licenses/>.
 #
 #############################################################################
+#    """manifest"""
 {
     'name': "Password Reset Manager",
-    'version': '16.0.1.0.0',
-    'summary': """User Password Reset Manager""",
-    'description': """User Password Reset Manager""",
-    'category': 'Technical',
+    'version': '16.0.1.0.1',
+    'category': 'Extra Tools',
+    'summary': "User Password Reset Manager",
+    'description': "This module helps users to change and reset forgotten "
+                   "password. ",
     'author': "Cybrosys Techno Solutions",
-    'website': "https://www.cybrosys.com",
     'company': 'Cybrosys Techno Solutions',
     'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
     'depends': ['auth_signup', 'web'],
     'data': [
         'views/reset_password.xml',
@@ -36,5 +38,6 @@
     'images': ['static/description/banner.png'],
     'license': "AGPL-3",
     'installable': True,
+    'auto_install': False,
     'application': True,
 }
--- password_reset_manager/doc/RELEASE_NOTES.md
+++ password_reset_manager/doc/RELEASE_NOTES.md
@@ -3,3 +3,4 @@
 #### 21.12.2022
 #### Version 16.0.1.0.0
 #### ADD
+Initial Commit for Password Reset Manager
