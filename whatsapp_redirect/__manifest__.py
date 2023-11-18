18315f54c5277ddbbd06965bd38d20dc9fc52db1
AjmalCybro <ajmal@cybrosys.in>
2023-11-18 16:37:44 +0530

    Nov 18 : [ADD] Initial Commit 'whatsapp_redirect'

---
+++ whatsapp_redirect/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author:Jumana Jabin MP (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Send Whatsapp Message',
+    'version': '17.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Send Message to partner via Whatsapp web',
+    'description': 'This module helps you to directly send messages to your '
+                   'contacts through WhatsApp web.',
+    'author': 'Cybrosys Techno solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'contacts'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_partner_views.xml',
+        'wizard/whatsapp_send_message_views.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ whatsapp_redirect/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <whatsapp_redirect>
+
+#### 17.11.2023
+#### Version 17.0.1.0.0
+##### ADD
+- Initial commit for Send Whatsapp Message 
