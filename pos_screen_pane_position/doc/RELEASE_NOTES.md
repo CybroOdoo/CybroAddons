5f8c16ae7a3b5cddf81423b5c11b5d35b423ca47
Shijin V <shijin@cybrosys.in>
2023-11-18 16:23:19 +0530

    NOV 18 : [ADD] Initial Commit 'pos_screen_pane_position'

---
+++ pos_screen_pane_position/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <pos_screen_pane_position>
+
+#### 18.11.2023
+#### Version 16.0.1.0.0
+#### ADD
+
+- Initial commit for Screen Positions in POS
---
+++ pos_screen_pane_position/models/pos_config.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: THASNI CP (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+from odoo import fields, models
+
+
+class PosConfig(models.Model):
+    """To add a fields in pos config to view the positions of the screen"""
+    _inherit = 'pos.config'
+
+    screen_view = fields.Selection([('left', 'Left'), ('right', 'Right')],
+                                   string="Screen View",
+                                   help="To set the position of the screen",
+                                   default='left', required=True)
