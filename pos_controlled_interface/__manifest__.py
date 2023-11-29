6e05238a85f0dbaa449a4f1c3d590bc2c5219ece
RisvanaCybro <risvana@cybrosys.info>
2023-11-29 10:08:10 +0530

    Nov 29 : [ADD] Initial Commit 'pos_controlled_interface'

---
+++ pos_controlled_interface/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Bhagyadev K P(<https://www.cybrosys.com>)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+################################################################################
+{
+    'name': 'Controlled Point Of Sale',
+    'version': '15.0.1.0.0',
+    'category': 'Point of Sale',
+    'summary': 'Control Discount & Price For the POS.',
+    'description': 'Manager Can Control Discount and Price For the '
+                   'Point Of Sale.',
+    'author': 'Cybrosys Techno solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/pos_config_views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'pos_controlled_interface/static/src/js/restrict_pos.js'],
+    },
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
