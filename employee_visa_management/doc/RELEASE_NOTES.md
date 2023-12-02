0e68b2583cb5e44519840cf8897eeec3e624292a
Sayooj <cybroodoo@gmail.com>
2023-12-02 10:47:38 +0530

    Dec 02 : [ADD] Initial Commit 'employee_visa_management'

---
+++ employee_visa_management/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <employee_visa_management>
+
+#### 02.12.2023
+#### Version 15.0.1.0.0
+#### ADD
+-  Initial commit for Visa Management
---
+++ employee_visa_management/models/hr_employee.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Hridhya D (<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+from odoo import fields, models
+
+
+class HrEmployee(models.Model):
+    """The class HrEmployee for the model hr_employee which
+    is for customization regarding Employee Visa page"""
+    _inherit = "hr.employee"
+
+    employee_visa_ids = fields.One2many('visa.approval', 'employee_id',
+                                        string="Visa Application no",
+                                        help="Visa Application no")
