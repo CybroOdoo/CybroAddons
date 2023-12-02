3c06c82239ada02672fc8423824ee70d3630d58e
RisvanaCybro <risvana@cybrosys.info>
2023-12-02 10:00:52 +0530

    Dec 02 : [ADD] Initial Commit 'pos_kitchen_screen_odoo'

---
+++ pos_kitchen_screen_odoo/__manifest__.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Gayathri v(odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'POS Kitchen Screen',
+    'version': '15.0.1.0.0',
+    'category': 'Point Of Sale',
+    'summary': 'POS Kitchen Screen facilitates sending certain orders '
+               'automatically to the kitchen.The POS Kitchen Screen in '
+               'Odoo 15 also allows for the customization of order views, '
+               'so that staff can see the information that is most '
+               'important to them.',
+    'description': 'The POS Kitchen Screen in Odoo 15 is a feature that '
+                   'allows restaurant staff to view and manage orders in '
+                   'real-time from the kitchen. This screen provides a clear '
+                   'and organized display of all active orders, enabling '
+                   'kitchen staff to prioritize and manage their tasks '
+                   'efficiently. The POS Kitchen Screen in Odoo 15 also '
+                   'allows for the customization of order views, so that '
+                   'staff can see the information that is most important to '
+                   'them. Additionally, this feature facilitates '
+                   'communication between front-end and back-end staff, '
+                   'enabling them to work together seamlessly and provide a '
+                   'better dining experience for customers.',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['point_of_sale'],
+    'data': [
+        'security/pos_kitchen_screen_odoo_groups.xml',
+        "security/ir.model.access.csv",
+        'data/kitchen_screen_sequence_data.xml',
+        "views/kitchen_screen_views.xml",
+        "views/pos_kitchen_screen_odoo_menus.xml",
+        "views/pos_order_views.xml",
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_kitchen_screen_odoo/static/src/scss/pos.scss',
+            'pos_kitchen_screen_odoo/static/src/js/product_screen.js',
+            'pos_kitchen_screen_odoo/static/src/js/pos_js.js',
+            'pos_kitchen_screen_odoo/static/src/js/Receipt.js',
+        ],
+        'web.assets_backend': [
+            'pos_kitchen_screen_odoo/static/src/js/dashboard_action.js',
+            'pos_kitchen_screen_odoo/static/src/css/dashboard.css',
+            'https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js',
+            'https://fonts.googleapis.com',
+            'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js',
+            'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js'
+        ],
+        'web.assets_qweb': [
+            'pos_kitchen_screen_odoo/static/src/xml/SubmitOrderButton.xml',
+            'pos_kitchen_screen_odoo/static/src/xml/dashboard_templates.xml',
+        ],
+    },
+    'images': [
+        'static/description/banner.jpg',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
+
---
+++ pos_kitchen_screen_odoo/static/src/js/product_screen.js
@@ -0,0 +1,48 @@
+/** @odoo-module **/
+
+import ProductScreen from 'point_of_sale.ProductScreen';
+import Registries from 'point_of_sale.Registries';
+const rpc = require('web.rpc');
+const { Gui } = require('point_of_sale.Gui');
+var core = require('web.core');
+var _t = core._t;
+//Extending the ProductScreen for adding validation for kitchen orders
+export const KitchenProductScreen = (ProductScreen) =>
+ class extends ProductScreen {
+        setup() {
+            super.setup();
+        }
+        async _onClickPay() {
+            if (this.env.pos.get_order().orderlines.some(line => line.get_product().tracking !== 'none' && !line.has_valid_product_lot()) && (this.env.pos.picking_type.use_create_lots || this.env.pos.picking_type.use_existing_lots)) {
+                const { confirmed } = await this.showPopup('ConfirmPopup', {
+                    title: this.env._t('Some Serial/Lot Numbers are missing'),
+                    body: this.env._t('You are trying to sell products with serial/lot numbers, but some of them are not set.\nWould you like to proceed anyway?'),
+                    confirmText: this.env._t('Yes'),
+                    cancelText: this.env._t('No')
+                });
+                if (confirmed) {
+                    this.showScreen('PaymentScreen');
+                }
+            } else {
+             var order_name=this.currentOrder.name
+             var self=this
+             rpc.query({
+                    model: 'pos.order',
+                    method: 'check_order',
+                    args: [[],order_name]
+                }).then(function(result){
+                if (result==true){
+                     Gui.showPopup('ErrorPopup',{
+                'title': _t('Food is not ready'),
+                'body':  _t('Please Complete all the food first.'),
+            });
+                }
+                else{
+                self.showScreen('PaymentScreen');
+                }
+                });
+
+            }
+        }
+ };
+Registries.Component.extend(ProductScreen, KitchenProductScreen);
