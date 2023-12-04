04945ab9cc45565dd904bf403673294a61ed2d75
RisvanaCybro <risvana@cybrosys.info>
2023-12-04 15:11:43 +0530

    Dec 04 : [ADD] Initial Commit 'website_bargain'

---
+++ website_bargain/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <website_bargain>
+
+#### 21.11.2023
+#### Version 16.0.1.0.0
+#### ADD
+- Initial Commit for Product Bidding In ECommerce
---
+++ website_bargain/models/bargain_subscribers.py
@@ -0,0 +1,44 @@
+"""subscribers class and its functions"""
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Mruthul Raj (<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+from odoo import fields, models
+
+
+class BargainSubscribers(models.Model):
+    """Model for adding the subscriber details """
+    _name = 'bargain.subscribers'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = "Subscriber details"
+
+    subscriber_id = fields.Many2one('res.partner', required=True,
+                                    help="Subscribers id will be here ")
+    name = fields.Char(related='subscriber_id.name', string='Name',
+                       readonly=False, help="Subscribers name",
+                       required=True)
+    email = fields.Char(related='subscriber_id.email', string='Email',
+                        readonly=False, help="Subscribers email")
+    auction_id = fields.Many2one('website.bargain', string='Auction',
+                                 required=True, help="Auction details")
+    subscribe_time = fields.Datetime(string='Subscribe On',
+                                     default=fields.Datetime.today(),
+                                     readonly=True,
+                                     help="Time of subscription")
