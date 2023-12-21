cf9f7ddcedeba0a974948f6c5f202c73552ccd71
Shijin V <shijin@cybrosys.in>
2023-12-21 17:12:11 +0530

    DEC 21 : [ADD] Initial Commit 'multi_image_in_pos'

---
+++ multi_image_in_pos/models/multi_image.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: MOHAMMED DILSHAD TK (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+from odoo import fields, models
+
+
+class MultiImage(models.Model):
+    """Add fields to the Multi Image"""
+    _name = 'multi.image'
+    _description = 'Multi Image'
+
+    image = fields.Binary(string="images", help="Upload the Images")
+    product_id = fields.Many2one(comodel_name='product.product',
+                                 help="Connecting field to 'product.product'"
+                                      " model")
---
+++ multi_image_in_pos/models/pos_session.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: MOHAMMED DILSHAD TK (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+from odoo import models
+
+
+class PosSession(models.Model):
+    """Load fields into pos session"""
+    _inherit = 'pos.session'
+    _description = 'Pos Session'
+
+    def _loader_params_product_product(self):
+        """Super the function and loads image field to pos props"""
+        result = super()._loader_params_product_product()
+        result['search_params']['fields'].append('image_ids')
+        return result
