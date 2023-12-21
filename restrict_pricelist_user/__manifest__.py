e2d62d3aea7b4ebc52e6027228f36d6e85350d36
Shijin V <shijin@cybrosys.in>
2023-12-21 16:58:27 +0530

    DEC 21 : [UPDT] Index file updated 'restrict_pricelist_user'

---
+++ restrict_pricelist_user/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Amaya Aravind (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Restrict Pricelists for Users',
+    'version': '16.0.1.0.0',
+    'category': 'Sales',
+    'summary': 'Feature to Restrict Pricelist for users.',
+    'description': "This module provides an option to set allowed pricelists "
+                   "for users. The defined users can access all the pricelists"
+                   " if not particular pricelists are being assigned to them."
+                   " Moreover, this feature is compatible with multi-company "
+                   "setup of the Odoo platform business management.",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['sale_management'],
+    'data': [
+        'views/res_config_settings_views.xml',
+        'views/res_users_views.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ restrict_pricelist_user/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <restrict_pricelist_user>
+
+#### 21.12.2023
+#### Version 16.0.1.0.0
+#### ADD
+
+- Initial commit for Restrict Pricelists for Users
