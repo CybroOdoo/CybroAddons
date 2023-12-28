3366d03fa00a5f6c686cb9d3762e016c8d42eaa1
RisvanaCybro <risvana@cybrosys.info>
2023-12-28 12:44:26 +0530

    Dec 28 : [ADD] Initial Commit 'task_deadline_reminder'

---
+++ task_deadline_reminder/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author:Anjhana A K(<https://www.cybrosys.com>)
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': "Task Deadline Reminder",
+    'version': '17.0.1.0.0',
+    'category': "Project",
+    'summary': "Automatically Send Mail To Responsible User if Deadline Of "
+               "Task is Today",
+    'description': """This app sends an email to the person in charge whenever 
+    a task deadline is today. This helps in keeping everyone updated and 
+    ensures tasks are completed on time.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['project'],
+    'data': [
+        'views/project_task_views.xml',
+        'data/ir_cron_data.xml',
+        'data/mail_template_data.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ task_deadline_reminder/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <task_deadline_reminder>
+
+#### 28.12.2023
+#### Version 17.0.1.0.0
+#### ADD
+
+- Initial Commit for Task Deadline Reminder
