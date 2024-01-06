58fcf1f93c805739f740772177b4ff8198c635b4
Shijin V <shijin@cybrosys.in>
2024-01-06 11:40:37 +0530

    Jan 06: [ADD] Initial Commit 'whatsapp_product_inquiry'

---
+++ whatsapp_product_inquiry/models/res_company.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Unnimaya C O (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class ResCompany(models.Model):
+    """Inherits Company for Adding the Whatsapp Fields"""
+    _inherit = 'res.company'
+
+    whatsapp_number = fields.Char(string='Whatsapp Number',
+                                  help="The Company Whatsapp Number to "
+                                       "which the Inquiry messages has to"
+                                       " be received")
+    message = fields.Text(string='Message',
+                          default="I want to know more details of the product",
+                          help="This will be the Inquiry message")
+
+    @api.constrains('whatsapp_number')
+    def _check_whatsapp_number(self):
+        """Validate Mobile Number"""
+        if self.whatsapp_number:
+            if not self.whatsapp_number.isnumeric() or not len(
+                    self.whatsapp_number) > 8 or ' ' in self.whatsapp_number:
+                raise UserError(_('Whatsapp Number should be valid number'
+                                  ' without any space or signs'))
