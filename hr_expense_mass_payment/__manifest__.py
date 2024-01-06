044bf670f9073d7915edd7abd7c7ebd947e35cdb
RisvanaCybro <risvana@cybrosys.info>
2024-01-06 15:11:57 +0530

    Jan 06 : [ADD] Initial Commit 'hr_expense_mass_payment'

---
+++ hr_expense_mass_payment/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Nikhil M(odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+
+###############################################################################
+{
+    'name': 'HR Expense Mass Payment',
+    'version': '15.0.1.0.0',
+    'category': 'Human Resources',
+    'summary': 'Register payment of multiple expenses together.',
+    'description': """This module allows you to register multiple expenses 
+    payment at once. The module makes the payment and journal posting of 
+    expenses quicker and makes the process more efficient.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['hr_expense'],
+    'data': [
+        'data/hr_expense_mass_payment_data.xml'
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ hr_expense_mass_payment/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <hr_expense_mass_payment>
+
+#### 22.12.2023
+#### Version 15.0.1.0.0
+##### ADD
+
+- Initial Commit for HR Expense Mass Payment
