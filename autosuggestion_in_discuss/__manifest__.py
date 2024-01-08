dea59bf51efce339b0b844628a2da0430d26c703
Shijin V <shijin@cybrosys.in>
2024-01-08 18:28:32 +0530

    Jan 08: [ADD] Initial Commit 'autosuggestion_in_discuss'

---
+++ autosuggestion_in_discuss/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Ruksana P (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Autosuggestion in Discuss',
+    'version': '14.0.1.0.0',
+    'category': 'Discuss',
+    'summary': """ This module is used to see automatic suggestions of previous
+     messages of user.""",
+    'description': """ To get a fast message-typing feature, previous messages 
+     of  user will be seen as autosuggestions in the chat when the user types 
+     his/her messages. """,
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'mail'],
+    'data': [
+        'views/assets.xml',
+    ],
+    'qweb': [
+        'static/src/xml/autosuggest_templates.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ autosuggestion_in_discuss/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <autosuggestion_in_discuss>
+
+#### 08.01.2024
+#### Version 14.0.1.0.0
+#### ADD
+- Initial commit for Autosuggestion in Discuss
