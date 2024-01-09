7de4621e0592c0334b1793077ea67155c940fbd1
RisvanaCybro <risvana@cybrosys.info>
2024-01-09 20:24:50 +0530

    Jan 09 : [ADD] Initial Commit 'web_login_styles'

---
+++ web_login_styles/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Mohammed Dilshad Tk (<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Customize Login Page Style',
+    'version': '17.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Customize The Login Page With Different Styles',
+    'description': 'The Module helps to customize login page with different '
+                   'styles',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base','base_setup','web'],
+    'data': [
+        'views/res_config_settings_views.xml',
+        'views/webclient_templates_right.xml',
+        'views/webclient_templates_left.xml',
+        'views/webclient_templates_middle.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ web_login_styles/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <web_login_styles>
+
+#### 08.01.2024
+#### Version 17.0.1.0.0
+#### ADD
+- Initial Commit for Customize Login Page Style.
