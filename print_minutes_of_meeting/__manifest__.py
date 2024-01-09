a31644c26f83b3cb5843821100f9318f6ff9adda
Shijin V <shijin@cybrosys.in>
2024-01-09 12:30:48 +0530

    Jan 09: [ADD] Initial Commit 'print_minutes_of_meeting'

---
+++ print_minutes_of_meeting/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Mohammed Irfan T @ Cybrosys, (odoo@cybrosys.com)
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Minutes of Meetings Report',
+    'version': '14.0.1.0.0',
+    'category': 'Project',
+    'summary': 'Module For Print PDF Report And Records The Meeting Minutes ',
+    'description': 'This module helps you to keep record of minutes of meeting'
+                   ' and to print it in the PDF format.',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'calendar'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/calendar_event_views.xml',
+        'report/calendar_event_reports.xml',
+        'report/calendar_event_templates.xml',
+        'data/calendar_event_data.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ print_minutes_of_meeting/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <print_minutes_of_meeting>
+
+#### 09.01.2024
+#### Version 14.0.1.0.0
+#### ADD
+
+-Initial Commit for Minutes of Meetings Report
