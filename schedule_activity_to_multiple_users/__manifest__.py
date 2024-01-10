30d9a05e21ad52ec0bf9dc780ef47d58a8835fd3
Shijin V <shijin@cybrosys.in>
2024-01-10 11:45:55 +0530

    Jan 10: [ADD] Initial Commit 'schedule_activity_to_multiple_users'

---
+++ schedule_activity_to_multiple_users/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Mruthul Raj(odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Schedule Activity to Multiple Users',
+    'version': '14.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': """Add multiple users in activity""",
+    'description': """The "Schedule Activity to Multiple Users" feature in 
+     Odoo 15 allows you to assign an activity or task to multiple users within 
+     the system. This feature is particularly useful when you have a task 
+     that requires collaboration or involvement from multiple individuals
+     or teams.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['base_setup', 'mail'],
+    'data': [
+        'views/mail_activity_views.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
