b29882559a49224f8ce145f69f20b226870200c2
Shijin V <shijin@cybrosys.in>
2024-01-11 16:44:36 +0530

    Jan 10: [ADD] Initial Commit 'automatic_invoice_and_post'

---
+++ automatic_invoice_and_post/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Afra K (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Automatic Invoice And Post',
+    'version': "14.0.1.0.0",
+    'category': 'Sales, Warehouse, Accounting',
+    'summary': """ Auto Invoice from Delivery validate.""",
+    'description': """This module generates and post invoice
+     while validating the delivery.""",
+    'author': "Cybrosys Techno Solutions",
+    'company': "Cybrosys Techno Solutions",
+    'maintainer': "Cybrosys Techno Solutions",
+    'website': "https://www.cybrosys.com",
+    'depends': ['base', 'sale_management', 'stock', 'account'],
+    'data': ['views/res_config_settings_views.xml'],
+    'images': ['static/description/banner.png'],
+    'license': "AGPL-3",
+    'installable': True,
+    'auto_install': False,
+    'application': False
+}
