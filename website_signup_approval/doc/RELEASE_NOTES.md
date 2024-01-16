64bdbac6c63633a03a8930585dcfc4bf7fa6c370
RisvanaCybro <risvana@cybrosys.info>
2024-01-16 20:00:46 +0530

    Jan 16 : [ADD] Initial Commit 'website_signup_approval'

---
+++ website_signup_approval/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <website_signup_approval>
+
+#### 15.01.2024
+#### Version 17.0.1.0.0
+#### ADD
+- Initial Commit for Website Signup Approval.
---
+++ website_signup_approval/static/src/js/signup.js
@@ -0,0 +1,39 @@
+/** @odoo-module **/
+    import publicWidget from "@web/legacy/js/public/public_widget";
+    import { jsonrpc } from "@web/core/network/rpc_service";
+    var MySignUpForm = publicWidget.registry.SignUpForm.extend({
+        _onSubmit: function (el) {
+        /**
+        *Override onSubmit function for sending approval request
+        */
+        var file = this.$('.get_attach');
+        var email = this.$('input[name=login]').val();
+        var username = this.$('input[name=name]').val();
+        var password = this.$('input[name=password]').val();
+        //Get signup information's from user
+        const data_array = []
+        var count=0;
+        for (var doc = 0; doc < file.length; doc++) {
+              var SelectedFile = new FileReader();
+              var data = SelectedFile.readAsDataURL(file[doc].files[0]);
+              SelectedFile.addEventListener('load', (e) => {
+                 count++;
+                 const data = e.target.result;
+                 data_array.push(data)
+                 if (count===(file.length)){
+                 //Pass parameters to the route
+                      const route = jsonrpc("/web/signup/approve",
+                      {
+                          'data':data_array,
+                          'email':email,
+                          'username':username,
+                          'password':password
+                      }
+                      )
+                 }
+              });
+            }
+        },
+    });
+    publicWidget.registry.MySignUpForm = MySignUpForm;
+    return MySignUpForm;
