b66c1b220f54ced4e4ad065350bd348a1e430921
RisvanaCybro <risvana@cybrosys.info>
2024-01-17 13:12:46 +0530

    Jan 17 : [ADD] Initial Commit 'gender_contact'

---
+++ gender_contact/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Gender In Contacts',
+    'version': '17.0.1.0.0',
+    'category': "Sales",
+    'summary': """This module will helps you to give 
+                gender details for your contact.""",
+    'description': "Module helps you to manage the gender information "
+                   "of your partner as well as in the contacts.",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['contacts'],
+    'data': ['views/res_partner_views.xml'],
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ gender_contact/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <gender_contact>
+
+#### 17.01.2024
+#### Version 17.0.1.0.0
+##### ADD
+
+- Initial commit for Gender in Contacts
