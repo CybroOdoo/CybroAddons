d214a02bcfbda681b51ed6a485bc118f17e653b3
RisvanaCybro <risvana@cybrosys.info>
2024-01-18 17:31:06 +0530

    Jan 18 : [ADD] Initial Commit 'mrp_work_order_print'

---
+++ mrp_work_order_print/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author:Anjhana A K(<https://www.cybrosys.com>)
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Print Work Order Details',
+    'version': '17.0.1.0.0',
+    'category': 'Manufacturing',
+    'summary': """Generate PDF Report of Work Order""",
+    'description': """Could Generate Work Order Report of the Manufacturing
+     module that helps to generate PDF Report of the Particular Manufcturing Orders""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['mrp'],
+    'data': [
+        'report/mrp_workorder_reports.xml',
+        'report/mrp_workorder_template.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ mrp_work_order_print/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <mrp_work_order_print>
+
+#### 18.01.2024
+#### Version 17.0.1.0.0
+#### ADD
+- Initial Commit for Print Work Order Details
