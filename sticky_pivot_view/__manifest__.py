0cec219e8217de9882c8ff8acf4412d73bb1d839
RisvanaCybro <risvana@cybrosys.info>
2024-01-18 17:43:12 +0530

    Jan 18 : [ADD] Initial Commit 'sticky_pivot_view'

---
+++ sticky_pivot_view/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author:Anjhana A K(<https://www.cybrosys.com>)
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LIC15thENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Sticky Pivot View',
+    'version': '17.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': """Helps to stick the pivot view (Row and Column)""",
+    'description': """Helps ensuring persistent row and column summaries for 
+    effortless navigation and informed decision-making""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['web'],
+    'assets': {
+        'web.assets_backend': [
+            '/sticky_pivot_view/static/src/css/main.css',
+            '/sticky_pivot_view/static/src/xml/pivot_template.xml',
+        ],
+    },
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ sticky_pivot_view/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <sticky_pivot_view>
+
+#### 16.01.2024
+#### Version 17.0.1.0.0
+#### ADD 
+
+- Initial Commit for Sticky Pivot View
