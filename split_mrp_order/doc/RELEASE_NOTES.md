07f97b368135fe77ebc41a627eb93044c2c7d745
RisvanaCybro <risvana@cybrosys.info>
2024-01-19 15:19:16 +0530

    Jan 19 : [ADD] Initial Commit 'split_mrp_order'

---
+++ split_mrp_order/doc/RELEASE_NOTES.md
@@ -0,0 +1,5 @@
+## Module <split_mrp_order>
+#### 16.01.2024
+#### Version 16.0.1.0.0
+#### ADD
+- Initial commit for Manufacturing Orders Splitting
---
+++ split_mrp_order/models/mrp_production.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+from odoo import models, exceptions
+
+
+class MrpProduction(models.Model):
+    """ Open wizard."""
+    _inherit = 'mrp.production'
+
+    def action_split_order(self):
+        """ Open wizard when click on 'split mo' button """
+        if self.product_qty <= 1:
+            raise exceptions.UserError(
+                "Here, splitting is not possible. Enter enough quantity.")
+
+        work_order = self.env.user.has_group('mrp.group_mrp_routings')
+        if work_order:
+            context = {
+                'default_order_id': self._origin.id,
+                'default_works': True
+            }
+        else:
+            context = {
+                'default_order_id': self._origin.id,
+            }
+        return {
+            'name': 'Split Order',
+            'res_model': 'split.order',
+            'view_mode': 'form',
+            'view_type': 'form',
+            'type': 'ir.actions.act_window',
+            'target': 'new',
+            'context': context
+        }
