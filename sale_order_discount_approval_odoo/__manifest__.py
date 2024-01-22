d3326a01cd7d8326bcb63f6d5905b4b47d6b3c17
RisvanaCybro <risvana@cybrosys.info>
2024-01-22 18:41:13 +0530

    Jan 22 : [ADD] Initial Commit 'sale_order_discount_approval_odoo'

---
+++ sale_order_discount_approval_odoo/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Gayathri V(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Sale Order Discount Approval',
+    'version': '17.0.1.0.0',
+    'category': 'Sales',
+    'summary': 'Sale Order Discount Approval based on the Allowed discount values',
+    'description': """Module for discount approval of sales orders if 
+     discount limit exceeded of allowed discount of user.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': [
+        'sale_management',
+    ],
+    'data': [
+        'security/sale_order_discount_approval_odoo_groups.xml',
+        'view/res_users_views.xml',
+        'view/sale_order_views.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ sale_order_discount_approval_odoo/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <sale_order_discount_approval_odoo>
+
+#### 22.01.2024
+#### Version 17.0.1.0.0
+#### ADD
+
+- Initial commit for Sale Order Discount Approval
