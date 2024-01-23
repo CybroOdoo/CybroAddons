89cb4107c04be9afa62ffaff486fc8b4d074df4d
RisvanaCybro <risvana@cybrosys.info>
2024-01-23 10:03:11 +0530

    Jan 23 : [ADD] Initial Commit 'customer_group_pricelist'

---
+++ customer_group_pricelist/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <customer_group_pricelist>
+
+#### 22.01.2024
+#### Version 17.0.1.0.0
+#### ADD
+
+- Initial commit for Customer Group PriceList
---
+++ customer_group_pricelist/models/sale_order.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Jumana Haseen (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+from odoo import api, models
+
+
+class SaleOrder(models.Model):
+    """Inheriting Sale Order model to activate the
+       price-list based on customer group."""
+    _inherit = 'sale.order'
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        """Price list will be added based on the customer group"""
+        for group in self.env['customer.group'].search([]):
+            contacts = group.mapped('contact_ids').ids
+            if self.partner_id.id in contacts:
+                self.pricelist_id = group.pricelist_id.id
