e22e46123cf0f7e27b106c54cad772705778830b
RisvanaCybro <risvana@cybrosys.info>
2024-01-25 20:00:33 +0530

    Jan 25 : [ADD] Initial Commit 'survey_question_duplicator'

---
+++ survey_question_duplicator/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Cybrosys Techno Solutions (Contact : odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+{
+    'name': 'Question Duplicator In Survey',
+    'version': '17.0.1.0.0',
+    'category': 'Marketing',
+    'summary': """Option for duplicating survey questions.""",
+    'description': """In this module, users can get an option for duplicating 
+     questions for differnt survey.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base','survey'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/action_question_duplicate.xml',
+        'wizards/question_duplicate_views.xml'
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
