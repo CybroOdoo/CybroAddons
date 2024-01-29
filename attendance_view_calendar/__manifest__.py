8e6c0ccda9779e822f19a8e89377a8a6bc289409
RisvanaCybro <risvana@cybrosys.info>
2024-01-29 22:18:25 +0530

    Jan 29 : [ADD] Initial Commit 'attendance_view_calendar'

---
+++ attendance_view_calendar/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Attendance Calendar View',
+    'version': '17.0.1.0.0',
+    'category': 'Human Resources',
+    'summary': 'Calender View for Attendance Module.',
+    'description': 'This module will help you to create timesheet'
+                   'for manufacturing orders according to the '
+                   'work order time tracking.',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['base', 'hr_attendance',  'hr_attendance_gantt'],
+    'data': [
+        'views/hr_attendance_calendar_view.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
