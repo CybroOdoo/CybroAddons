a986c2edfe362ddb8e0b8d6d785d27325c5db988
RisvanaCybro <risvana@cybrosys.info>
2024-01-29 22:19:05 +0530

    Jan 29 : [ADD] Initial Commit 'openai_odoo_base'

---
+++ openai_odoo_base/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Anagha S (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'OpenAI Odoo Base',
+    'version': '17.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Seamlessly integrates OpenAI capabilities into the Odoo.',
+    'description': """This module helps to effortlessly integrate your OpenAI 
+     API key within Odoo settings.Once set up, unleash the power of OpenAI 
+     throughout the Odoo platform, amplifying your applications with advanced
+     AI functionalities.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'base_setup'],
+    'data': ['views/res_config_settings_views.xml'],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ openai_odoo_base/doc/RELEASE_NOTES.md
@@ -0,0 +1,8 @@
+## Module <openai_odoo_base>
+
+#### 26.01.2024
+#### Version 17.0.1.0.0
+#### ADD
+
+- Initial commit for OpenAI Odoo Base
+
