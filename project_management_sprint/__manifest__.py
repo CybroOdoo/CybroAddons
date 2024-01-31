9066eaeb7351f6fd6de71108d42677c51e630ef7
RisvanaCybro <risvana@cybrosys.info>
2024-01-31 11:51:33 +0530

    Jan 31: [ADD] Initial Commit 'project_management_sprint'

---
+++ project_management_sprint/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Bhagyadev K P (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+{
+    'name': 'Project Sprint',
+    'version': '16.0.1.0.0',
+    'category': 'Project',
+    'summary': 'A sprint is a fixed time period where teams complete work from'
+               ' their product backlog',
+    'description': 'In this app we have added the features of a sprint which is'
+                   ' a feaure in Jira software. A sprint is a fixed time period'
+                   ' where teams complete work from their product backlog',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['project'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/project_sprint_views.xml',
+        'views/project_project_views.xml',
+        'views/project_task_views.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ project_management_sprint/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <project_management_sprint>
+
+#### 28.01.2024
+#### Version 16.0.1.0.0
+##### ADD
+
+- Initial Commit for Project Sprint
