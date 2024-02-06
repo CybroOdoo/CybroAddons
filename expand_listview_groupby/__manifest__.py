3a52c5a75c4fd5d5e3d8c709c18da4b84d984171
RisvanaCybro <risvana@cybrosys.info>
2024-02-06 13:17:44 +0530

    Feb 06 : [ADD] Initial Commit 'expand_listview_groupby'

---
+++ expand_listview_groupby/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Anurudh P (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+{
+    'name': 'Expanding Listview In GroupBy',
+    'version': '17.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Allows to expanded the records that folded by GroupBy filter',
+    'description': 'By simply clicking a Button the records that folded will '
+                   'be unfold and arrange in List view',
+    'author': 'Cybrosys Techno solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['web', 'base'],
+    'assets': {
+        'web.assets_backend': [
+            'expand_listview_groupby/static/src/xml/expand_list_templates.xml',
+            'expand_listview_groupby/static/src/js/expand_list.js',
+        ],
+    },
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ expand_listview_groupby/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <expand_listview_groupby>
+
+#### 02.02.2024
+#### Version 17.0.1.0.0
+#### ADD
+- Initial commit for Expanding Listview In GroupBy
