e85e596caad464dfa398970df54dd1799e4afae6
RisvanaCybro <risvana@cybrosys.info>
2024-02-07 11:53:48 +0530

    Feb 07 : [ADD] Initial Commit 'force_availability_in_stock'

---
+++ force_availability_in_stock/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Jumana Haseen (<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+{
+    'name': 'Force Availability Button in Delivery',
+    'version': '17.0.1.0.0',
+    'category': 'Warehouse',
+    'summary': 'Make goods available forcefully in the time of '
+               'delivery when there is an availability problem occurs.',
+    'description': """This module will help you to make goods available 
+    forcefully in the time of delivery and deliver goods to the customer 
+    without any interruptions when there is an availability problem occurs.
+    Force availability button in delivery/stock picking.Can change done 
+    quantities just by a button click.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['base', 'sale_management', 'account', 'stock'],
+    'data': [
+        'views/stock_picking_views.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ force_availability_in_stock/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <force_availability_in_stock>
+
+#### 18.01.2024
+#### Version 17.0.1.0.0
+#### ADD
+- Initial Commit for Force Availability Button in Delivery.
