eb51c9ffdc9f9c8789aef884d52ebe0f2f779324
RisvanaCybro <risvana@cybrosys.info>
2024-02-12 10:09:28 +0530

    Feb 12 : [ADD] Initial Commit 'odoo_product_tags'

---
+++ odoo_product_tags/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': "Odoo Product Tags",
+    'version': '17.0.1.0.0',
+    "category": 'Inventory',
+    'summary': """This Module helps to manage products easily using tags""",
+    'description': """This Module helps to manage the products easily using 
+    tags and we can easily apply the product tags for the multiple products.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'sale_management'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_template_views.xml',
+        'views/product_product_views.xml',
+        'views/res_config_settings_views.xml',
+        'wizard/product_multiple_tag_views.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': "LGPL-3",
+    'installable': True,
+    'auto_install': False,
+    'application': False
+}
---
+++ odoo_product_tags/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <odoo_product_tags>
+
+#### 24.01.2024
+#### Version 17.0.1.0.0
+#### ADD
+- Initial commit for Odoo Product Tags
