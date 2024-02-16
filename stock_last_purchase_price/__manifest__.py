d5bb724aa559630c401758d8f04969a22727a2e1
RisvanaCybro <risvana@cybrosys.info>
2024-02-16 16:28:51 +0530

    Feb 16 : [ADD] Initial Commit 'stock_last_purchase_price'

---
+++ stock_last_purchase_price/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Fathima Mazlin AM (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+{
+    'name': 'Costing Method: Last Purchase Price',
+    'version': '17.0.1.0.0',
+    'category': 'Warehouse',
+    'summary': "Introducing new costing method in Odoo 'last purchase price'",
+    'description': """Introducing new costing method in Odoo 
+    'last purchase price'.The cost of the product changed based on the last 
+    purchase order.""",
+    'author': 'Cybrosys Techno solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.openhrms.com',
+    'depends': ['stock_account', 'purchase', 'account_accountant'],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ stock_last_purchase_price/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <stock_last_purchase_price>
+
+#### 14.02.2024
+#### Version 17.0.1.0.0
+#### ADD
+- Initial commit for Costing Method: Last Purchase Price
