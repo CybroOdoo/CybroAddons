3d0f60867cfb41f86a112b8ef54e21ec239d692a
RisvanaCybro <risvana@cybrosys.info>
2024-02-16 16:26:37 +0530

    Feb 16 : [ADD] Initial Commit 'delivery_date_ecommerce'

---
+++ delivery_date_ecommerce/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Name of Developer (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Delivery Date on eCommerce',
+    'version': '17.0.1.0.0',
+    "category": "eCommerce",
+    'summary': """Expected Delivery Date Will Be Displayed On The eCommerce""",
+    'description': 'This module is very useful for end users '
+                   'to track their products.',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['base', 'web', 'website_sale', 'stock'],
+    'data': [
+        'views/res_config_settings_views.xml',
+        'views/website_sale_templates.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ delivery_date_ecommerce/doc/RELEASE_NOTES.md
@@ -0,0 +1,5 @@
+## Module <delivery_date_ecommerce>
+#### 14.02.2024
+#### Version 17.0.1.0.0
+#### ADD
+- Initial commit for Delivery Date on eCommerce
