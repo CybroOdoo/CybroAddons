2ef6c727c4ef53132d9a5ad06e3fe4f05151050e
RisvanaCybro <risvana@cybrosys.info>
2024-02-19 19:12:02 +0530

    Feb 19 : [ADD] Initial Commit 'autosuggestion_in_discuss'

---
+++ autosuggestion_in_discuss/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <autosuggestion_in_discuss>
+
+#### 30.01.2024
+#### Version 17.0.1.0.0
+#### ADD
+- Initial commit for Autosuggestion in Discuss
---
+++ autosuggestion_in_discuss/static/src/js/composer.js
@@ -0,0 +1,176 @@
+/** @odoo-module **/
+import { Composer } from "@mail/core/common/composer";
+import { useService } from "@web/core/utils/hooks";
+import { patch } from "@web/core/utils/patch";
+import { jsonrpc } from "@web/core/network/rpc_service";
+patch(Composer.prototype, {
+        setup() {
+            super.setup();
+            },
+        onKeydown(ev) {
+            /*While keydown works on a keyboard this function takes the value in the input area and
+            compares it with the ones in the list and finds a proper match.On clicking the tab the full
+            sentence or word will be available.*/
+            let autoCompleteTextAreaEl = document.querySelector('#autoCompleteText');
+            let userTextAreaEl = document.querySelector('#userInputText');
+            var value = userTextAreaEl.value;
+            jsonrpc('/web/dataset/call_kw', {
+                model: 'mail.message',
+                method: 'get_message',
+                args: [],
+                kwargs: {},
+            }).then(function (data) {
+                    var search_terms = data
+                    if (value !== '/') {
+                        /*Matching sentences or words are taken from the list search_terms*/
+                        value = value.replace(new RegExp("\\\\", "g"), "\\\\");
+                        const matcher = new RegExp(`^${value}`, 'g');
+                        var filter_list = search_terms.filter(word => word.match(matcher));
+                        filter_list.sort();
+                        if (filter_list[0] == null) {
+                            autoCompleteTextAreaEl.value = ' ';
+                        }
+                        else {
+                            autoCompleteTextAreaEl.value = filter_list[0];
+                        }
+                        if (value == '') {
+                            autoCompleteTextAreaEl.value = " ";
+                        }
+                        switch (ev.key) {
+                            case 'Tab':
+                                userTextAreaEl.value = filter_list[0];
+                                autoCompleteTextAreaEl.value = ' ';
+                            case ' ':
+                                autoCompleteTextAreaEl.value = ' ';
+                            case 'Enter':
+                                autoCompleteTextAreaEl.value = ' ';
+                        }
+                    }
+                })
+            if (ev.key === 'Escape') {
+                if (isEventHandled(ev, 'ComposerTextInput.closeSuggestions')) {
+                    return;
+                }
+                if (isEventHandled(ev, 'Composer.closeEmojisPopover')) {
+                    return;
+                }
+                ev.preventDefault();
+                this.discard();
+            }
+        },
+        onKeyupTextarea(ev) {
+            /*While keyup works on a keyboard this function takes the value in the input area and
+             compares it with the ones in the list and finds a proper match.While using onKeyupTextarea()
+             the value from the text area will be available without  any delay.*/
+            let autoCompleteTextAreaEl = document.querySelector('#autoCompleteText');
+            let userTextAreaEl = document.querySelector('#userInputText');
+            var value = userTextAreaEl.value;
+            jsonrpc('/web/dataset/call_kw', {
+                model: 'mail.message',
+                method: 'get_message',
+                args: [],
+                kwargs: {},
+                }).then(function (data) {
+                    var search_terms = data
+                    if (value !== '/') {
+                        /*Matching sentences or words are taken from the list search_terms*/
+                        value = value.replace(new RegExp("\\\\", "g"), "\\\\");
+                        const matcher = new RegExp(`^${value}`, 'g');
+                        var filter_list = search_terms.filter(word => word.match(matcher));
+                        filter_list.sort();
+                        if (filter_list[0] == null) {
+                            autoCompleteTextAreaEl.value = ' ';
+                        }
+                        else {
+                            autoCompleteTextAreaEl.value = filter_list[0];
+                        }
+                        if (value == '') {
+                            autoCompleteTextAreaEl.value = " ";
+                        }
+                        switch (ev.key) {
+                            case 'ArrowRight':
+                                userTextAreaEl.value = filter_list[0];
+                                autoCompleteTextAreaEl.value = ' ';
+                            case ' ':
+                                autoCompleteTextAreaEl.value = ' ';
+                            case 'Enter':
+                                autoCompleteTextAreaEl.value = ' ';
+                        }
+                    }
+                })
+            switch (ev.key) {
+                case 'Escape':
+                    // already handled in _onKeydownTextarea, break to avoid default
+                    break;
+                // ENTER, HOME, END, UP, DOWN, PAGE UP, PAGE DOWN, TAB: check if navigation in mention suggestions
+                case 'Enter':
+                    if (this.hasSuggestions) {
+                        this.insertSuggestion();
+                        this.closeSuggestions();
+                        this.update({ doFocus: true });
+                    }
+                    break;
+                case 'ArrowUp':
+                case 'PageUp':
+                    if (ev.key === 'ArrowUp' && !this.hasSuggestions && !this.composer.textInputContent && this.threadView) {
+                        this.threadView.startEditingLastMessageFromCurrentUser();
+                        break;
+                    }
+                    if (this.composerSuggestionListView) {
+                        this.composerSuggestionListView.setPreviousSuggestionViewActive();
+                        this.composerSuggestionListView.update({ hasToScrollToActiveSuggestionView: true });
+                    }
+                    break;
+                case 'ArrowDown':
+                case 'PageDown':
+                    if (ev.key === 'ArrowDown' && !this.hasSuggestions && !this.composer.textInputContent && this.threadView) {
+                        this.threadView.startEditingLastMessageFromCurrentUser();
+                        break;
+                    }
+                    if (this.composerSuggestionListView) {
+                        this.composerSuggestionListView.setNextSuggestionViewActive();
+                        this.composerSuggestionListView.update({ hasToScrollToActiveSuggestionView: true });
+                    }
+                    break;
+                case 'Home':
+                    if (this.composerSuggestionListView) {
+                        this.composerSuggestionListView.setFirstSuggestionViewActive();
+                        this.composerSuggestionListView.update({ hasToScrollToActiveSuggestionView: true });
+                    }
+                    break;
+                case 'End':
+                    if (this.composerSuggestionListView) {
+                        this.composerSuggestionListView.setLastSuggestionViewActive();
+                        this.composerSuggestionListView.update({ hasToScrollToActiveSuggestionView: true });
+                    }
+                    break;
+                case 'Tab':
+                    if (this.composerSuggestionListView) {
+                        if (ev.shiftKey) {
+                            this.composerSuggestionListView.setPreviousSuggestionViewActive();
+                            this.composerSuggestionListView.update({ hasToScrollToActiveSuggestionView: true });
+                        } else {
+                            this.composerSuggestionListView.setNextSuggestionViewActive();
+                            this.composerSuggestionListView.update({ hasToScrollToActiveSuggestionView: true });
+                        }
+                    }
+                    break;
+                case 'Alt':
+                case 'AltGraph':
+                case 'CapsLock':
+                case 'Control':
+                case 'Fn':
+                case 'FnLock':
+                case 'Hyper':
+                case 'Meta':
+                case 'NumLock':
+                case 'ScrollLock':
+                case 'Shift':
+                case 'ShiftSuper':
+                case 'Symbol':
+                case 'SymbolLock':
+                    // prevent modifier keys from resetting the suggestion state
+                    break;
+            }
+        },
+    });
