735fd6428127d8e0fa4c39aeecd7491dc2c1abce
RisvanaCybro <risvana@cybrosys.info>
2024-02-22 18:23:47 +0530

    Feb 22 : [ADD] Initial Commit 'custom_robots_txt'

---
+++ custom_robots_txt/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Aysha Shalin (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Website Robots.Txt',
+    'version': '17.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Custom Robots.txt file for website in odoo 17',
+    'description': """This module enables users to customize their robots.txt
+    file according to specific needs and preferences, providing flexibility in
+    configuring directives for search engine crawlers.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['website'],
+    'data': [
+            'wizards/website_robots_view.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ custom_robots_txt/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <custom_robots_txt>
+
+#### 17.02.2024
+#### Version 17.0.1.0.0
+##### ADD
+- Initial commit for Website Robots.Txt
