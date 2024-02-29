d272291bf2c6b22098de33707052642462de1605
RisvanaCybro <risvana@cybrosys.info>
2024-02-29 17:24:43 +0530

    Feb 29 : [ADD] Initial Commit 'restrict_delivery_method'

---
+++ restrict_delivery_method/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <restrict_delivery_method>
+
+#### 31.01.2024
+#### Version 17.0.1.0.0
+#### ADD
+ - Initial Commit for Restrict Delivery Method
---
+++ restrict_delivery_method/wizards/choose_delivery_carrier.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Sabeel B (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from odoo import fields, models
+
+
+class ChooseDeliveryCarrier(models.TransientModel):
+    """This class contains the fields in wizard which is related
+    to that particular sale order"""
+    _inherit = "choose.delivery.carrier"
+
+    delivery_method_ids = fields.Many2many('delivery.carrier',
+                                           related='order_id.'
+                                                   'delivery_method_ids',
+                                           help="Choose the delivery method")
