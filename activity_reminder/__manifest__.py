6a6d5a940ef53d5a46f53afb4029c8c7e65750fa
RisvanaCybro <risvana@cybrosys.info>
2024-02-29 16:55:07 +0530

    Feb 29 : [ADD] Initial Commit 'activity_reminder'

---
+++ activity_reminder/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Fathima Mazlin AM (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Activity Reminder',
+    'version': '15.0.1.0.0',
+    'category': 'Discuss',
+    'summary': "Setting a reminder email to employees for their activity ",
+    'description': 'Setting a reminder email to employees for their '
+                   'activity. Based on user wise we can set activity '
+                   'reminder.',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': [
+        'mail', 'base'],
+    'data':
+        [
+            'security/activity_reminder_groups.xml',
+            'data/ir_cron_data.xml',
+            'views/mail_activity_views.xml',
+        ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
