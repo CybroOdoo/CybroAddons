8b1d610aeee2b6205c63f765ce155f96c9650f97
RisvanaCybro <risvana@cybrosys.info>
2024-03-06 20:29:53 +0530

    Mar 06 : [ADD] Initial Commit 'auto_daily_weekly_report'

---
+++ auto_daily_weekly_report/README.rst
@@ -0,0 +1,47 @@
+.. image:: https://img.shields.io/badge/license-AGPL--3-blue.svg
+    :target: https://www.gnu.org/licenses/agpl-3.0-standalone.html
+    :alt: License: AGPL-3
+
+Daily/Weekly Task Status Report
+===============================
+This module has the feature to sent the daily and weekly report automatically
+based on the entered timesheet.
+
+Company
+-------
+* `Cybrosys Techno Solutions <https://cybrosys.com/>`__
+
+License
+-------
+GNU Affero General Public License v3.0 (AGPL v3)
+(http://www.gnu.org/licenses/agpl-3.0-standalone.html)
+
+Credits
+-------
+Developer:
+            (V15) Aswathi C,
+            (V16) Jumana Haseen,
+            (V17) Jumana Haseen,
+Contact: odoo@cybrosys.com
+
+Contacts
+--------
+* Mail Contact : odoo@cybrosys.com
+* Website : https://cybrosys.com
+
+Bug Tracker
+-----------
+Bugs are tracked on GitHub Issues. In case of trouble, please check there if your issue has already been reported.
+
+Maintainer
+==========
+.. image:: https://cybrosys.com/images/logo.png
+   :target: https://cybrosys.com
+
+This module is maintained by Cybrosys Technologies.
+
+For support and more information, please visit `Our Website <https://cybrosys.com/>`__
+
+Further information
+===================
+HTML Description: `<static/description/index.html>`__
---
+++ auto_daily_weekly_report/models/account_analytic_line.py
@@ -0,0 +1,114 @@
+"""Daily/weekly task status report"""
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Jumana Haseen (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+import datetime
+from odoo import models
+
+
+class AccountAnalyticLine(models.Model):
+    """
+    Class inherits account.analytic.line to send the daily and weekly report as
+    email.
+    """
+    _inherit = 'account.analytic.line'
+
+    def create_daily_report(self):
+        """
+        Method for automatically send email for daily work report.
+        """
+        employee_ids = self.env['hr.employee'].search(
+            [('parent_id', '!=', False)])
+        template = self.env.ref(
+            'auto_daily_weekly_report.email_template_daily_report',
+            raise_if_not_found=False)
+        for employee in employee_ids:
+            user_email = employee.work_email
+            working_days = employee.resource_calendar_id.attendance_ids.mapped(
+                'dayofweek')
+            account_analytic_line_ids = self.env[
+                'account.analytic.line'].search(
+                [('date', '=', datetime.datetime.today().date()),
+                 ('employee_id', '=', employee.id)]
+            ).filtered(
+                lambda x: x.date.weekday() in [int(day) for day in
+                                               working_days])
+            lines = []
+            for record in account_analytic_line_ids:
+                lines.append({
+                    'project': record.project_id.name,
+                    'task': record.task_id.name,
+                    'description': record.name,
+                    'hours_spent': record.unit_amount
+                })
+            ctx = {
+                'data': lines,
+                'from_email': user_email,
+                'to_email': employee.parent_id.work_email,
+                'employee': employee.name,
+            }
+            if len(lines) >= 1:
+                template.sudo().with_context(ctx).send_mail(employee.user_id.id,
+                                                            force_send=True)
+
+    def create_weekly_report(self):
+        """
+        Method for automatically send email for weekly task report.
+        """
+        today_date = datetime.datetime.today()
+        week_num = today_date.isocalendar()[1]
+        start_date = today_date - datetime.timedelta(
+            days=datetime.datetime.today().isoweekday() % 7)
+        employee_ids = self.env['hr.employee'].search(
+            [('parent_id', '!=', False)])
+        template = self.env.ref(
+            'auto_daily_weekly_report.email_template_weekly_report',
+            raise_if_not_found=False)
+        for employee in employee_ids:
+            user_email = employee.work_email
+            working_days = employee.resource_calendar_id.attendance_ids.mapped(
+                'dayofweek')
+            account_analytic_line_ids = self.env[
+                'account.analytic.line'].search(
+                [('date', '>=', start_date),
+                 ('date', '<=', datetime.datetime.today().date()),
+                 ('employee_id', '=', employee.id)]
+            ).filtered(
+                lambda x: x.date.weekday() in [int(day) for day in
+                                               working_days])
+            lines = []
+            for record in account_analytic_line_ids:
+                lines.append({
+                    'project': record.project_id.name,
+                    'task': record.task_id.name,
+                    'description': record.name,
+                    'hours_spent': record.unit_amount
+                })
+            ctx = {
+                'data': lines,
+                'week': "W" + str(week_num) + "_" + str(today_date.year),
+                'from_email': user_email,
+                'to_email': employee.parent_id.work_email,
+                'employee': employee.name,
+            }
+            if len(lines) >= 1:
+                template.sudo().with_context(ctx).send_mail(employee.user_id.id,
+                                                            force_send=True)
