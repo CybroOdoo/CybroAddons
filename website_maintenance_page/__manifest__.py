e66e1879fe4fc90e6a8ed63a88d722090f0ac90b
RisvanaCybro <risvana@cybrosys.info>
2024-03-16 17:17:24 +0530

    Mar 16: [ADD] Initial Commit 'website_maintenance_page'

---
+++ website_maintenance_page/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Website Maintenance Page',
+    'version': '17.0.1.0.0',
+    'category': 'Website',
+    'summary': 'Shows maintenance page for public users',
+    'description': 'The app allows users to redirect all website public '
+                   'pages to maintenance pages',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['website_sale'],
+    'data': ['views/res_config_settings_views.xml',
+             'views/maintenance_templates.xml'
+             ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ website_maintenance_page/doc/RELEASE_NOTES.md
@@ -0,0 +1,5 @@
+## Module <website_maintenance_page>
+#### 12.03.2024
+#### Version 17.0.1.0.0
+#### ADD
+- Initial commit for Website Maintenance Page Module
