d6c75fe60b689cea7b85e9191cc9b9ee47b48d7c
AjmalCybro <ajmal@cybrosys.in>
2024-03-21 14:32:49 +0530

    Mar 21 [UPDT] : Index Updated 'auto_database_backup'

--- auto_database_backup/__manifest__.py
+++ auto_database_backup/__manifest__.py
@@ -27,7 +27,7 @@
     'summary': 'Generate automatic backup of databases and store to local, '
                'google drive, dropbox, nextcloud, amazon S3, onedrive or '
                'remote server, Odoo17',
-    'description': 'automatic database backup, odoo17, odoo apps,backup, automatic backup,odoo17 automatic database backup,backup google drive,backup dropbox, backup nextcloud, backup amazon S3, backup onedrive',
+    'description': 'Odoo Database Backup, Database Backup, Automatci Backup, automatic database backup, odoo17, odoo apps,backup, automatic backup,odoo17 automatic database backup,backup google drive,backup dropbox, backup nextcloud, backup amazon S3, backup onedrive',
     'author': "Cybrosys Techno Solutions",
     'maintainer': 'Cybrosys Techno Solutions',
     'company': 'Cybrosys Techno Solutions',
