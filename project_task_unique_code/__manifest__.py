6dce4d91b62d6b75b771358c35a983f88f8f556b
RisvanaCybro <risvana@cybrosys.info>
2024-03-25 20:10:27 +0530

    Mar 25: [ADD] Initial Commit 'project_task_unique_code'

---
+++ project_task_unique_code/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Jumana Haseen (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#
+###############################################################################
+{
+    'name': 'Project Task Unique Code',
+    'version': '17.0.1.0.0',
+    'category': 'Project',
+    'summary': """Auto Generated Task Unique Code""",
+    'description': """"This module helps you to track each tasks with the
+    unique code which is generated automatically.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "http://www.cybrosys.com",
+    'depends': ['base', 'project'],
+    'data': [
+        'views/project_project_views.xml',
+        'views/project_task_views.xml'
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ project_task_unique_code/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <project_task_unique_code>
+
+#### 25.03.2024
+#### Version 17.0.1.0.0
+#### ADD
+
+- Initial commit for Project Task Unique Code
