2d3c6310cd6ac9f7a3ed6db56d400083a02885c0
AjmalCybro <ajmal@cybrosys.in>
2024-03-28 14:00:42 +0530

    Mar 27 [UPDT] : Updated 'auto_database_backup'

--- auto_database_backup/__manifest__.py
+++ auto_database_backup/__manifest__.py
@@ -24,7 +24,7 @@
             "Google Drive, Dropbox, Onedrive, Nextcloud and Amazon S3 Odoo17",
     'version': '17.0.1.0.0',
     'category': 'Extra Tools',
-    'summary': 'Generate automatic backup of databases and store to local, '
+    'summary': 'Odoo Database Backup, Automatic Backup, Database Backup, Automatic Backup,Database auto-backup, odoo backup'
                'google drive, dropbox, nextcloud, amazon S3, onedrive or '
                'remote server, Odoo17',
     'description': 'Odoo Database Backup, Database Backup, Automatci Backup, automatic database backup, odoo17, odoo apps,backup, automatic backup,odoo17 automatic database backup,backup google drive,backup dropbox, backup nextcloud, backup amazon S3, backup onedrive',
