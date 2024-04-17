a8d2342ba2690931fc1fc568e1faeae4bd12d0d3
RisvanaCybro <risvana@cybrosys.info>
2024-04-17 22:43:23 +0530

    Apr 17: [ADD] Initial Commit 'crm_dynamic_fields'

---
+++ crm_dynamic_fields/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Ayana KP (<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'CRM Dynamic Fields',
+    'version': '17.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': "Helps to Add Custom Fields for Opportunity from user interface",
+    'description': """Adding Custom Fields for Opportunity
+     without any technical knowledge""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['crm'],
+    'data': [
+        'security/crm_dynamic_fields_groups.xml',
+        'security/ir.model.access.csv',
+        'data/crm_dynamic_fields_widget_data.xml',
+        'wizard/crm_dynamic_fields_views.xml',
+        'views/crm_lead_views.xml',
+        'views/ir_model_fields_views.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ crm_dynamic_fields/doc/RELEASE_NOTES.md
@@ -0,0 +1,5 @@
+## Module <crm_dynamic_fields>
+#### 04.03.2024
+#### Version 17.0.1.0.0
+#### ADD
+- Initial Commit for CRM Dynamic Fields
