15bd7778ad6d93d77b9c1b0529b440d9be92d40a
Shijin V <shijin@cybrosys.in>
2024-04-20 09:48:15 +0530

    APR 20: [ADD] Initial Commit 'website_subscription_package'

---
+++ website_subscription_package/__manifest__.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Anagha S (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Website Subscription Package Management',
+    'version': '16.0.1.0.0',
+    'category': 'Sales, Website',
+    'summary': 'Subscription Package Management Through website',
+    'description': """This Module Create Subscription for A Product From 
+     Website""",
+    'author': 'Cybrosys Techno solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['subscription_package', 'website_sale'],
+    'data': [
+        'data/mail_template_data.xml',
+        'security/ir.model.access.csv',
+        'views/recurrence_views.xml',
+        'views/subscription_package_views.xml',
+        'views/product_product_views.xml',
+        'views/product_template_views.xml',
+        'views/sale_order_views.xml',
+        'views/portal_templates.xml',
+        'views/website_product_subscription_template.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'website_subscription_package/static/src/js/website_sale.js',
+        ],
+    },
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ website_subscription_package/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <website_subscription_package>
+
+#### 20.04.2024
+#### Version 16.0.1.0.0
+#### ADD
+- Initial commit for Website Subscription Package Management
