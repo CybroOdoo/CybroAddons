42818166d7dfda53bacb9cccd55b8813e2da3cb9
AjmalCybro <ajmal@cybrosys.in>
2024-04-22 14:19:53 +0530

    Apr 22 [IMP] : Updated 'custom_receipts_for_pos'

--- custom_receipts_for_pos/__manifest__.py
+++ custom_receipts_for_pos/__manifest__.py
@@ -22,7 +22,7 @@
 ###################################################################################
 {
     'name': 'POS Receipt Design',
-    'version': '17.0.1.0.0',
+    'version': '17.0.1.0.1',
     'category': 'Point of Sale',
     'summary': """Option to select the customised Receipts for each POS""",
     'description': "Option to select the customised Receipts for each POS,"
--- custom_receipts_for_pos/doc/RELEASE_NOTES.md
+++ custom_receipts_for_pos/doc/RELEASE_NOTES.md
@@ -2,5 +2,10 @@
 
 #### 05.02.2024
 #### Version 17.0.1.0.0
-#### ADD
+##### ADD
 - Initial Commit for POS Receipt Design
+
+#### 17.04.2024
+#### Version 17.0.1.0.1
+##### BUGFIX
+- Fixed the error in printing the receipt
