f38ebe32e5e9436b56aa6fb6371cb9d7571cc429
RisvanaCybro <risvana@cybrosys.info>
2024-05-06 20:24:04 +0530

    May 06: [ADD] Initital Commit 'partner_search_by_number'

---
+++ partner_search_by_number/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Ayana KP (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+{
+    'name': 'Partner Search By Number',
+    'version': '17.0.1.0.0',
+    'summary': """ This app allows us to search customer by their phone number
+     and mobile numbers.""",
+    'description': """ With the help of this software, we can search for
+     customers using their phone and cell numbers in Sale and Purchase mosule. """,
+    'category': 'Sales',
+    'author': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['contacts'],
+    'data':
+        [
+            'views/res_partner_views.xml'
+        ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ partner_search_by_number/doc/RELEASE_NOTES.md
@@ -0,0 +1,5 @@
+## Module <partner_search_by_number>
+#### 30.04.2024
+#### Version 17.0.1.0.0
+#### ADD
+-initial Commit for Partner Search By Number
