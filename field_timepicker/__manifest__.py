4565ee87e71661929654f9ba1356c7ead2242483
RisvanaCybro <risvana@cybrosys.info>
2024-05-10 17:34:04 +0530

    May 10: [FIX] Bug Fixed 'field_timepicker'

--- field_timepicker/__manifest__.py
+++ field_timepicker/__manifest__.py
@@ -33,9 +33,9 @@
     'depends': ['web'],
     "assets": {
         'web.assets_backend': [
-            'field_timepicker/static/wickedpicker/dist/wickedpicker.min.css',
-            'field_timepicker/static/wickedpicker/dist/wickedpicker.min.js',
-            'field_timepicker/static/src/xml/timepicker_templates.xml',
+            'field_timepicker/static/wickedpicker/stylesheets/wickedpicker.css',
+            'field_timepicker/static/wickedpicker/src/wickedpicker.js',
+            'field_timepicker/static/src/xml/timepicker.xml',
             'field_timepicker/static/src/js/time_widget.js',
         ],
     },
