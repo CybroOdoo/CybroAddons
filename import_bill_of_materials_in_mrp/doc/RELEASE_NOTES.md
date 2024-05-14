fbf704862df532f43f586e71f4303a97c68c2fbe
RisvanaCybro <risvana@cybrosys.info>
2024-05-14 21:12:52 +0530

    May 14: [ADD] Initital Commit 'import_bill_of_materials_in_mrp'

---
+++ import_bill_of_materials_in_mrp/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <import_bill_of_materials_in_mrp>
+
+#### 24.04.2024
+#### Version 15.0.1.0.0
+#### ADD
+- Initial commit for Import Bill Of Materials
---
+++ import_bill_of_materials_in_mrp/wizards/success_message.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Saneen K (<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+from odoo import models, fields
+
+
+class SuccessWizard(models.TransientModel):
+    """model for adding warning message """
+
+    _name = 'success.message'
+    _description = "Show Message"
+
+    message = fields.Text('Success', required=True)
+
+    def action_close(self):
+        """function for close button"""
+        return {'type': 'ir.actions.act_window_close'}
