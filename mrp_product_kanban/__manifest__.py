f4cfdf9776ddb217ee56905fd76b9f9a339823bb
RisvanaCybro <risvana@cybrosys.info>
2024-06-14 14:55:05 +0530

    Jun 14: [ADD] Initial Commit 'mrp_product_kanban'

---
+++ mrp_product_kanban/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author:  Aysha Shalin (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': "Manufacturing Product Kanban",
+    'version': '15.0.1.0.0',
+    'category': 'Manufacturing',
+    'summary': """This module is to manage manufacturing order details in
+    an product kanban view.""",
+    'description': """This module allows to view details about manufacturing
+    Orders, work orders, BOM, unbuild orders, and scrap orders in a product
+    kanban view.""",
+    'author': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['mrp'],
+    'data': [
+         'views/product_product_views.xml'
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False
+}
