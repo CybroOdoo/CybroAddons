bd23e904d619c25f6492691ea5cd55edc4afb0a4
RisvanaCybro <risvana@cybrosys.info>
2024-07-04 17:15:20 +0530

    July 4: [ADD] Initial commit 'website_cancel_sale_order'

---
+++ website_cancel_sale_order/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <website_cancel_sale_order>
+
+#### 27.06.2024
+#### Version 15.0.1.0.0
+##### ADD
+- Initial Commit for Website Cancel Order
---
+++ website_cancel_sale_order/static/src/js/sale_order_portal.js
@@ -0,0 +1,34 @@
+/** @odoo-module **/
+//This JavaScript function handles the cancellation of a sale order on the website.
+//The function checking for there is a valid reason for cancelling the order
+var publicWidget = require('web.public.widget');
+var ajax = require('web.ajax');
+publicWidget.registry.WebsiteSaleOrderCancel = publicWidget.Widget.extend({
+     selector: '.editReasonForm',
+     events: {
+            'click #submit_button': '_onSubmit',
+        },
+        _onSubmit: function (e) {
+            var reason = this.$("#reason").val();
+            var sale_order_id =this.$("#sale_order_id").val();
+            var iChars = /[`!@#$%^&*()_+\-=\[\]{}]/;
+            var iCharsString = iChars.toString().slice(1, -1);
+            var numbers=/[^0-9]/;
+            var containsSpecialChars = new RegExp('[' + iCharsString.replace(/([()[\]{}\-.*+?^$|\\])/g, '\\$1') + ']').test(reason); // Convert regex to string
+             if (reason == "" || containsSpecialChars || !numbers.test(reason) ) {
+                    alert("Please specify the reason properly");
+                    e.preventDefault();
+                }
+             else{
+                  ajax.jsonRpc("/cancel/reason/edit", 'call', {
+                       'reason': reason,
+                       'sale_order_id': sale_order_id
+                  }).then(function() {
+                     location.reload();
+            });
+            }
+        }
+    });
+    var WebsiteSaleOrderCancel = new publicWidget.registry.WebsiteSaleOrderCancel(this);
+    WebsiteSaleOrderCancel.appendTo($(".editReasonForm"));
+    return publicWidget.registry.WebsiteSaleOrderCancel;
