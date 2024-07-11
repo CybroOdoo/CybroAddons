aee488fe08498eb00ecfe3a9877affd8fbf8e48b
RisvanaCybro <risvana@cybrosys.info>
2024-07-11 22:10:58 +0530

    July 11: [ADD] Initial commit 'website_menu_restriction'

---
+++ website_menu_restriction/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: NAVEEN KRISHNA T (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <https://www.gnu.org/licenses/>.
+#
+################################################################################
+{
+    'name': 'Website Menu Restriction',
+    'version': '16.0.1.0.0',
+    'category': 'Website',
+    'summary': 'Menu Group Access Restriction',
+    'description': 'This module helps to manage visibility of website'
+                   ' menu based on user groups.',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'website'],
+    'data': [
+        'security/website_security.xml',
+        'views/website_menu_views.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
