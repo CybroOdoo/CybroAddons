d9ab1777c2268935e6657aeb6af591d04b4bcb19
RisvanaCybro <risvana@cybrosys.info>
2024-07-11 21:42:35 +0530

    July 11: [ADD] Initial commit 'kanban_fixed_header'

---
+++ kanban_fixed_header/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Kanban Fixed Header',
+    'version': '15.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': """ Kanban Fixed Header for Odoo 15 Community and Enterprise
+     Edition. """,
+    'description': """ This module allows users to view kanban without
+     loosing heading details.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'depends': ['web'],
+    'website': 'https://www.cybrosys.com',
+    'assets': {
+        'web.assets_backend': [
+            'kanban_fixed_header/static/src/css/kanban_fixed_header.scss',
+        ],
+    },
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
