a9a798ae1d491f9ff7508b823878ed9a8189c0d8
RisvanaCybro <risvana@cybrosys.info>
2024-07-17 22:58:48 +0530

    July 17: [ADD] Initial commit 'project_task_sequence_number'

---
+++ project_task_sequence_number/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Project & Project Task Sequence Number',
+    'version': '15.0.1.0.0',
+    'category': 'Project',
+    'summary': 'Add dynamic sequence number for project and task.',
+    'description': """This module enables the generation of unique sequence
+    numbers for projects and tasks. When creating a new project or task, the
+    sequence number will be generated based on project.""",
+    'author': 'Cybrosys Techno solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'project'],
+    'data': [
+        'data/ir_sequence_project_data.xml',
+        'views/project_project_views.xml',
+        'views/project_task_views.xml'
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ project_task_sequence_number/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <project_task_sequence_number>
+
+#### 06.07.2024
+#### Version 15.0.1.0.0
+##### ADD
+- Initial commit for Project & Project Task Sequence Number
