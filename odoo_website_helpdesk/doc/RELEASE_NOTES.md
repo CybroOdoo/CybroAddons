afe0d38587529b65e4607f4397fa335790895d9e
RisvanaCybro <risvana@cybrosys.info>
2024-07-20 17:05:37 +0530

    July 20: [ADD] Initial commit 'odoo_website_helpdesk'

---
+++ odoo_website_helpdesk/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <odoo_website_helpdesk>
+
+#### 18.07.2024
+#### Version 17.0.1.0.0
+#### ADD
+
+- Initial commit for Website Helpdesk Support Ticket Management
---
+++ odoo_website_helpdesk/static/src/js/multiple_product_choose.js
@@ -0,0 +1,24 @@
+/** @odoo-module **/
+import { jsonrpc } from "@web/core/network/rpc_service";
+import publicWidget from "@web/legacy/js/public/public_widget";
+// loads product data from the server and populates a dropdown menu with the received product information
+publicWidget.registry.product_load = publicWidget.Widget.extend({
+    selector: '#product',
+    start: function() {
+            var self = this;
+            return this._super.apply(this, arguments).then(function() {
+                self._loadProducts();
+            });
+        },
+         _loadProducts: function() {
+
+            var self = this;
+            jsonrpc('/product').then(function(res) {
+                var ar = res;
+                self.$el.find('#product').prevObject.empty();
+                $(ar).each(function(i) {
+                    self.$el.find('#product').prevObject.append("<option value=" + ar[i].id + ">" + ar[i].name + "</option>");
+                });
+            });
+        },
+});
