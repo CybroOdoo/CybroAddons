9dcda579e01bc57d2e289d36e3cc9888d3b0b558
AjmalCybro <ajmal@cybrosys.in>
2024-08-03 13:02:57 +0530

    Aug 3 [UPDT] Updated 'sales_order_double_approval'

--- sales_order_double_approval/models/sale_order.py
+++ sales_order_double_approval/models/sale_order.py
@@ -1,27 +1,45 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2022-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
 from odoo import fields, models
 
 
 class SaleOrder(models.Model):
-    _inherit = ['sale.order']
+    _inherit = 'sale.order'
 
     state = fields.Selection(selection_add=
-                             [('to_approve', 'To Approve'),
-                              ('sent',)], ondelete={'to_approve': 'cascade'})
+                             [('to_approve', 'To Approve')], ondelete={'to_approve': 'cascade'})
 
     def button_approve(self):
-        self.write({'state': 'sale'})
+        self.action_confirm()
 
     def action_confirm(self):
-        res = super(SaleOrder, self).action_confirm()
-        if self.company_id.so_double_validation:
-            if self.env['ir.config_parameter'].sudo().get_param('sales_order_double_approval.so_approval'):
-                if self.amount_total > float(
-                        self.env['ir.config_parameter'].sudo().get_param('sales_order_double_approval.so_min_amount')):
-                    if self.user_has_groups('sales_team.group_sale_manager'):
-                        self.state = 'sale'
-                    else:
+        if not self.user_has_groups('sales_team.group_sale_manager'):
+            if self.company_id.so_double_validation:
+                if self.env['ir.config_parameter'].sudo().get_param('sales_order_double_approval.so_approval'):
+                    if self.amount_total > float(
+                            self.env['ir.config_parameter'].sudo().get_param('sales_order_double_approval.so_min_amount')):
                         self.state = 'to_approve'
-        return res
+                        return
+        return super(SaleOrder, self).action_confirm()
 
     def action_cancel(self):
         self.state = 'cancel'
