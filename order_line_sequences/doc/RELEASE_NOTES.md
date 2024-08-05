a757eac58e3d0cea9eb7463a78ca5cda43e5685b
Cybrosys Technologies <risvana@cybrosys.info>
2024-08-05 14:52:15 +0530

    Aug 05: [FIX] Bug Fixed 'order_line_sequences'

--- order_line_sequences/doc/RELEASE_NOTES.md
+++ order_line_sequences/doc/RELEASE_NOTES.md
@@ -1,6 +1,6 @@
 ## Module <order_line_sequences>
 
-#### 25.03.2024
+#### 29.07.2024
 #### Version 17.0.1.0.0
 #### ADD
 - Initial Commit for Order Line Sequences/Line Numbers.
--- order_line_sequences/models/stock.py
+++ order_line_sequences/models/stock.py
@@ -24,8 +24,7 @@ from odoo import api, fields, models
 
 class StockMove(models.Model):
     """ Class for inherited model stock move. Contains a field for line
-            numbers and a function for computing line numbers.
-    """
+            numbers and a function for computing line numbers."""
 
     _inherit = 'stock.move'
     sequence_number = fields.Integer(string='#',
@@ -35,8 +34,26 @@ class StockMove(models.Model):
     @api.depends('picking_id')
     def _compute_sequence_number(self):
         """Function to compute line numbers"""
-        for ids in self.mapped('picking_id'):
+        self.sequence_number = 1
+        for picking in self.mapped('picking_id'):
             sequence_number = 1
-            for lines in ids.move_ids_without_package:
-                lines.sequence_number = sequence_number
-                sequence_number += 1
+            if picking.move_ids_without_package:
+                for lines in picking.move_ids_without_package:
+                    lines.sequence_number = sequence_number
+                    sequence_number += 1
+            else:
+                picking.sequence()
+
+
+class StockPicking(models.Model):
+    """ Class for inherited model stock picking. Contains
+        a function for computing line numbers."""
+    _inherit = 'stock.picking'
+
+    @api.onchange('move_ids_without_package')
+    def _onchange_move_ids_without_package(self):
+        """For calculating line number of operations"""
+        sequence_number = 1
+        for rec in self.move_ids_without_package:
+            rec.sequence_number = sequence_number
+            sequence_number += 1
