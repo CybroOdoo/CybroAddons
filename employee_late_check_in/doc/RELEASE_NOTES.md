62e0d50b0352e1acfbe89842fb9b7a28f7e9d26e
Cybrosys Technologies <risvana@cybrosys.info>
2024-08-12 12:03:40 +0530

    Aug 12: [FIX] Bug Fixed 'employee_late_check_in'

--- employee_late_check_in/doc/RELEASE_NOTES.md
+++ employee_late_check_in/doc/RELEASE_NOTES.md
@@ -10,3 +10,10 @@
 #### Version 16.0.1.0.1
 #### UPDATE
  - Updated scheduled action and ensured amount updates in the late-check-in. Displayed time-off when generating payslip and deducted late check in amount from it.
+
+#### 12.08.2024
+#### Version 16.0.1.0.2
+#### UPDATE
+ - Bug Fix in Employee Late Check-in
+
+
--- employee_late_check_in/models/hr_attendance.py
+++ employee_late_check_in/models/hr_attendance.py
@@ -1,40 +1,39 @@
 # -*- coding: utf-8 -*-
-###############################################################################
+#############################################################################
 #
 #    Cybrosys Technologies Pvt. Ltd.
 #
 #    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
-#    Author: Cybrosys Techno Solutions (odoo@cybrosys.com)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
 #
-#    You can modify it under the terms of the GNU AFFERO
-#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
 #
-#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
-#    (AGPL v3) along with this program.
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
 #    If not, see <http://www.gnu.org/licenses/>.
 #
-###############################################################################
+############################################################################.
 import pytz
 from datetime import datetime, timedelta
-from odoo import fields, models
+from odoo import api,fields, models
 
 
 class HrAttendance(models.Model):
     """Inherit the module to add fields and methods"""
-
-    _inherit = "hr.attendance"
+    _inherit = 'hr.attendance'
 
     late_check_in = fields.Integer(
-        string="Late Check-in(Minutes)",
-        compute="_compute_late_check_in",
+        string="Late Check-in(Minutes)", compute="_compute_late_check_in",
         help="This indicates the duration of the employee's tardiness.",
-    )
+        store=True)
 
+    @api.depends('check_in')
     def _compute_late_check_in(self):
         """Calculate late check-in minutes for each record in the current Odoo
         model.This method iterates through the records and calculates late
@@ -44,77 +43,62 @@ class HrAttendance(models.Model):
         for rec in self:
             rec.late_check_in = 0.0
             if rec.employee_id.contract_id:
-                for schedule in (
-                    rec.sudo()
-                    .employee_id.contract_id.resource_calendar_id.sudo()
-                    .attendance_ids
-                ):
-                    if (
-                        schedule.dayofweek == str(rec.sudo().check_in.weekday())
-                        and schedule.day_period == "morning"
-                    ):
+                for schedule in rec.sudo().employee_id.contract_id.resource_calendar_id.sudo().attendance_ids:
+                    if (schedule.dayofweek == str(
+                            rec.sudo().check_in.weekday()) and
+                            schedule.day_period == 'morning'):
                         dt = rec.check_in
                         if self.env.user.tz in pytz.all_timezones:
-                            old_tz = pytz.timezone("UTC")
+                            old_tz = pytz.timezone('UTC')
                             new_tz = pytz.timezone(self.env.user.tz)
                             dt = old_tz.localize(dt).astimezone(new_tz)
                         str_time = dt.strftime("%H:%M")
-                        check_in_date = datetime.strptime(str_time, "%H:%M").time()
+                        check_in_date = datetime.strptime(
+                            str_time, "%H:%M").time()
                         start_date = datetime.strptime(
-                            "{0:02.0f}:{1:02.0f}".format(
-                                *divmod(schedule.hour_from * 60, 60)
-                            ),
-                            "%H:%M",
-                        ).time()
-                        check_in = timedelta(
-                            hours=check_in_date.hour, minutes=check_in_date.minute
-                        )
-                        start_date = timedelta(
-                            hours=start_date.hour, minutes=start_date.minute
-                        )
+                            '{0:02.0f}:{1:02.0f}'.format(*divmod(
+                                schedule.hour_from * 60, 60)),
+                            "%H:%M").time()
+                        check_in = timedelta(hours=check_in_date.hour,
+                                             minutes=check_in_date.minute)
+                        start_date = timedelta(hours=start_date.hour,
+                                               minutes=start_date.minute)
                         if check_in > start_date:
                             final = check_in - start_date
                             rec.sudo().late_check_in = final.total_seconds() / 60
 
     def late_check_in_records(self):
         """Function creates records in late.check.in model for the employees
-        who were late as a scheduled action"""
-        minutes_after = (
-            int(
-                self.env["ir.config_parameter"]
-                .sudo()
-                .get_param("employee_late_check_in.late_check_in_after")
-            )
-            or 0
-        )
-        max_limit = (
-            int(
-                self.env["ir.config_parameter"]
-                .sudo()
-                .get_param("employee_late_check_in.maximum_minutes")
-            )
-            or 0
-        )
-        for rec in self.sudo().search(
-            [
-                (
-                    "id",
-                    "not in",
-                    self.env["late.check.in"].sudo().search([]).attendance_id.ids,
-                )
-            ]
-        ):
-            late_check_in = rec.sudo().late_check_in + 210
-            if (
-                rec.late_check_in > minutes_after
-                and late_check_in > minutes_after
-                and late_check_in < max_limit
-            ):
-                self.env["late.check.in"].sudo().create(
-                    {
-                        "employee_id": rec.employee_id.id,
-                        "late_minutes": late_check_in,
-                        "date": rec.check_in.date(),
-                        "attendance_id": rec.id,
-                    }
-                )
+        who were late"""
+        minutes_after = int(
+            self.env['ir.config_parameter'].sudo().get_param(
+                'employee_late_check_in.late_check_in_after')) or 0
+        max_limit = int(self.env['ir.config_parameter'].sudo().get_param(
+            'employee_late_check_in.maximum_minutes')) or 0
+        for rec in self.sudo().search([]):
+            print(rec,minutes_after)
+            if rec.id not in self.env['late.check.in'].sudo().search(
+                    []).attendance_id.ids:
+                if minutes_after < rec.late_check_in < max_limit:
+                    print(rec,'rec')
+                    self.env['late.check.in'].sudo().create({
+                        'employee_id': rec.employee_id.id,
+                        'late_minutes': rec.late_check_in,
+                        'date': rec.check_in.date(),
+                        'attendance_id': rec.id,
+                    })
+            else:
+                self.env['late.check.in'].sudo().search(
+                    [('attendance_id', '=', rec.id)]).update({
+                    'late_minutes': rec.late_check_in,
+                    'date': rec.check_in.date(),
+                    'attendance_id': rec.id,
+                })
+
+    def unlink(self):
+        """Override the unlink method to delete the corresponding records in
+        late.check.in model"""
+        for record in self:
+            self.env['late.check.in'].sudo().search(
+                [('attendance_id', '=', record.id)]).unlink()
+            return super(HrAttendance, self).unlink()
