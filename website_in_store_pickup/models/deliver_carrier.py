04324356b7e8ed56b69a4dae62d46a928455f0d0
Cybrosys Technologies <risvana@cybrosys.info>
2024-08-31 16:27:01 +0530

    Aug 31: [ADD] Initial commit 'website_in_store_pickup'

---
+++ website_in_store_pickup/models/deliver_carrier.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Ranjith R(odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+
+###############################################################################
+from odoo import fields, models
+
+
+class DeliveryCarrier(models.Model):
+    """Class to add the fields to identify a method as a store picking
+    delivery method and choose the stores to be available for that particular
+    delivery method"""
+    _inherit = 'delivery.carrier'
+
+    is_store_pick = fields.Boolean(string='In-Store Pickup',
+                                   help="Enable this to identify this as an "
+                                        "In-store PickUp delivery method")
+    store_ids = fields.Many2many('stock.warehouse',
+                                 string="Available Stores",
+                                 domain="[('is_store', '=', True)]",
+                                 help="Choose the stores available for "
+                                      "in-store picking and if no stores "
+                                      "implies all stores are available")
