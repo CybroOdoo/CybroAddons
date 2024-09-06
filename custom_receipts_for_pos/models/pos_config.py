23356a66a27b7b878f59a3aaf3173a2b060a113a
Cybrosys Technologies <risvana@cybrosys.info>
2024-09-06 11:05:08 +0530

    Sep 06: [FIX] Bug Fixed 'custom_receipts_for_pos'

--- custom_receipts_for_pos/models/pos_config.py
+++ custom_receipts_for_pos/models/pos_config.py
@@ -27,6 +27,10 @@ from odoo import fields, models
 class PosConfig(models.Model):
     _inherit = "pos.config"
 
-    receipt_design = fields.Many2one('pos.receipt', string="Receipt Design", help="Choose any receipt design")
-    design_receipt = fields.Text(related='receipt_design.design_receipt', string='Receipt XML')
+    receipt_design = fields.Many2one('pos.receipt', string="Receipt Design",
+                                     help="Choose any receipt design")
+    design_receipt = fields.Text(related='receipt_design.design_receipt',
+                                 string='Receipt XML')
     is_custom_receipt = fields.Boolean(string='Is Custom Receipt')
+    logo = fields.Binary(related='company_id.logo', string='Logo',
+                         readonly=False)
