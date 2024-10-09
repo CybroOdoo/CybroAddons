f902b1b89c108c6875fdc7ee80356f500915da82
Shijin V <shijin@cybrosys.in>
2024-10-09 10:30:21 +0530

    OCT 09: [ADD] Initial Commit 'print_minutes_of_meeting'

---
+++ print_minutes_of_meeting/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Aysha Shalin (odoo@cybrosys.com)
+
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Minutes of Meetings and Print Meetings',
+    'version': '18.0.1.0.0',
+    'summary': 'Module for Minutes of Meetings and Print PDF Report. ',
+    'description': 'Module for Minutes of Meetings and Print PDF Report',
+    'category': 'Project',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'calendar'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/mail_template.xml',
+        'views/calendar_event_views.xml',
+        'report/mom_report.xml',
+        'report/mom_report_template.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
---
+++ print_minutes_of_meeting/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <print_minutes_of_meeting>
+
+#### 09.10.2024
+#### Version 18.0.1.0.0
+##### ADD
+- Initial Commit for Minutes of Meetings and Print Meetings
