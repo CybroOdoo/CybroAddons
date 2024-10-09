d59edf2e005a9b5383fa3a3ba69303d2e7eaf747
Cybrosys Technologies <risvana@cybrosys.info>
2024-10-09 17:05:12 +0530

    Oct 09: [ADD] Initial commit 'sale_product_image'

---
+++ sale_product_image/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <sale_orderline_image>
+
+#### 05.10.2024
+#### Version 18.0.1.0.0
+##### ADD
+
+- Initial commit for Sale Order Line Images
---
+++ sale_product_image/models/res_config_settings.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions (<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    """Inherits the model res.config.settings to add the field"""
+    _inherit = 'res.config.settings'
+
+    is_show_product_image_in_sale_report = fields.Boolean(
+        string="Show Product Image",
+        config_parameter='sale_product_image.is_show_product_image_in_sale_report',
+        help='Show product Image in sale report')
