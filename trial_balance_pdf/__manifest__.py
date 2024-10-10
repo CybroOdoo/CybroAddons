60454606965a51432a684da75d3335b0f8d3ba13
Cybrosys Technologies <risvana@cybrosys.info>
2024-10-10 11:37:42 +0530

    Oct 10: [ADD] Initial commit 'trial_balance_pdf'

---
+++ trial_balance_pdf/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Jumana Haseen (<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Trial Balance PDF Report',
+    'version': '18.0.1.0.0',
+    'category': 'Accounting',
+    'summary': """This module helps to generate the trial balance report in PDF
+    format.""",
+    'description': """This module provides a trial balance PDF report within the
+    Odoo Community version's Invoicing App.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'report/trial_balance_report_templates.xml',
+        'report/trial_balance_report_reports.xml',
+        'wizard/trial_balance_report_views.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ trial_balance_pdf/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <trial_balance_pdf>
+
+#### 09.10.2024
+#### Version 18.0.1.0.0
+##### ADD
+- Initial commit for Trial Balance PDF Report
