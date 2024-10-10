442fad15ca1a76011414634d64121e852c4d02e4
Cybrosys Technologies <risvana@cybrosys.info>
2024-10-10 11:35:55 +0530

    Oct 10: [ADD] Initial commit 'customer_sequence'

---
+++ customer_sequence/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Aysha Shalin (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC
+#    LICENSE (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Customer Sequence',
+    'version': '18.0.1.0.0',
+    'category': 'Sales',
+    'summary': """Customer code for Partners.""",
+    'description': """This module provides a unique customer code to each
+    customer upon creation.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['sale_management'],
+    'data': [
+        'views/res_company_views.xml',
+        'views/res_partner_views.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install ': False,
+    'application': False
+}
+
---
+++ customer_sequence/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <customer_sequence>
+
+#### 19.09.2024
+#### Version 18.0.1.0.0
+##### ADD
+ - Initial Commit for Customer Sequence
