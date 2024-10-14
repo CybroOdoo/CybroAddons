da284ac91b3f8e57b02246610faeb726b3b41c1f
Cybrosys Technologies <risvana@cybrosys.info>
2024-10-14 14:28:50 +0530

    Oct 14: [FIX] Bug Fixed 'hotel_management_odoo'

--- hotel_management_odoo/wizard/room_booking_detail.py
+++ hotel_management_odoo/wizard/room_booking_detail.py
@@ -86,22 +86,25 @@ class RoomBookingWizard(models.TransientModel):
             )
         room_booking = self.env["room.booking"].search_read(
             domain=domain,
-            fields=["partner_id", "name"],
+            fields=["partner_id", "name", "checkin_date", "checkout_date"],
         )
         for rec in room_booking:
-            rooms = self.env["room.booking"].browse(rec["id"])
+            rooms = (
+                self.env["room.booking"]
+                .browse(rec["id"])
+                .room_line_ids.room_id.mapped("name")
+            )
             rec["partner_id"] = rec["partner_id"][1]
-            for line in rooms.room_line_ids:
+            for room in rooms:
                 if self.room_id:
-                    if self.room_id.id == line.room_id.id:
-                        rec["room_id"] = line.room_id.name
-                        rec["checkin_date"] = line.checkin_date.date()
-                        rec["checkout_date"] = line.checkout_date.date()
+                    if self.room_id.name == room:
+                        rec["room_id"] = room
                         room_list.append(rec)
                 else:
                     rec_copy = rec.copy()
-                    rec_copy["room_id"] = line.room_id.name
+                    rec_copy["room_id"] = room
                     room_list.append(rec_copy)
+
         return room_list
 
     def get_xlsx_report(self, data, response):
