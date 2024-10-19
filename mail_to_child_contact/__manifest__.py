b907bace0ab6a04ffbaff5506bc7884de29aa1ff
Cybrosys Technologies <risvana@cybrosys.info>
2024-10-19 14:53:44 +0530

    Oct 19: [ADD] Initial commit 'mail_to_child_contact'

---
+++ mail_to_child_contact/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author:Adarsh K (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Automatic Mail to Child Contact',
+    'version': '18.0.1.0.0',
+    'category': 'Discuss',
+    'summary': """Send Mail to all Child Contact in Sales and Invoice""",
+    'description': """Send Mail to all Child Contact in Sales and Invoice.
+    All child contacts are automatically add in the Recipients field.So we 
+    can easily manage the child contact in mail.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['base', 'sale_management'],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ mail_to_child_contact/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <mail_to_child_contact>
+
+#### 10.09.2024
+#### Version 18.0.1.0.0
+#### ADD
+- Initial commit for Automatic Mail to Child Contact
