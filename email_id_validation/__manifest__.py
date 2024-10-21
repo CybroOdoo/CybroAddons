12b932780a693b5925df4788f137fe86a07f8332
Cybrosys Technologies <risvana@cybrosys.info>
2024-10-21 18:40:35 +0530

    Oct 19: [ADD] Initial commit 'email_id_validation'

---
+++ email_id_validation/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Aysha Shalin (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC
+#    LICENSE (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': "Email Verification for Partner and Employee",
+    'version': '18.0.1.0.0',
+    'category': 'Discuss',
+    'summary': """Check whether a given E-mail ID is valid or not.""",
+    'description': """Check whether a given E-mail ID is valid or not in 
+    partner and employee form.""",
+    'author': "Cybrosys Techno Solutions",
+    'company': "Cybrosys Techno Solutions",
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['contacts', 'hr'],
+    'external_dependencies': {
+        'python': ['validate_email', 'py3DNS']
+    },
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
