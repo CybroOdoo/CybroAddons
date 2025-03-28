948cef66515f1dc313cc42faaec7b23ca3c3a9c4
AjmalCybro <ajmal@cybrosys.in>
2025-03-28 11:16:42 +0530

    Mar 28 : [UPDT] Updated 'automatic_invoice_and_post'

--- automatic_invoice_and_post/__manifest__.py
+++ automatic_invoice_and_post/__manifest__.py
@@ -21,7 +21,7 @@
 #############################################################################
 {
     'name': 'Automatic Invoice And Post',
-    'version': "17.0.1.0.0",
+    'version': "17.0.1.0.1",
     'category': 'Sales,Warehouse,Accounting',
     'summary': """ Auto Invoice Generation and Auto Sending of Invoice on 
      Delivery validation.""",
--- automatic_invoice_and_post/doc/RELEASE_NOTES.md
+++ automatic_invoice_and_post/doc/RELEASE_NOTES.md
@@ -5,4 +5,7 @@
 ##### ADD
 - Initial commit for Automatic Invoice And Post
 
-
+#### 27.03.2025
+#### Version 17.0.1.0.1
+##### UPDT
+- Commit for Bug fixing
