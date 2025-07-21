b9a673a1d1c00cc3b3a9d1e7dcd2655905290ec8
Cybrosys Technologies <risvana@cybrosys.info>
2025-07-21 12:58:07 +0530

    July 21: [UPDT] Banner Updated 'employee_check_list'

--- employee_check_list/__manifest__.py
+++ employee_check_list/__manifest__.py
@@ -36,7 +36,7 @@
         'views/employee_form_inherit_views.xml',
         'views/checklist_views.xml',
     ],
-    'images': ['static/description/banner.jpg'],
+    'images': ['static/description/banner.png'],
     'license': 'AGPL-3',
     'installable': True,
     'auto_install': False,
