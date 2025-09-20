131667dde68cdd4de6a91dcdcba4fe4f7b7711d3
Risvana Cybro <risvana@cybrosys.info>
2025-09-20 23:33:22 +0530

    Sep 20: [ADD] Initial Commit 'thermal_invoice_report'

---
+++ thermal_invoice_report/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2025-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': "Thermal Invoice Report",
+    'version': '18.0.1.0.0',
+    'summary': 'A Module For Printing Thermal Invoice',
+    'description': 'The Thermal Invoice Printing app enhances the '
+                   'invoicing workflow in Odoo by enabling seamless '
+                   'printing of compact, receipt-style invoices',
+    'category': 'Accounting',
+    'author': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'account'],
+    'data': [
+        'data/account_move_data.xml',
+        'report/account_move_reports.xml',
+        'report/account_move_templates.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ thermal_invoice_report/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <thermal_invoice_report>
+
+#### 02.07.2025
+#### Version 18.0.1.0.0
+#### ADD
+- Initial commit for Thermal Invoice Report
