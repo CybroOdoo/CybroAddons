58361ecb749c3ebbe97194bfcfa0f12f205aa336
Risvana Cybro <risvana@cybrosys.info>
2025-09-30 21:15:26 +0530

    Sep 30: [ADD] Initial Commit 'contacts_timezone'

---
+++ contacts_timezone/__manifest__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2025-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Contacts Timezone',
+    'version': '17.0.1.0.0',
+    'category': 'Contact',
+    'summary': 'Display a contacts local time and time zone based on their address.',
+    'description': """This module helps to get access to contact's local time and timezone using the address values in the partner form.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['contacts'],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'images': [
+        'static/description/banner.jpg',
+        'static/description/icon.png',
+    ],
+    'external_dependencies': {
+        'python': ['geopy','timezonefinder'],
+    },
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+
