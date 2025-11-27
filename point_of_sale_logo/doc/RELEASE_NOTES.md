332c9f107757b44d327da44eda1daac2c61e1612
Risvana Cybro <risvana@cybrosys.info>
2025-11-27 11:51:11 +0530

    NOV 27: [ADD] Initial Commit 'point_of_sale_logo'

---
+++ point_of_sale_logo/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <point_of_sale_logo>
+
+#### 09.10.2025
+#### Version 19.0.1.0.0
+#### ADD
+
+- Initial commit for Point of Sale Logo
---
+++ point_of_sale_logo/models/pos_config.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2025-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Anjali VP (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+from odoo import fields, models
+
+
+class PosConfig(models.Model):
+    """Inherited pos config for uploading logo image"""
+    _inherit = 'pos.config'
+
+    image = fields.Binary(string='Image', help="Set logo image for viewing it"
+                                               "in POS Screen and Receipt")
+    logo_option = fields.Selection(
+        selection=[
+            ('pos', 'Show POS Logo Only'),
+            ('company', 'Show Company Logo Only'),
+            ('both', 'Show Both POS and Company Logos')],
+        string="Receipt Logo Option",
+        default='pos',
+        help="Choose which logo(s) to display on the printed POS receipt.")
