5a2f30049f43bb3c025df298fdfe94a697b4ddfd
Risvana Cybro <risvana@cybrosys.info>
2025-12-31 21:59:37 +0530

    :Dec 31: Initial Commit 'customized_barcode_generator'

---
+++ customized_barcode_generator/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2025-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author:  Cybrosys Techno Solutions (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Customized Barcode Generator',
+    'version': '19.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': """Print user defined product labels.""",
+    'description': """This module enables user to print customized product
+    labels, Barcode, Barcode Generator, Barcode Label, Product Label, 
+    Product Barcode Generator, Product Barcode, Label Print and Product
+    Label Print.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['web', 'product', 'account'],
+    'data': [
+        'report/product_report_templates.xml',
+        'security/ir.model.access.csv',
+        'views/barcode_code_views.xml',
+        'views/res_config_settings_views.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ customized_barcode_generator/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <customized_barcode_generator>
+
+#### 23.10.2024
+#### Version 19.0.1.0.0
+##### ADD
+- Initial commit for Customized Barcode Generator
