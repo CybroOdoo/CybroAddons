d15aeb0f398d6f9859bab7348c318ffe7f777c12
Risvana Cybro <risvana@cybrosys.info>
2026-01-17 16:53:49 +0530

    :Jan 17 :[ADD]  Initial Commit 'master_search'

---
+++ master_search/README.rst
@@ -0,0 +1,52 @@
+.. image:: https://img.shields.io/badge/license-AGPL--3-blue.svg
+    :target: https://www.gnu.org/licenses/agpl-3.0-standalone.html
+    :alt: License: AGPL-3
+
+Global Search
+=============
+This module allows users to search the records in Customers, Products, Sale, Purchase, Inventory and Accounting Modules.
+
+Configuration
+=============
+* No additional configuration is needed.
+
+Company
+-------
+* `Cybrosys Techno Solutions <https://cybrosys.com/>`__
+
+License
+-------
+Affero General Public License, v3.0 (AGPL v3).
+(https://www.gnu.org/licenses/agpl-3.0-standalone.html)
+
+Credits
+-------
+* Developers: (v14) Shahil,
+              (v15) Athul,
+              (v16) Rosmy,
+              (v17) Anjhana A K,
+              (v18) Aysha Shalin,
+              (v19) Vyshnav P P
+  Contact: odoo@cybrosys.com
+
+Contacts
+--------
+* Mail Contact : odoo@cybrosys.com
+* Website : https://cybrosys.com
+
+Bug Tracker
+-----------
+Bugs are tracked on GitHub Issues. In case of trouble, please check there if your issue has already been reported.
+
+Maintainer
+==========
+.. image:: https://cybrosys.com/images/logo.png
+   :target: https://cybrosys.com
+
+This module is maintained by Cybrosys Technologies.
+
+For support and more information, please visit `Our Website <https://cybrosys.com/>`__
+
+Further information
+===================
+HTML Description: `<static/description/index.html>`__
---
+++ master_search/static/src/js/master_search.js
@@ -0,0 +1,118 @@
+/** @odoo-module **/
+import { formView } from '@web/views/form/form_view';
+import { registry } from "@web/core/registry";
+import { FormRenderer } from "@web/views/form/form_renderer";
+import { FormController } from "@web/views/form/form_controller";
+import { useEffect, useExternalListener } from "@odoo/owl";
+
+export class MasterSearch extends FormController {
+    setup() {
+        super.setup();
+        useEffect(() => {
+            const expandElements = document.querySelectorAll('.expand_tile')
+            const background = document.querySelector('.oe_search_bgnd')
+            const RadioBtns = document.querySelectorAll('.o_radio_input')
+            if (background.length > 0) {
+                document.title = 'Search';
+            }
+            expandElements.forEach((element) => {
+                element.addEventListener("click", this.clickedExpandTile)
+            })
+            RadioBtns.forEach((element) => {
+                element.addEventListener("change", this.onChangeRadioBtn)
+            })
+            return () => {
+                expandElements.forEach((element) => {
+                    element.removeEventListener("click", this.clickedExpandTile)
+                })
+                RadioBtns.forEach((element) => {
+                    element.removeEventListener("change", this.onChangeRadioBtn)
+                })
+            }
+        })
+    }
+    clickedExpandTile(e) {
+        var resultDiv = this.closest('.oe_result_div')
+        var element = resultDiv.querySelector('.oe_details_list')
+        if (element.style.display === "none" || element.style.display === "") {
+            element.style.display = "block";
+        } else {
+            element.style.display = "none";
+        }
+    }
+    onChangeRadioBtn(e) {
+        let targetEl = this.dataset.value;
+        const treeEls = document.querySelectorAll('.oe_details_list')
+        treeEls.forEach((element) => {
+            if (element.style.display === "none" || element.style.display === "") {
+                element.classList.remove('oe_details_list--show');
+            } else {
+                element.classList.add('d-none');
+            }
+        });
+        const searchTabs = document.querySelectorAll('.oe_search_tab')
+        switch(targetEl){
+            case 'customer':
+            searchTabs.forEach((el) => {
+                el.classList.add('d-none');
+            })
+            const customerResult = document.querySelector('#customer_search_results')
+            customerResult.classList.add('d-block');
+            customerResult.classList.remove('d-none');
+            break;
+            case 'product':
+            searchTabs.forEach((el) => {
+                el.classList.add('d-none');
+            })
+            const productResult = document.querySelector('#product_search_results')
+            productResult.classList.add('d-block');
+            productResult.classList.remove('d-none');
+            break;
+            case 'transaction details':
+            searchTabs.forEach((el) => {
+                el.classList.add('d-none');
+            })
+            const TransactionResult = document.querySelector('#inventory_search_results')
+            TransactionResult.classList.add('d-block');
+            TransactionResult.classList.remove('d-none');
+            break;
+            case 'sale details':
+            searchTabs.forEach((el) => {
+                el.classList.add('d-none');
+            })
+            const SaleResult = document.querySelector('#sale_search_results')
+            SaleResult.classList.add('d-block');
+            SaleResult.classList.remove('d-none');
+            break;
+            case 'purchase details':
+            searchTabs.forEach((el) => {
+                el.classList.add('d-none');
+            })
+            const PurchaseResult = document.querySelector('#purchase_search_results')
+            PurchaseResult.classList.add('d-block');
+            PurchaseResult.classList.remove('d-none');
+            break;
+            case 'account details':
+            searchTabs.forEach((el) => {
+                el.classList.add('d-none');
+            })
+            const AccountResult = document.querySelector('#accounting_search_results')
+            AccountResult.classList.add('d-block');
+            AccountResult.classList.remove('d-none');
+            break;
+            case 'any':
+            searchTabs.forEach((el) => {
+                el.classList.remove('d-none');
+            })
+            break;
+        }
+        const RecentSearch = document.querySelector('#recent_searches')
+        RecentSearch.classList.add('d-block');
+        RecentSearch.classList.remove('d-none');
+    }
+}
+export const MasterSearchClass = {
+   ...formView,
+   Controller: MasterSearch,
+};
+registry.category("views").add("master_search", MasterSearchClass);
