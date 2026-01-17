362106abc432c7e5c9e34ec86cfcb88a35a0e0a2
Risvana Cybro <risvana@cybrosys.info>
2026-01-17 17:06:19 +0530

    :Jan 17 :[ADD]  Initial Commit 'website_hide_button'

---
+++ website_hide_button/controllers/website_sale.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2026-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from odoo import http
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+
+class WebsiteSaleInherit(WebsiteSale):
+    """class to hide price, add to cart and quantity"""
+
+    @http.route([
+        '''/shop''',
+        '''/shop/page/<int:page>''',
+        '''/shop/category/<model("product.public.category"):category>''',
+        '''/shop/category/<model("product.public.category"):category>/
+        page/<int:page>'''
+    ], type='http', auth="public", website=True)
+    def shop(self, page=0, category=None, search='', min_price=0.0,
+             max_price=0.0, ppg=False, **post):
+        """Method for finding log in user or not in shop page """
+        res = super().shop(page, category, search, min_price,
+                           max_price, ppg, **post)
+        res.qcontext.update({
+            'login_user': True if request.env.user._is_public() and request.env[
+                'ir.config_parameter'].sudo().get_param(
+                'website_hide_button.hide_cart') else False
+        })
+        return res
+
+    def _prepare_product_values(self, product, category,**kwargs):
+        """Method for finding log in user or not in product page """
+        res = super(WebsiteSaleInherit, self)._prepare_product_values(product,
+                                                                      category,
+                                                                      **kwargs)
+        res['login_user'] = True if request.env.user._is_public() and \
+                                    request.env[
+                                        'ir.config_parameter'].sudo().get_param(
+                                        'website_hide_button.hide_cart') else False
+        return res
+
+    @http.route()
+    def shop_payment(self, **post):
+        """  Restrict public visitors from accessing payment page so that SO
+        creation will be disabled   """
+        user = http.request.env.user
+        if (
+                not user._is_public() or user._is_public() and not request.env.user._is_public() and not
+        request.env[
+            'ir.config_parameter'].sudo().get_param(
+            'website_hide_button.hide_cart')) and user.has_group(
+            'base.group_portal') or \
+                user.has_group('base.group_user'):
+            res = super(WebsiteSaleInherit, self).shop_payment(**post)
+            return res
+        return request.redirect("/")
---
+++ website_hide_button/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <website_hide_button>
+
+####  13.01.2026
+#### Version 19.0.1.0.0
+#### ADD
+- Initial commit for Hide Price, Add To Cart And Quantity Button In Website
