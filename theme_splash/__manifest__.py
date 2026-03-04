3aebb251b842f507cd4da086f69c01c966d874eb
Risvana Cybro <risvana@cybrosys.info>
2026-03-04 14:33:35 +0530

    Mar 04: [FIX] Bug Fixed 'theme_splash'

--- theme_splash/__manifest__.py
+++ theme_splash/__manifest__.py
@@ -67,8 +67,7 @@
     },
     'images': [
         'static/description/banner.jpg',
-        'static/description/thumbnail.jpg'
-    ],
+        'static/description/theme_screenshot.jpg'],
     'license': 'LGPL-3',
     'installable': True,
     'auto_install': False,
