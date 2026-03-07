34e1164d7da0d195730b10169a4af81ef7327cbd
Risvana Cybro <risvana@cybrosys.info>
2026-03-07 16:23:23 +0530

    :Mar 05: [ADD] Initial commit 'odoo_website_helpdesk_dashboard'

---
+++ odoo_website_helpdesk_dashboard/__manifest__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2026-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': "Website HelpDesk Dashboard",
+    'version': '19.0.1.0.0',
+    'category': 'Website',
+    'summary': """Helpdesk Support Ticket Management Dashboard""",
+    'description': """Website HelpDesk Dashboard Module Brings a Multipurpose"""
+                   """Graphical Dashboard for Website HelpDesk module""",
+    'author': "Cybrosys Techno Solutions",
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['odoo_website_helpdesk', 'base'],
+    'data': [
+        'views/menu_item.xml',
+        'views/dashboard_templates.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'odoo_website_helpdesk_dashboard/static/src/css/dashboard.css',
+            'odoo_website_helpdesk_dashboard/static/src/js/lib/Chart.bundle.js',
+            'odoo_website_helpdesk_dashboard/static/src/js/dashboard_view.js',
+            'odoo_website_helpdesk_dashboard/static/src/xml/dashboard_view.xml',
+            'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js'
+        ],
+    },
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'auto_install': False,
+    'installable': True,
+    'application': False,
+}
---
+++ odoo_website_helpdesk_dashboard/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <odoo_website_helpdesk_dashboard>
+
+#### 03.01.2026
+#### Version 19.0.1.0.0
+#### ADD
+
+- Initial commit for Website HelpDesk Dashboard
