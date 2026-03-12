6831a45f821f8eb0cd873ead471cb3cea30d9cd8
Risvana Cybro <risvana@cybrosys.info>
2026-03-12 12:38:21 +0530

    Mar 12: [ADD] Initial Commit 'excel_report_designer'

---
+++ excel_report_designer/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <excel_report_designer>
+
+#### 09.03.2026
+#### Version 19.0.1.0.0
+#### ADD
+- Initial Commit Excel Report Designer
---
+++ excel_report_designer/static/src/js/action_manager.js
@@ -0,0 +1,16 @@
+/** @odoo-module **/
+import { registry } from "@web/core/registry";
+import { BlockUI } from "@web/core/ui/block_ui";
+import { download } from "@web/core/network/download";
+registry.category("ir.actions.report handlers").add("xlsx", async function (action) {
+    console.log(action)
+    if (action.report_type === 'xlsx') {
+        BlockUI;
+       await download({
+               url: '/xlsx_reports',
+               data: action.data,
+               complete: () => unblockUI,
+               error: (error) => this.call('crash_manager', 'rpc_error', error),
+       });
+    }
+});
