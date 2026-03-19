3d0c200698fa0eaa230418403aa67bbe27d79251
Risvana Cybro <risvana@cybrosys.info>
2026-03-19 20:58:56 +0530

    Mar 18: [ADD] Initial Commit 'customer_success_sentiment_tracker'

---
+++ customer_success_sentiment_tracker/__manifest__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2026-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions (<https://www.cybrosys.com>)
+#
+#    This program is under the terms of Odoo Proprietary License v1.0 (OPL-1)
+#    It is forbidden to publish, distribute, sublicense, or sell copies of the
+#    Software or modified copies of the Software.
+#
+#    THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL
+#    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,ARISING
+#    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#    DEALINGS IN THE SOFTWARE.
+#
+###############################################################################
+{
+    'name': 'Customer Success Sentiment Tracker',
+    'version': '19.0.1.0.0',
+    'category': 'Extra Tools',
+    'sequence': 1,
+    'summary': 'Customer Success Sentiment Tracker using AI',
+    'description': """Track customer sentiment from helpdesk tickets and emails using AI""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['crm', 'helpdesk', 'website'],
+    'data': [
+        'views/helpdesk_ticket_views.xml',
+        'views/res_config_settings_views.xml',
+        'views/res_partner_views.xml',
+        'views/success_sentiment_menus.xml'
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'customer_success_sentiment_tracker/static/src/xml/customer_success_sentiment_dashboard.xml',
+            'customer_success_sentiment_tracker/static/src/js/customer_success_sentiment_dashboard.js',
+        ],
+    },
+    'images': ['static/description/banner.jpg'],
+    'license': 'OPL-1',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ customer_success_sentiment_tracker/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <customer_success_sentiment_tracker>
+
+#### 19.03.2026
+#### Version 19.0.1.0.0
+##### ADD
+- Initial commit for Customer Success Sentiment Tracker.
