8bca4562c216276704b25c6485f0a49a346eb41c
Risvana Cybro <risvana@cybrosys.info>
2026-03-26 15:37:28 +0530

    Mar 26: [ADD] Initial Commit 'multiple_order_confirm'

---
+++ multiple_order_confirm/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2026-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions (<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Multiple Sale/Purchase Order Confirm And Cancel',
+    'version': '19.0.1.0.0',
+    'category': 'Sales/Purchases',
+    'summary': 'Confirm or Cancel Multiple Sale or Purchase Orders',
+    'description': """Adds "Confirm" and "Cancel" buttons to the tree view of
+                      Sale and Purchase orders, allowing users to confirm or
+                      cancel multiple records by selecting them directly from
+                      the tree view.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'purchase', 'sale_management'],
+    'data': [
+        'views/purchase_order_views.xml',
+        'views/sale_order_views.xml'
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ multiple_order_confirm/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <multiple_order_confirm>
+
+#### 24.03.2026
+#### Version 19.0.1.0.0
+##### ADD
+
+Initial Commit for Multiple Sale/Purchase Order Confirm And Cancel
