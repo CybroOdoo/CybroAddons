2556091ab9f0f13e6a4880b23b2a47afb70dff73
Risvana Cybro <risvana@cybrosys.info>
2026-03-31 22:03:42 +0530

    Mar 31: [ADD] Initial Commit 'payment_status_in_sale'

---
+++ payment_status_in_sale/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <payment_status_in_sale>
+
+#### 04.03.2026
+#### Version 19.0.1.0.0
+##### ADD
+- Initial commit for Sale Order Payment Status
---
+++ payment_status_in_sale/models/sale_order.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2026-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Technologies (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from odoo import api, fields, models, _
+
+
+class SaleOrder(models.Model):
+    """ Extend the base Sale Order model to add custom fields and behaviors
+    for Sale Order Payment Status. """
+    _inherit = "sale.order"
+    _description = 'Sale order'
+
+    payment_status = fields.Char(string="Payment Status",
+                                 compute="_compute_payment_status",
+                                 help="Field to check the payment status of the"
+                                      " sale order")
+    payment_details = fields.Binary(string="Payment Details",
+                                    compute="_compute_payment_details",
+                                    help="Shows the payment done details "
+                                         "including date and amount")
+    amount_due = fields.Float(string="Amount Due",
+                              compute='_compute_amount_due',
+                              help="Shows the amount that in due for the "
+                                   "corresponding sale order")
+    invoice_state = fields.Char(string="Invoice State",
+                                compute="_compute_invoice_state",
+                                help="Field to check the invoice state of "
+                                     "sale order")
+
+    @api.depends('invoice_ids')
+    def _compute_payment_status(self):
+        """ The function will compute the payment status of the sale order, if
+        an invoice is created for the corresponding sale order.Payment status
+        will be either in paid,not paid,partially paid, reversed etc. """
+        for order in self:
+            order.payment_status = 'No invoice'
+            posted_invoices = order.invoice_ids.filtered(
+                lambda x: x.state == 'posted')
+            if not posted_invoices:
+                order.payment_status = 'No invoice'
+            else:
+                payment_states = posted_invoices.mapped('payment_state')
+                status_length = len(payment_states)
+                if order.amount_due > 0:
+                    if 'not_paid' in payment_states and status_length == payment_states.count('not_paid'):
+                        order.payment_status = 'Not Paid'
+                    elif 'partial' in payment_states or 'not_paid' in payment_states:
+                        order.payment_status = 'Partially Paid'
+                elif order.amount_due <= 0:  # Changed to <= 0 to handle overpayments or credit notes
+                    if 'paid' in payment_states and status_length == payment_states.count(
+                            'paid'):
+                        order.payment_status = 'Paid'
+                    elif 'in_payment' in payment_states and status_length == payment_states.count(
+                            'in_payment'):
+                        order.payment_status = 'In Payment'
+                elif 'reversed' in payment_states and status_length == payment_states.count(
+                        'reversed'):
+                    order.payment_status = 'Reversed'
+
+    @api.depends('invoice_ids')
+    def _compute_invoice_state(self):
+        """ The function will compute the state of the invoice , Once an invoice
+        is existing in a sale order. """
+        for rec in self:
+            rec.invoice_state = 'No invoice'
+            for order in rec.invoice_ids:
+                if order.state == 'posted':
+                    rec.invoice_state = 'posted'
+                elif order.state != 'posted':
+                    rec.invoice_state = 'draft'
+                else:
+                    rec.invoice_state = 'No invoice'
+
+    @api.depends('invoice_ids')
+    def _compute_amount_due(self):
+        """The function is used to compute the amount due from the invoice and
+        if payment is registered, accounting for exchange rate differences and credit notes."""
+        for rec in self:
+            total_invoiced = 0
+            total_paid = 0
+            for invoice in rec.invoice_ids.filtered(lambda x: x.state == 'posted'):
+                if invoice.move_type == 'out_invoice':  # Regular invoices
+                    total_invoiced += invoice.amount_total
+                    total_paid += invoice.amount_total - invoice.amount_residual
+                elif invoice.move_type == 'out_refund':  # Credit notes
+                    total_invoiced -= invoice.amount_total
+                    total_paid -= (
+                                invoice.amount_total - invoice.amount_residual)
+
+            rec.amount_due = total_invoiced - total_paid
+
+    def action_open_business_doc(self):
+        """ This method is intended to be used in the context of an
+        account.move record.
+        It retrieves the associated payment record and opens it in a new window.
+
+        :return: A dictionary describing the action to be performed.
+        :rtype: dict """
+        name = _("Journal Entry")
+        move = self.env['account.move'].browse(self.id)
+        res_model = 'account.payment'
+        payments = move.payment_ids
+        res_id = payments.id
+        return {
+            'name': name,
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'views': [(False, 'form')],
+            'res_model': res_model,
+            'res_id': res_id,
+            'target': 'current',
+        }
+
+    def js_remove_outstanding_partial(self, partial_id):
+        """ Called by the 'payment' widget to remove a reconciled entry to the
+        present invoice.
+
+        :param partial_id: The id of an existing partial reconciled with the
+        current invoice.
+        """
+        self.ensure_one()
+        partial = self.env['account.partial.reconcile'].browse(partial_id)
+        return partial.unlink()
+
+    @api.depends('invoice_ids')
+    def _compute_payment_details(self):
+        """ Compute the payment details from invoices and added into the sale
+        order form view. """
+        for rec in self:
+            payment = []
+            rec.payment_details = False
+            if rec.invoice_ids:
+                for line in rec.invoice_ids:
+                    if line.invoice_payments_widget:
+                        for pay in line.invoice_payments_widget['content']:
+                            payment.append(pay)
+                for line in rec.invoice_ids:
+                    if line.invoice_payments_widget:
+                        payment_line = line.invoice_payments_widget
+                        payment_line['content'] = payment
+                        rec.payment_details = payment_line
+                        break
+                    rec.payment_details = False
+
+    def action_register_payment(self):
+        """ Open the account.payment.register wizard to pay the selected journal
+         entries.
+        :return: An action opening the account.payment.register wizard.
+        """
+        self.ensure_one()
+        return {
+            'name': _('Register Payment'),
+            'res_model': 'account.payment.register',
+            'view_mode': 'form',
+            'context': {
+                'active_model': 'account.move',
+                'active_ids': self.invoice_ids.ids,
+            },
+            'target': 'new',
+            'type': 'ir.actions.act_window',
+        }
