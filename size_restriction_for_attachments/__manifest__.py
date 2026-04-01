7806d5f0c5837aa6d5985797424648d13a7c57ae
Risvana Cybro <risvana@cybrosys.info>
2026-04-01 18:20:43 +0530

    Apr 01 [UPDT] Banner updated 'size_restriction_for_attachments'

--- size_restriction_for_attachments/__manifest__.py
+++ size_restriction_for_attachments/__manifest__.py
@@ -41,7 +41,7 @@
     'data': [
         'views/res_users_views.xml',
     ],
-    'images': ['static/description/banner.png'],
+    'images': ['static/description/banner.jpg'],
     'license': 'AGPL-3',
     'installable': True,
     'auto_install': False,
