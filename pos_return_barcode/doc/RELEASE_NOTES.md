57f75565db3b59eb8b4bffeca8c4e4fe5b7eb19e
Risvana Cybro <risvana@cybrosys.info>
2026-04-06 17:25:20 +0530

    Apr 06: [ADD] Initial Commit 'pos_return_barcode'

---
+++ pos_return_barcode/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <pos_return_barcode>
+
+#### 22.01.2026
+#### Version 19.0.1.0.0
+#### ADD
+- Initial commit for POS Return Barcode
---
+++ pos_return_barcode/static/src/js/receipt_screen.js
@@ -0,0 +1,36 @@
+// Part of Odoo. See LICENSE file for full copyright and licensing details.
+import { patch } from "@web/core/utils/patch";
+import { useService } from "@web/core/utils/hooks";
+import  { useState } from "@odoo/owl";
+import { onWillStart } from "@odoo/owl";
+import { OrderReceipt } from "@point_of_sale/app/screens/receipt_screen/receipt/order_receipt";
+import { usePos } from "@point_of_sale/app/hooks/pos_hook";
+
+
+patch(OrderReceipt.prototype, {
+    setup() {
+        super.setup(...arguments);
+        this.orm = useService("orm");
+        this.pos = usePos();
+        this.state = useState({
+            barcode :''
+        });
+        onWillStart(() => {
+            this.fetch_barcode()
+        });
+    },
+
+    async fetch_barcode() {
+        const is_barcode = await this.orm.call("ir.config_parameter", "get_param", ["pos_return_barcode.receipt_barcode"])
+         const order = await this.orm.searchRead("pos.order",
+                       [["id", "=", this.order.id]],
+                       ['barcode']);
+         if(is_barcode){
+        order.forEach(element =>{
+            this.state.barcode = element.barcode
+            this.order.barcode = element.barcode
+
+        })
+        }
+    }
+});
