40a859d9ce7134fbfc2b516145f8e633161bf7ff
AjmalCybro <ajmal@cybrosys.in>
2026-04-24 16:07:27 +0530

    Apr 24[ADD] Initial Commit

---
+++ music_school_institute/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <music_school_institute>
+
+#### 30.03.2026
+#### Version 18.0.1.0.0
+##### ADD
+
+- Initial Commit for Music School Institute
---
+++ music_school_institute/models/account_move.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2026-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+from odoo import fields, models
+
+
+class AccountMove(models.Model):
+    """Model for adding details about the classes in invoice model."""
+    _inherit = 'account.move'
+
+    class_id = fields.Many2one(
+        'class.type',
+        string="Class",
+        readonly=True,
+        copy=False,
+        help="Reference to the music class associated with this invoice."
+    )
