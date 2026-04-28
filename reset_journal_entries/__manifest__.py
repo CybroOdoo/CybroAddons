bd12c278b6bb5b7fcee11c788bdd5be21636560a
Risvana Cybro <risvana@cybrosys.info>
2026-04-28 17:11:48 +0530

    Apr 28: [ADD] Initial Commit 'reset_journal_entries'

---
+++ reset_journal_entries/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2026-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author:  Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC
+#    LICENSE (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Reset Journal Entries',
+    'version': '19.0.1.0.0',
+    'category': 'Accounting',
+    'summary': """Using this module, multiple journal entries can be set as
+    draft, canceled, and posted in invoicing.""",
+    'description': """This module allows users to post multiple journal entries
+    in draft state to posted, reset posted entries back to draft state, and
+    cancel entries.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['account'],
+    'data': [
+        'data/ir_action_server_data.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False
+}
---
+++ reset_journal_entries/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <reset_journal_entries>
+
+#### 11.02.2026
+#### Version 19.0.1.0.0
+##### ADD
+- Initial Commit for Reset Journal Entries
