385e5a5f50030734f707635bf9939dd50eba2909
Risvana Cybro <risvana@cybrosys.info>
2026-06-29 15:19:19 +0530

    Jun 29: [UPDT] Test cases Added 'master_search'

--- master_search/__manifest__.py
+++ master_search/__manifest__.py
@@ -21,8 +21,9 @@
 ##############################################################################
 {
     'name': 'Global Search',
-    'version': '19.0.1.0.0',
+    'version': '19.0.1.0.1',
     'category': 'Extra Tools',
+    
     'summary': """Easy Search in Customers, Products, Sale, Purchase, Inventory
      and Accounting modules""",
     'description': """This module allows users to search the records in
--- master_search/doc/RELEASE_NOTES.md
+++ master_search/doc/RELEASE_NOTES.md
@@ -4,3 +4,10 @@
 #### Version 19.0.1.0.0
 ##### ADD
 - Initial Commit for Global Search 
+
+## Module <master_search>
+
+#### 26.06.2026
+#### Version 19.0.1.0.1
+##### UPDT
+- Added Test Cases
