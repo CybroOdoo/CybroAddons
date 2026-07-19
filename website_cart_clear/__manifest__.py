93c76fa179bc9c4728ee9f347ec0999681fda7ec
Risvana Cybro <risvana@cybrosys.info>
2026-07-19 17:46:50 +0530

    Jul 19:[ADD] Initial Commit 'website_cart_clear'

---
+++ website_cart_clear/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2026-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Website Clear Cart',
+    'version': '19.0.1.0.0',
+    'category': 'Website',
+    'summary': 'Quick option to empty the entire shopping cart in website.',
+    'description': """Provides a dedicated button to remove all cart products 
+     at once, helping users reset their selections quickly and efficiently.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['website_sale'],
+    'data': [
+        'views/website_sale_templates.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ website_cart_clear/doc/RELEASE_NOTES.md
@@ -0,0 +1,5 @@
+## Module <website_cart_clear>
+#### 02.07.2026
+#### Version 19.0.1.0.0
+#### ADD
+- Initial commit for Website Clear Cart
