a57d12ad3ebc6f9ba924224a4be7052f140415ac
Risvana Cybro <risvana@cybrosys.info>
2026-07-19 17:43:39 +0530

    Jul 19:[ADD] Initial Commit '[200~mail_push_notification~'

---
+++ mail_push_notification/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <mail_push_notification>
+
+#### 07.07.2026
+#### Version 19.0.1.0.0
+#### ADD
+
+- Initial release of Push Notification From ChatBox.
---
+++ mail_push_notification/models/res_users.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2026-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+from odoo import models
+
+
+class ResUsers(models.Model):
+    """Extend users with push notification permission helpers."""
+    _inherit = 'res.users'
+
+    def has_push_notification_permission(self):
+        """Return whether the current user can receive push notifications."""
+        return self.has_group('base.group_user')
