21bc8a958b67800dd2df7d5a6ffe0e939f94441d
RISVANA A R <risvana@cybrosys.info>
2026-09-22 19:54:12 +0530

    Sep 22 : [ADD] Initial Commit 'easy_timesheet_add'

---
+++ easy_timesheet_add/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2026-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Technologies (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Quick Add Timesheet',
+    'version': '19.0.1.0.0',
+    'category': 'Project',
+    'summary': """Easily Add Timesheets via Systray.""",
+    'description': """This module helps to add timesheets from systray by 
+    selecting Project, Task and Description for Timesheets.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['hr_timesheet', 'project'],
+    'assets': {
+        'web.assets_backend': [
+            'easy_timesheet_add/static/src/js/TimesheetSystray.js',
+            'easy_timesheet_add/static/src/xml/TimesheetSystray.xml',
+        ]
+    },
+    'pre_init_hook': '_pre_init_project',
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ easy_timesheet_add/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <easy_timesheet_add>
+
+#### 23.07.2026
+#### Version 19.0.1.0.0
+##### ADD
+- Initial commit for Quick Add Timesheet
