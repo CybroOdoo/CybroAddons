2f30218a994c5134c70f77e099956ded7488a2d3
RISVANA A R <risvana@cybrosys.info>
2026-09-24 12:48:52 +0530

    Sep 23: [UPDT] Banner  Updated 'dynamic_accounts_report'

--- dynamic_accounts_report/__manifest__.py
+++ dynamic_accounts_report/__manifest__.py
@@ -73,7 +73,7 @@
             'dynamic_accounts_report/static/src/js/tax_report.js',
         ],
     },
-    'images': ['static/description/banner.gif'],
+    'images': ['static/description/banner.jpg'],
     'license': 'LGPL-3',
     'installable': True,
     'auto_install': False,
